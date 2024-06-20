eb19146b079c5c3501cc0c1d22b28a498edf93eb
ZX-ModelCloud <zx@modelcloud.ai>
2024-06-20 11:54:00 +0000

    cleanup

--- gptqmodel/models/phi3.py
+++ gptqmodel/models/phi3.py
@@ -1,16 +1,14 @@
 from .base import BaseGPTQModel
 
 
-class Phi3GPTQForCausalLM(BaseGPTQModel):
+class Phi3GPTQ(BaseGPTQModel):
+    non_layer_modules = ["model.embed_tokens", "embed_dropout", "model.norm"]
+
+    layers_node = "model.layers"
     layer_type = "Phi3DecoderLayer"
-    layers_block_name = "model.layers"
-    outside_layer_modules = ["model.embed_tokens", "embed_dropout", "model.norm"]
-    inside_layer_modules = [
+    layer_modules = [
         ["self_attn.qkv_proj"],
         ["self_attn.o_proj"],
         ["mlp.gate_up_proj"],
         ["mlp.down_proj"],
     ]
-
-
-__all__ = ["Phi3GPTQForCausalLM"]
