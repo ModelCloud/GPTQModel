6fff288ed214479b9ae47f8505ec81a800f1396c
LRL-ModelCloud <lrl@modelcloud.ai>
2024-06-21 01:08:58 +0800

    use #isort:off and # isort: on

--- tests/test_repacking.py
+++ tests/test_repacking.py
@@ -1,14 +1,14 @@
 import copy
 import unittest
 
+# isort: off
+import torch
 import torch.nn as nn
+import gptqmodel_marlin_cuda
+# isort: on
 from gptqmodel.nn_modules.qlinear.qlinear_cuda_old import QuantLinear as CudaOldQuantLinear
 from gptqmodel.nn_modules.qlinear.qlinear_marlin import QuantLinear as MarlinQuantLinear
 from gptqmodel.nn_modules.qlinear.qlinear_marlin import _get_perms, dequantize_weight
-
-import torch # isort: skip
-import gptqmodel_marlin_cuda # isort: skip
-
 
 
 def gen_quant4(k, n, groupsize=-1):
