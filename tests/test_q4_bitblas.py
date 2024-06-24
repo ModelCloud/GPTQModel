14990bed8bbbe968af392db3a94064ea2032565b
ZX-ModelCloud <zx@modelcloud.ai>
2024-06-24 08:17:36 +0000

    add test_q4_bitblas.py

---
+++ tests/test_q4_bitblas.py
@@ -0,0 +1,79 @@
+# -- do not touch
+import os
+
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+# -- end do not touch
+
+import unittest  # noqa: E402
+
+import torch  # noqa: E402
+from gptqmodel.nn_modules.qlinear.qlinear_bitblas import QuantLinear as BitBLASQuantLinear  # noqa: E402
+
+try:
+    from exllama_kernels import prepare_buffers, set_tuning_params  # noqa: F401
+except ImportError as e:
+    print(f"[WARNING] Could not load exllama_kernels: {e}")
+
+from gptqmodel import GPTQModel  # noqa: E402
+from transformers import AutoTokenizer  # noqa: E402
+
+
+class TestQ4BitBLAS(unittest.TestCase):
+    def test_generation(self):
+        # Reference generated with the cuda-old kernel and TheBloke/Llama-2-7B-Chat-GPTQ
+        reference_output = "<s> I am in Paris and I am feeling very sad and lonely. everybody I know is busy and I don't have any friends here. I am staying in a small apartment in the 11th arrondissement and I am feeling very isolated. I miss my friends and family back home and I don'"
+
+        prompt = "I am in Paris and"
+        device = torch.device("cuda:0")
+
+        model_id = "TheBloke/Llama-2-7B-Chat-GPTQ"
+
+        try:
+            model_q = GPTQModel.from_quantized(model_id, device="cuda:0", use_bitblas=True)
+        except ValueError as e:
+            raise e
+
+        has_bitblas = False
+        for _, module in model_q.named_modules():
+            if isinstance(module, BitBLASQuantLinear):
+                has_bitblas = True
+                break
+        self.assertTrue(has_bitblas)
+
+        tokenizer = AutoTokenizer.from_pretrained(model_id)
+
+        inp = tokenizer(prompt, return_tensors="pt").to(device)
+
+        res = model_q.generate(**inp, num_beams=1, min_new_tokens=60, max_new_tokens=60)
+
+        predicted_text = tokenizer.decode(res[0])
+
+        self.assertEqual(predicted_text, reference_output)
+
+    def test_bias(self):
+        # TheBloke/Llama-2-7B-Chat-GPTQ has bias, but they are all zeros, use a checkpoint which really uses bias.
+        model_id = "s3nh/starcoderbase-1b-GPTQ"
+        try:
+            model_q = GPTQModel.from_quantized(model_id, device="cuda:0", use_bitblas=True)
+        except ValueError as e:
+            raise e
+
+        for _, param in model_q.named_parameters():
+            self.assertTrue(param.device != torch.device("meta"))
+
+        for _, param in model_q.named_buffers():
+            self.assertTrue(param.device != torch.device("meta"))
+
+        self.assertTrue(torch.count_nonzero(model_q.model.transformer.h[0].attn.c_proj.bias) > 0)
+        self.assertTrue(torch.count_nonzero(model_q.model.transformer.h[0].attn.c_attn.bias) > 0)
+
+        tokenizer = AutoTokenizer.from_pretrained("Xenova/starcoderbase-1b")
+
+        prompt = "Today I am in Paris and"
+        inp = tokenizer(prompt, return_tensors="pt").to("cuda:0")
+
+        res = model_q.generate(**inp, num_beams=1, min_new_tokens=60, max_new_tokens=60)
+
+        predicted_text = tokenizer.decode(res[0])
+
+        self.assertTrue(predicted_text.startswith("Today I am in Paris and I am a student of the Master's"))
