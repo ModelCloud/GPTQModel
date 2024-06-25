923cb44c59e6ddd5a1622bbee598c85aefa23754
LRL-ModelCloud <165116337+LRL-ModelCloud@users.noreply.github.com>
2024-06-25 16:59:46 +0800

    fix marlin format desc_act must be False. (#57)
    Co-authored-by: LRL-ModelCloud <lrl@modelcloud.ai>

--- tests/test_perplexity.py
+++ tests/test_perplexity.py
@@ -47,7 +47,6 @@ class TestPerplexity(unittest.TestCase):
         model = AutoModelForCausalLM.from_pretrained(
             self.NATIVE_MODEL_ID,
             device_map="auto",
-            torch_dtype=torch.float16,
         )
 
         self.native_ppl = self.calculate_avg_ppl(model, self.tokenizer)
@@ -89,6 +88,7 @@ class TestPerplexity(unittest.TestCase):
             bits=4,
             group_size=128,
             format=format,
+            desc_act=False if format == FORMAT.MARLIN else True
         )
 
         model = GPTQModel.from_pretrained(
@@ -115,5 +115,5 @@ class TestPerplexity(unittest.TestCase):
             print(f"Format {format}, Quantized PPL: {quantized_ppl}")
 
             # 4090: [wikitext-2-raw-v1, test, text, 512, 512] data split
-            # FORMAT.GTPQ and FORMAT.GTPQ_V2 ppl == 8.7954, FORMAT.MARLIN ppl == 8.9865
+            # FORMAT.GTPQ and FORMAT.GTPQ_V2 ppl == 8.7863, FORMAT.MARLIN ppl == 9.0036
             assert abs(quantized_ppl - self.native_ppl) < 0.6
