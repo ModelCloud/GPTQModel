e089a0282f4a4df15e4e9a96f9435f476c5d26af
Qubitium-ModelCloud <qubitium@modelcloud.ai>
2024-06-27 16:29:43 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ at_least_one_cuda_v6 = any(torch.cuda.get_device_capability(i)[0] >= 6 for i in 
 
 if not at_least_one_cuda_v6:
     raise EnvironmentError(
-        "At least one device must have CUDA capability >= 6.0\nExiting setup..."
+       "GPTQModel requires at least one gpu device with CUDA compute capability >= `6.0`."
     )
 
 extras_require = {
