93db0eeb4880e25d7f3a8725ed0ac014985ce5fd
CSY-ModelCloud <csy@modelcloud.ai>
2024-07-01 17:49:46 +0800

    fix doc

--- gptqmodel/models/auto.py
+++ gptqmodel/models/auto.py
@@ -77,10 +77,10 @@ MODEL_MAP = {
     "deepseek_v2": DeepSeekV2GPTQ,
 }
 
-at_least_one_cuda_v6 = any(torch.cuda.get_device_capability(i)[0] >= 6 for i in range(torch.cuda.device_count()))
-
-if not at_least_one_cuda_v6:
-    raise EnvironmentError("GPTQModel requires at least one GPU device with CUDA compute capability >= `6.0`.")
+# at_least_one_cuda_v6 = any(torch.cuda.get_device_capability(i)[0] >= 6 for i in range(torch.cuda.device_count()))
+#
+# if not at_least_one_cuda_v6:
+#     raise EnvironmentError("GPTQModel requires at least one GPU device with CUDA compute capability >= `6.0`.")
 
 
 class GPTQModel:
