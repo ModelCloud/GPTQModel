3f250dd496ab069b3038a23133f7e3759a3845e4
LRL-ModelCloud <lrl@modelcloud.ai>
2024-07-01 16:20:59 +0800

    add Gemma2GPTQ

---
+++ gptqmodel/models/gemma2.py
@@ -0,0 +1,14 @@
+from .base import BaseGPTQModel
+
+
+class Gemma2GPTQ(BaseGPTQModel):
+    base_modules = ["model.embed_tokens", "model.norm"]
+
+    layers_node = "model.layers"
+    layer_type = "Gemma2DecoderLayer"
+    layer_modules = [
+        ["self_attn.k_proj", "self_attn.v_proj", "self_attn.q_proj"],
+        ["self_attn.o_proj"],
+        ["mlp.up_proj", "mlp.gate_proj"],
+        ["mlp.down_proj"],
+    ]
