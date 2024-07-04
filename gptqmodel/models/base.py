e5ecb805c78c9eac07004cd0e100bb6a065c4ff8
CSY-ModelCloud <csy@modelcloud.ai>
2024-07-04 16:54:22 +0800

    remove cpu check again.. not sure what it is

--- gptqmodel/models/base.py
+++ gptqmodel/models/base.py
@@ -218,7 +218,6 @@ class BaseGPTQModel(nn.Module):
         num_batches = len(calibration_dataset)
         layers = get_module_by_name_prefix(self.model, self.layers_node)
 
-        cur_device = self.device
         cur_layer_device = get_device(layers[0])
         data_device = cur_layer_device if calibration_enable_gpu_cache else CPU
 
@@ -246,7 +245,7 @@ class BaseGPTQModel(nn.Module):
             raise ValueError
 
         force_layer_back_to_cpu = False
-        if get_device(layers[0]) == CPU and cur_device != torch.device("cpu"):
+        if get_device(layers[0]) == CPU:
             layers[0] = layers[0].to(CUDA_0)
             force_layer_back_to_cpu = True
 
@@ -304,7 +303,7 @@ class BaseGPTQModel(nn.Module):
             layer_pb.set_description(f"Quantizing layer {i + 1} of {layer_count}")
             layer = layers[i]
             force_layer_back_to_cpu = False
-            if get_device(layer) == CPU and cur_device != torch.device("cpu"):
+            if get_device(layer) == CPU:
                 move_to(layer, CUDA_0)
                 force_layer_back_to_cpu = True
             cur_layer_device = get_device(layer)
@@ -412,8 +411,7 @@ class BaseGPTQModel(nn.Module):
                 layer_outputs,
                 [],
             )  # TODO: is it really OK to cache only the first positional argument?
-            if cur_device != torch.device("cpu"):
-                torch.cuda.empty_cache()
+            torch.cuda.empty_cache()
 
         logger.info(f"Quantization summary:\n{quant_log}")
         for module_log in quant_log:
@@ -437,8 +435,7 @@ class BaseGPTQModel(nn.Module):
 
         self._quantized = True
 
-        if cur_device != torch.device("cpu"):
-            torch.cuda.empty_cache()
+        torch.cuda.empty_cache()
 
         if self.quantize_config.format == FORMAT.BITBLAS:
             from ..nn_modules.qlinear.qlinear_bitblas import BitBLASQuantLinear
