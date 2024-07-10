a49e13a590341feb1171b4a71bd7973734b37da6
Qubitium <Qubitium@modelcloud.ai>
2024-07-10 15:22:19 +0000

    add log/info for TVM arch matching to debug 4090 ci issue

--- gptqmodel/nn_modules/qlinear/bitblas_target_detector.py
+++ gptqmodel/nn_modules/qlinear/bitblas_target_detector.py
@@ -1,5 +1,5 @@
 # Copyright (c) Microsoft Corporation.
-# Licensed under the MIT License.
+# License: GPTQModel/licenses/LICENSE.mit
 import logging
 import os
 import subprocess
@@ -15,6 +15,7 @@ TARGET_MISSING_ERROR = (
     "TVM target not found. Please set the TVM target environment variable using `export TVM_TARGET=<target>`, "
     "where <target> is one of the available targets can be found in the output of `tools/get_available_targets.py`."
 )
+
 
 def get_gpu_model_from_nvidia_smi(gpu_id: int = 0):
     """
@@ -45,12 +46,14 @@ def get_gpu_model_from_nvidia_smi(gpu_id: int = 0):
 
     return gpus[gpu_id]
 
+
 def find_best_match(tags, query):
     """
     Finds the best match for a query within a list of tags using fuzzy string matching.
     """
     MATCH_THRESHOLD = 25
     best_match, score = process.extractOne(query, tags)
+    logger.info(f"TVM arch find_best_match: best_match = {best_match}, score = {score}")
 
     def check_target(best, default):
         return best if Target(best).arch == Target(default).arch else default
@@ -88,7 +91,6 @@ def patched_auto_detect_nvidia_target(gpu_id: int = 0) -> str:
     # Get the current GPU model and find the best matching target
     gpu_model = get_gpu_model_from_nvidia_smi(gpu_id=gpu_id)
 
-    # TODO: move to a more res-usable device remapping util method
     # compat: Nvidia makes several oem (non-public) versions of A100 and perhaps other models that
     # do not have clearly defined TVM matching target so we need to manually map them to the correct one.
     if gpu_model in ["NVIDIA PG506-230", "NVIDIA PG506-232"]:
