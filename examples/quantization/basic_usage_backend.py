4019f7c9a2d6dadd7d5fe5ffc2e82cddac12c3f2
zyc <zyc@lbx.dev>
2024-07-15 05:18:07 +0000

    modify backend usage code

--- examples/quantization/basic_usage_backend.py
+++ examples/quantization/basic_usage_backend.py
@@ -14,6 +14,7 @@ pretrained_model_id = "facebook/opt-125m"
 quantized_model_id = "./facebook/opt-125m-4bit-128g"
 
 def main():
+    global quantized_model_id
 
     parser = ArgumentParser()
     parser.add_argument("--backend", choices=['AUTO', 'TRITON', 'EXLLAMA', 'EXLLAMA_V2', 'MARLIN', 'BITBLAS', 'QBITS', 'SGLANG', 'VLLM'])
@@ -21,62 +22,71 @@ def main():
 
     backend = get_backend(args.backend)
 
-    sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
+    device = 'cpu' if backend == BACKEND.QBITS else 'cuda:0'
 
     if backend == BACKEND.SGLANG:
         subprocess.check_call([sys.executable, "-m", "pip", "install", "sglang>=0.1.19"])
     elif backend == BACKEND.VLLM:
         subprocess.check_call([sys.executable, "-m", "pip", "install", "vllm>=0.5.1"])
 
-    tokenizer = AutoTokenizer.from_pretrained(pretrained_model_id, use_fast=True)
-    examples = [
-        tokenizer(
-            "gptqmodel is an easy-to-use model quantization library with user-friendly apis, based on GPTQ algorithm."
+    prompt = "I am in Paris and"
+
+    if backend == BACKEND.SGLANG or backend == BACKEND.VLLM:
+        quantized_model_id = "LnL-AI/TinyLlama-1.1B-Chat-v1.0-GPTQ-4bit"
+
+        if backend == BACKEND.SGLANG:
+            subprocess.check_call([sys.executable, "-m", "pip", "install", "sglang>=0.1.19"])
+            model = GPTQModel.from_quantized(
+                quantized_model_id,
+                device=device,
+                backend=backend,
+                disable_flashinfer=True
+            )
+
+            output = model.generate(prompts=prompt, temperature=0.8, top_p=0.95)
+            model.shutdown()
+            del model
+        else:
+            subprocess.check_call([sys.executable, "-m", "pip", "install", "vllm>=0.5.1"])
+            model = GPTQModel.from_quantized(
+                quantized_model_id,
+                device=device,
+                backend=backend,
+            )
+            output = model.generate(prompts=prompt, temperature=0.8, top_p=0.95)[0].outputs[0].text
+    else:
+        tokenizer = AutoTokenizer.from_pretrained(pretrained_model_id, use_fast=True)
+        examples = [
+            tokenizer(
+                "I am in Paris and I am going to be there for a week. I am going to be in the middle of the city and I am going to be in the middle of the city. I am going to be in the middle of the city and I am going to be in the middle of the city. I am"
+            )
+        ]
+
+        quantize_config = QuantizeConfig(
+            bits=4,  # quantize model to 4-bit
+            group_size=128,  # it is recommended to set the value to 128
+            # set to False can significantly speed up inference but the perplexity may slightly bad
+            desc_act=False if backend == BACKEND.BITBLAS or backend == BACKEND.MARLIN else True,
         )
-    ]
 
-    quantize_config = QuantizeConfig(
-        bits=4,  # quantize model to 4-bit
-        group_size=128,  # it is recommended to set the value to 128
-        # set to False can significantly speed up inference but the perplexity may slightly bad
-        desc_act=False if backend == BACKEND.BITBLAS or backend == BACKEND.MARLIN else True,
-    )
+        # load un-quantized model, by default, the model will always be loaded into CPU memory
+        model = GPTQModel.from_pretrained(pretrained_model_id, quantize_config)
 
-    # load un-quantized model, by default, the model will always be loaded into CPU memory
-    model = GPTQModel.from_pretrained(pretrained_model_id, quantize_config)
+        # quantize model, the examples should be list of dict whose keys can only be "input_ids" and "attention_mask"
+        model.quantize(examples)
 
-    # quantize model, the examples should be list of dict whose keys can only be "input_ids" and "attention_mask"
-    model.quantize(examples)
+        # save quantized model
+        model.save_quantized(quantized_model_id)
+        tokenizer.save_pretrained(quantized_model_id)
 
-    # save quantized model
-    model.save_quantized(quantized_model_id)
-    tokenizer.save_pretrained(quantized_model_id)
-
-    prompt = "gptqmodel is"
-
-    device = 'cpu' if backend == BACKEND.QBITS else 'cuda:0'
-
-    if backend == BACKEND.SGLANG:
         model = GPTQModel.from_quantized(
             quantized_model_id,
             device=device,
             backend=backend,
-            disable_flashinfer=True
-        )
-    else:
-        model = GPTQModel.from_quantized(
-            quantized_model_id,
-            device=device,
-            backend=backend,
         )
 
-    inp = tokenizer(prompt, return_tensors="pt").to(device)
+        inp = tokenizer(prompt, return_tensors="pt").to(device)
 
-    if backend == BACKEND.VLLM:
-        output = model.generate(prompts=prompt, sampling_params=sampling_params)[0].outputs[0].text
-    elif backend == BACKEND.SGLANG:
-        output = model.generate(prompts=prompt, temperature=0.8, top_p=0.95)
-    else:
         res = model.generate(**inp, num_beams=1, min_new_tokens=60, max_new_tokens=60)
         output = tokenizer.decode(res[0])
 
