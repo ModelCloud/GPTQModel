4c9cf754120722f68d860e75edf41aaf33393713
LRL-ModelCloud <lrl@modelcloud.ai>
2024-07-22 13:53:43 +0800

    gemma2 27b can load with vLLM now

--- gptqmodel/models/gemma2.py
+++ gptqmodel/models/gemma2.py
@@ -1,5 +1,13 @@
 from .base import BaseGPTQModel
+import logging
+from logging import getLogger
 
+logger = getLogger(__name__)
+handler = logging.StreamHandler()
+formatter = logging.Formatter("%(levelname)s - %(message)s")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
 
 class Gemma2GPTQ(BaseGPTQModel):
     base_modules = ["model.embed_tokens", "model.norm"]
@@ -16,10 +24,13 @@ class Gemma2GPTQ(BaseGPTQModel):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        # There is an issue with duplicate outputs in the quantized gemma-2 model 27b.
-        # Until this issue is fixed, quantize gemma-2 27b model is not supported.
+        # There is an issue with duplicate outputs in the quantized gemma-2 model 27b with transformers.
+        # Until this issue is fixed, quantized gemma-2 27b model only support vLLM load.
         num_hidden_layers = getattr(self.model.config, "num_hidden_layers")
         # The gemma-2 model 9b has 42 hidden layers, while the gemma-2 model 27b has 46 hidden layers.
         if num_hidden_layers > 42:
-            raise ValueError("Only Gemma-2 9B models are supported at the moment. For Gemma-2 27B models please following the github issue at https://github.com/ModelCloud/GPTQModel/issues/140 .")
+            if self.quantized:
+                raise ValueError("Currently, only vllm can load the quantized gemma2-27b for proper inference. https://huggingface.co/ModelCloud/gemma-2-27b-it-gptq-4bit is a quantized gemma-2-27b-it model, along with an example of loading it using vLLM.")
+            else:
+                logger.warning("Currently, only vllm can load the quantized gemma2-27b for proper inference. https://huggingface.co/ModelCloud/gemma-2-27b-it-gptq-4bit is a quantized gemma-2-27b-it model, along with an example of loading it using vLLM.")
 
