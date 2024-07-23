5b18b257f31c404e356d0d3d2cdfc5248ea6d33f
CSY-ModelCloud <csy@modelcloud.ai>
2024-07-23 11:28:51 +0800

    fix flashinfer index url

--- tests/test_sglang.py
+++ tests/test_sglang.py
@@ -6,16 +6,20 @@ os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 import subprocess  # noqa: E402
 import sys  # noqa: E402
 import unittest  # noqa: E402
+import torch  # noqa: E402
 
 from gptqmodel import BACKEND, GPTQModel  # noqa: E402
 
+torch_version = '.'.join(torch.__version__.split('.')[:2])
+cuda_version = torch.version.cuda.replace('.', '')
+pip_index_url = f"https://flashinfer.ai/whl/cu{cuda_version}/torch{torch_version}"
 
 class TestLoadSglang(unittest.TestCase):
 
     @classmethod
     def setUpClass(self):
         # sglang set disable_flashinfer=True still import flashinfer
-        subprocess.check_call([sys.executable, "-m", "pip", "install", "flashinfer", "-i", "https://flashinfer.ai/whl/cu121/torch2.3"])
+        subprocess.check_call([sys.executable, "-m", "pip", "install", "flashinfer", "-i", pip_index_url])
         subprocess.check_call([sys.executable, "-m", "pip", "install", "sglang[srt]>=0.1.19"])
 
         self.MODEL_ID = "LnL-AI/TinyLlama-1.1B-Chat-v1.0-GPTQ-4bit"
