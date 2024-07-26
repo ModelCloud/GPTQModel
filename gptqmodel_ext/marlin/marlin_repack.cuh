8c684003ee907dbf2d1bc35b47ab89ea2a9d6f99
ZX-ModelCloud <zx@modelcloud.ai>
2024-07-26 11:53:30 +0000

    update marlin_repack.cu

--- gptqmodel_ext/marlin/marlin_repack.cuh
+++ gptqmodel_ext/marlin/marlin_repack.cuh
@@ -1,12 +1,10 @@
-#include <torch/all.h>
+#include "marlin.cuh"
 
-__global__ void gptq_repack_kernel(
-  uint32_t* in,
-  uint32_t* out,
-  int m,
-  int n
-);
+__global__ void gptq_marlin_repack_kernel(
+    uint32_t const* __restrict__ b_q_weight_ptr,
+    uint32_t const* __restrict__ perm_ptr, uint32_t* __restrict__ out_ptr,
+    int size_k, int size_n)
 
-torch::Tensor gptq_repack(
-    torch::Tensor W
-);
+torch::Tensor gptq_repack(torch::Tensor& b_q_weight, torch::Tensor& perm,
+                                 int64_t size_k, int64_t size_n,
+                                 int64_t num_bits)
