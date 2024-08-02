1f2edc5d7a3916038e6a86ca0a96cf306a3c260e
ZX-ModelCloud <zx@modelcloud.ai>
2024-08-02 08:02:44 +0000

    add test for BACKEND.Marlin

--- tests/test_dynamic_bits.py
+++ tests/test_dynamic_bits.py
@@ -6,6 +6,7 @@ os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 
 import tempfile  # noqa: E402
 import unittest  # noqa: E402
+from parameterized import parameterized
 
 from datasets import load_dataset  # noqa: E402
 from gptqmodel import BACKEND, GPTQModel  # noqa: E402
@@ -35,22 +36,22 @@ class TestDynamic(unittest.TestCase):
         return avg
 
     @classmethod
-    def setUpClass(self):
-        self.tokenizer = AutoTokenizer.from_pretrained(self.NATIVE_MODEL_ID, use_fast=True)
+    def setUpClass(cls):
+        cls.tmp_dir = tempfile.TemporaryDirectory()
+        cls.tokenizer = AutoTokenizer.from_pretrained(cls.NATIVE_MODEL_ID, use_fast=True)
 
-        if not self.tokenizer.pad_token_id:
-            self.tokenizer.pad_token_id = self.tokenizer.eos_token_id
+        if not cls.tokenizer.pad_token_id:
+            cls.tokenizer.pad_token_id = cls.tokenizer.eos_token_id
 
         traindata = load_dataset("wikitext", "wikitext-2-raw-v1", split="train").filter(lambda x: len(x['text']) >= 512)
-        self.calibration_dataset = [self.tokenizer(example["text"]) for example in traindata.select(range(1024))]
+        cls.calibration_dataset = [cls.tokenizer(example["text"]) for example in traindata.select(range(1024))]
 
-    def test_dynamic_bits(self):
         dynamic_bits = {
             # `.*\.` matches the layers_node prefix
-            r".*\.18\..*gate.*": 8, # match layer 18 (index start at 0) gate module
-            r".*\.19\..*gate.*": 8, # match layer 19 (index start at 0) gate module
-            r".*\.20\..*gate.*": 8, # match layer 21 (index start at 0) gate module
-            r".*\.21\..*gate.*": 8, # match layer 22 (index start at 0) gate module
+            r".*\.18\..*gate.*": 8,  # match layer 18 (index start at 0) gate module
+            r".*\.19\..*gate.*": 8,  # match layer 19 (index start at 0) gate module
+            r".*\.20\..*gate.*": 8,  # match layer 21 (index start at 0) gate module
+            r".*\.21\..*gate.*": 8,  # match layer 22 (index start at 0) gate module
         }
         quantize_config = QuantizeConfig(
             bits=4,
@@ -58,24 +59,33 @@ class TestDynamic(unittest.TestCase):
             group_size=128,
         )
         model = GPTQModel.from_pretrained(
-            self.NATIVE_MODEL_ID,
+            cls.NATIVE_MODEL_ID,
             quantize_config=quantize_config,
         )
-        model.quantize(self.calibration_dataset, batch_size=4)
+        model.quantize(cls.calibration_dataset, batch_size=4)
 
-        with tempfile.TemporaryDirectory() as tmp_dir:
-            model.save_quantized(
-                tmp_dir,
-            )
+        model.save_quantized(
+            cls.tmp_dir.name,
+        )
 
-            del model
+    @classmethod
+    def tearDownClass(cls):
+        cls.tmp_dir.cleanup()
+        assert os.path.exists(cls.tmp_dir.name)
 
-            model = GPTQModel.from_quantized(
-                tmp_dir,
-                backend=BACKEND.TRITON,
-            )
+    @parameterized.expand(
+        [
+            (BACKEND.TRITON),
+            (BACKEND.MARLIN),
+        ]
+    )
+    def test_dynamic_bits(self, backend):
+        model = GPTQModel.from_quantized(
+            self.tmp_dir.name,
+            backend=backend,
+        )
 
-            dynamic_bits_ppl = self.calculate_avg_ppl(model, self.tokenizer)
+        dynamic_bits_ppl = self.calculate_avg_ppl(model, self.tokenizer)
 
-            del model
-            assert dynamic_bits_ppl < 10
+        del model
+        assert dynamic_bits_ppl < 10
