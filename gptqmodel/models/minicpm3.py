6fe5378e5ffee062a143410e1772878f1cf96d4e
LRL-ModelCloud <165116337+LRL-ModelCloud@users.noreply.github.com>
2024-09-19 10:58:32 +0800

    fix minicpm3 support (#387)
    Co-authored-by: LRL-ModelCloud <lrl@modelcloud.ai>

--- gptqmodel/models/minicpm3.py
+++ gptqmodel/models/minicpm3.py
@@ -1,15 +1,15 @@
-from ._base import BaseGPTQModel
+from .base import BaseGPTQModel
+
+
 class MiniCPM3GPTQ(BaseGPTQModel):
+    base_modules = ["model.embed_tokens",]
+
+    layers_node = "model.layers"
     layer_type = "MiniCPM3DecoderLayer"
-    layers_block_name = "model.layers"
-    outside_layer_modules = [
-        "model.embed_tokens",
-    ]
-    inside_layer_modules = [
+    layer_modules = [
         ["self_attn.q_a_proj","self_attn.kv_a_proj_with_mqa"],
         ["self_attn.q_b_proj","self_attn.kv_b_proj"],
         ["self_attn.o_proj"],
         ["mlp.gate_proj","mlp.up_proj"],
         ["mlp.down_proj"],
     ]
-__all__ = ["MiniCPM3GPTQForCausalLM"]
