e5b559e66c11ccc03cbe9ab6889a965cfd9a16c8
LRL-ModelCloud <lrl@modelcloud.ai>
2024-09-26 16:15:53 +0800

    add mllama support

---
+++ gptqmodel/models/mllama.py
@@ -0,0 +1,22 @@
+from .base import BaseGPTQModel
+
+
+class MLlamaGPTQ(BaseGPTQModel):
+    # Non-repeating layers at the root level: same level as `layers_node`
+    # Excluding `layers_node`.
+    base_modules = ["language_model.model.embed_tokens", "language_model.model.norm"]
+
+    # Below describes all the repeating layers in this transformer model
+    # `model.layers` is a node/module that hold all the repeating layers. The parent node for all n-layers.
+    layers_node = "language_model.model.layers"
+    # Each repeating layer in `model.layers` is of type `LlamaDecoderLayer`
+    layer_type = ["MllamaSelfAttentionDecoderLayer", "MllamaCrossAttentionDecoderLayer"]
+    # Inside each `LlamaDecoderLayer` layer are many internal modules
+    # List them in the order executed in model forward() code
+    # Many models have same execution order of: attention (q_k_v) projection, attention (output) projection, mlp (n) projections
+    layer_modules = [
+        ["self_attn.k_proj", "self_attn.v_proj", "self_attn.q_proj"],
+        ["self_attn.o_proj"],
+        ["mlp.up_proj", "mlp.gate_proj"],
+        ["mlp.down_proj"],
+    ]
