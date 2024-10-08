bf26114a4fbbe5f38655be7e30daf71101966920
ZX <zx@lbx.dev>
2024-10-08 09:52:43 +0000

    fix wrong variable name

--- gptqmodel/models/base.py
+++ gptqmodel/models/base.py
@@ -683,7 +683,7 @@ class BaseGPTQModel(nn.Module):
             model_save_name = model_base_name + ".safetensors"
         else:
             model_save_name = model_base_name + ".bin"
-        if not self.qlinear_kernel.SUPPORTED_SHARDS and max_shard_size is not None:
+        if not self.qlinear_kernel.SUPPORTS_SHARDS and max_shard_size is not None:
             logger.warning("Sharding is not supported for this quant. Disabling sharding.")
             max_shard_size = None
 
