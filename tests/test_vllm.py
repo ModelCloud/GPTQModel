4333954d755d41a1e7d57215f7c944f449a2a696
PZS-ModelCloud <pzs@modelcloud.ai>
2024-10-10 02:45:55 +0000

    update vllm version

--- tests/test_vllm.py
+++ tests/test_vllm.py
@@ -17,7 +17,7 @@ class TestLoadVLLM(unittest.TestCase):
     @classmethod
     def setUpClass(self):
         subprocess.check_call([sys.executable, "-m", "pip", "install", "flashinfer", "-i", f"https://flashinfer.ai/whl/cu{torch.version.cuda.replace('.', '')}/torch{'.'.join(torch.__version__.split('.')[:2])}"])
-        subprocess.check_call([sys.executable, "-m", "pip", "install", "vllm>=0.5.1"])
+        subprocess.check_call([sys.executable, "-m", "pip", "install", "vllm>=0.6.2"])
         from vllm import SamplingParams  # noqa: E402
         self.MODEL_ID = "LnL-AI/TinyLlama-1.1B-Chat-v1.0-GPTQ-4bit"
         self.SHARDED_MODEL_ID = "ModelCloud/TinyLlama-1.1B-Chat-v1.0-GPTQ-4bit-sharded"
