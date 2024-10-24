97a1a9026240f61cae9e5e3d257a6d376922f869
PZS-ModelCloud <pzs@modelcloud.ai>
2024-10-24 10:45:01 +0000

    update code

--- tests/example/base_test.py
+++ tests/example/base_test.py
@@ -4,23 +4,39 @@ from gptqmodel import BACKEND, GPTQModel
 from gptqmodel.quantization import FORMAT  # noqa: E402
 from gptqmodel.quantization.config import QuantizeConfig # noqa: E402
 from transformers import AutoTokenizer  # noqa: E402
+import tempfile
 
 class BaseTest(unittest.TestCase):
     GENERATE_EVAL_SIZE = 100
 
     def generate(self, model, tokenizer):
         prompt = "I am in Paris and"
-
-        inp = tokenizer(prompt, return_tensors="pt").to(model.device)
+        device = model.device
+        inp = tokenizer(prompt, return_tensors="pt").to(device)
         res = model.generate(**inp, num_beams=1, do_sample=False, min_new_tokens=60, max_new_tokens=60)
         output = tokenizer.decode(res[0])
 
         return output
 
-    def loadModel(self, model_name_or_path, quant=False):
+    def load_tokenizer(self, model_name_or_path):
         tokenizer = AutoTokenizer.from_pretrained(model_name_or_path, trust_remote_code=True)
+        return tokenizer
+
+    def load_dataset(self, tokenizer):
         traindata = load_dataset("wikitext", "wikitext-2-raw-v1", split="train").filter(lambda x: len(x['text']) >= 512)
         calibration_dataset = [tokenizer(example["text"]) for example in traindata.select(range(1024))]
+        return calibration_dataset
+
+    def quantModel(self, model, tokenizer):
+        calibration_dataset = self.load_dataset(tokenizer)
+        model.quantize(calibration_dataset, batch_size=64)
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            model.save_quantized(tmpdirname)
+            q_model, q_tokenizer = self.loadQuantModel(tmpdirname)
+        return q_model, q_tokenizer
+
+    def loadModel(self, model_name_or_path, quant=False):
+        tokenizer = self.load_tokenizer(model_name_or_path)
 
         quantize_config = QuantizeConfig(
             bits=4,
@@ -34,8 +50,7 @@ class BaseTest(unittest.TestCase):
         )
 
         if quant:
-            quantized_model = model.quantize(calibration_dataset, batch_size=64)
-            return quantized_model, tokenizer
+            return self.quantModel(model, tokenizer)
         else:
             return model, tokenizer
 
---
+++ tests/example/test_llama3.py
@@ -0,0 +1,19 @@
+# -- do not touch
+import os
+
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+# -- end do not touch
+from base_test import BaseTest
+
+class TestLlama3(BaseTest):
+    NATIVE_MODEL_ID = "meta-llama/Llama-3.1-8B-Instruct"
+    # QUANT_MODEL_ID = "ModelCloud/Meta-Llama-3.1-8B-gptq-4bit"
+
+    def test_llama3(self):
+        model, tokenizer = self.loadModel(self.NATIVE_MODEL_ID, quant=True)
+        # model, tokenizer = self.loadQuantModel(self.QUANT_MODEL_ID)
+        reference_output = "<|begin_of_text|>I am in Paris and I am looking for a job in the field of international relations. I am a French citizen, 24 years old, and I have a master degree in international relations. I am fluent in English and I am looking for a job in the field of international relations. I am available for a job in"
+        result = self.generate(model, tokenizer)
+        print(result)
+
+        self.assertEqual(result[:self.GENERATE_EVAL_SIZE], reference_output[:self.GENERATE_EVAL_SIZE])
