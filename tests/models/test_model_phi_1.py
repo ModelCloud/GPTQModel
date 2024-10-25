7978ae3aea528e23a650db737dc1264f7f700426
PZS-ModelCloud <pzs@modelcloud.ai>
2024-10-25 11:01:46 +0000

    fix format str

--- tests/models/test_model_phi_1.py
+++ tests/models/test_model_phi_1.py
@@ -5,7 +5,7 @@ class TestPhi_1(ModelTest):
 
     def test_phi_1(self):
         model, tokenizer = self.quantModel(self.NATIVE_MODEL_ID)
-        reference_output = "I am in Paris and I like to eat pizza and pasta.\"\n    \"\"\"\n    words = s.split()\n    new_words = []\n    for word in words:\n        if word.lower() == "paris":\n            new_words.append("pizza")\n        elif word.lower() == \"pasta\":\n            new_words.append(\"pasta\")\n        else:\n            new_words.append(word)\n    return " ".join\n"
+        reference_output = "I am in Paris and I like to eat pizza and pasta.\"\n    \"\"\"\n    words = s.split()\n    new_words = []\n    for word in words:\n        if word.lower() == \"paris\":\n            new_words.append(\"pizza\")\n        elif word.lower() == \"pasta\":\n            new_words.append(\"pasta\")\n        else:\n            new_words.append(word)\n    return " ".join\n"
         result = self.generate(model, tokenizer)
 
         self.assertEqual(result[:self.GENERATE_EVAL_SIZE], reference_output[:self.GENERATE_EVAL_SIZE])
