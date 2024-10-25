ca839429792ba9ac4b1f33ddcaa123ae1432fda3
PZS-ModelCloud <pzs@modelcloud.ai>
2024-10-25 09:15:05 +0000

    add falcon test

--- tests/models/test_model_falcon.py
+++ tests/models/test_model_falcon.py
@@ -1,12 +1,11 @@
 from model_test import ModelTest
 
 class TestFalcon(ModelTest):
-    NATIVE_MODEL_ID = "tiiuae/falcon-mamba-tiny-dev"
+    NATIVE_MODEL_ID = "tiiuae/falcon-7b-instruct"
 
     def test_falcon(self):
         model, tokenizer = self.quantModel(self.NATIVE_MODEL_ID)
-        reference_output = ""
+        reference_output = "I am in Paris and,.....\n,,,,,,,, ,,, and and,, ,, and and and,, ,, and and, and, and, and, and, and, and, and and, and, and, and, and, and, and, and, and, and, and, and, and, and, and, and, and, and, and, and, and, and, and, and the, and"
         result = self.generate(model, tokenizer)
 
-        self.assertTrue(len(result) > 0)
-        # self.assertEqual(result[:self.GENERATE_EVAL_SIZE], reference_output[:self.GENERATE_EVAL_SIZE])
+        self.assertEqual(result[:self.GENERATE_EVAL_SIZE], reference_output[:self.GENERATE_EVAL_SIZE])
