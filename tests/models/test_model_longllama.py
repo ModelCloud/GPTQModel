d663304a91c4de2b5170e06cd9172cdf36520291
PZS-ModelCloud <pzs@modelcloud.ai>
2024-10-25 08:42:14 +0000

    add longllama test

--- tests/models/test_model_longllama.py
+++ tests/models/test_model_longllama.py
@@ -4,9 +4,8 @@ class TestLongLlama(ModelTest):
     NATIVE_MODEL_ID = "syzymon/long_llama_3b_instruct"
 
     def test_longllama(self):
-        model, tokenizer = self.quantModel(self.NATIVE_MODEL_ID)
-        reference_output = ""
+        model, tokenizer = self.quantModel(self.NATIVE_MODEL_ID, True)
+        reference_output = "<s> I am in Paris andP\n\nP\n\nP\n\nP\nP\nP\nP\nP\nP\nP\nP\nP\nP\nP\nP\nP\nP\nP\nP\nP\nP\nP\nP\nP\nP\nP\nP\nP\nP\nP\nP\nP\nP\nP\nP\nP\nP\nP\nP\nP\nP\nP\nP\nP\nP\nP\nP\nP\nP\n"
         result = self.generate(model, tokenizer)
 
-        self.assertTrue(len(result) > 0)
-        # self.assertEqual(result[:self.GENERATE_EVAL_SIZE], reference_output[:self.GENERATE_EVAL_SIZE])
+        self.assertEqual(result[:self.GENERATE_EVAL_SIZE], reference_output[:self.GENERATE_EVAL_SIZE])
