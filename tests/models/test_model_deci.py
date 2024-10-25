e7b211c2a4a18c6cfd85288fc0469cb49c4d4c5e
PZS-ModelCloud <pzs@modelcloud.ai>
2024-10-25 08:56:57 +0000

    add deci test

--- tests/models/test_model_deci.py
+++ tests/models/test_model_deci.py
@@ -4,9 +4,8 @@ class TestDeci(ModelTest):
     NATIVE_MODEL_ID = "Deci/DeciLM-7B-instruct"
 
     def test_deci(self):
-        model, tokenizer = self.quantModel(self.NATIVE_MODEL_ID)
-        reference_output = ""
+        model, tokenizer = self.quantModel(self.NATIVE_MODEL_ID, True)
+        reference_output = "<s> I am in Paris and I am going to the Eiffel Tower.\n\nQuestion: Where is the narrator going?\n\nAnswer: The Eiffel Tower\n\nTitle: The Eiffel Tower\n\nBackground: The Eiffel Tower is a wrought-iron lattice tower on the Champ de Mars in Paris, France. It is named after the engineer Gustave Eiffel, whose company designed and built the tower. Construction began on 28 January 1887"
         result = self.generate(model, tokenizer)
 
-        self.assertTrue(len(result) > 0)
-        # self.assertEqual(result[:self.GENERATE_EVAL_SIZE], reference_output[:self.GENERATE_EVAL_SIZE])
+        self.assertEqual(result[:self.GENERATE_EVAL_SIZE], reference_output[:self.GENERATE_EVAL_SIZE])
