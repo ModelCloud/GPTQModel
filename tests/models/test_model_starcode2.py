4e5ce68856d88093353a6220b9f07da553d9ee89
PZS-ModelCloud <pzs@modelcloud.ai>
2024-10-25 10:21:35 +0000

    add starcode2 test

--- tests/models/test_model_starcode2.py
+++ tests/models/test_model_starcode2.py
@@ -1,12 +1,11 @@
 from model_test import ModelTest
-
+import torch
 class TestStarCode2(ModelTest):
     NATIVE_MODEL_ID = "bigcode/starcoder2-3b"
 
     def test_starcode2(self):
-        model, tokenizer = self.quantModel(self.NATIVE_MODEL_ID)
-        reference_output = ""
+        model, tokenizer = self.quantModel(self.NATIVE_MODEL_ID, torch_dtype=torch.float16)
+        reference_output = "I am in Paris and I am going to visit you.\n\nI am in Paris and I am going to visit you.\n\nI am in Paris and I am going to visit you.\n\nI am in Paris and I am going to visit you.\n\nI am in Paris and I am going to visit you.\n\nI am in Paris and I am going to visit you.\n\nI am in Paris and I am going to visit you.\n\nI"
         result = self.generate(model, tokenizer)
 
-        self.assertTrue(len(result) > 0)
-        # self.assertEqual(result[:self.GENERATE_EVAL_SIZE], reference_output[:self.GENERATE_EVAL_SIZE])
+        self.assertEqual(result[:self.GENERATE_EVAL_SIZE], reference_output[:self.GENERATE_EVAL_SIZE])
