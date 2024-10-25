8303c434e1b078182119903d3e62afb2ae3416cc
PZS-ModelCloud <pzs@modelcloud.ai>
2024-10-25 08:58:45 +0000

    add mistral test

--- tests/models/test_model_mistral.py
+++ tests/models/test_model_mistral.py
@@ -5,8 +5,7 @@ class TestMistral(ModelTest):
 
     def test_mistral(self):
         model, tokenizer = self.quantModel(self.NATIVE_MODEL_ID)
-        reference_output = ""
+        reference_output = "<s> I am in Paris and I am looking for a good restaurant for a special occasion. I have heard that Le Jules Verne is a great option, but I am wondering if there are any other restaurants in Paris that would be worth considering for a special occasion?\n\nThere are indeed several other restaurants in Paris that are known for their exceptional dining experiences and would be worth considering for a special occasion. Here are a few suggestions:\n\n1. L'Arpege: This three-Michelin-star"
         result = self.generate(model, tokenizer)
 
-        self.assertTrue(len(result) > 0)
-        # self.assertEqual(result[:self.GENERATE_EVAL_SIZE], reference_output[:self.GENERATE_EVAL_SIZE])
+        self.assertEqual(result[:self.GENERATE_EVAL_SIZE], reference_output[:self.GENERATE_EVAL_SIZE])
