07316b2679deecbc104d16e91bc23acc49bbaadc
PZS-ModelCloud <pzs@modelcloud.ai>
2024-10-25 09:38:59 +0000

    add gpt2 test

--- tests/models/test_model_gpt2.py
+++ tests/models/test_model_gpt2.py
@@ -1,12 +1,11 @@
 from model_test import ModelTest
-
+import torch
 class TestGpt2(ModelTest):
-    NATIVE_MODEL_ID = "openai-community/gpt2-medium"
+    NATIVE_MODEL_ID = "openai-community/gpt2"
 
     def test_gpt2(self):
-        model, tokenizer = self.quantModel(self.NATIVE_MODEL_ID)
-        reference_output = ""
+        model, tokenizer = self.quantModel(self.NATIVE_MODEL_ID, torch_dtype=torch.float16)
+        reference_output = "I am in Paris and I am going to be in Paris. I am going to be in Paris. I am going to be in Paris. I am going to be in Paris. I am going to be in Paris. I am going to be in Paris. I am going to be in Paris. I am going to be in Paris. I am going to be in Paris. I am going to be in Paris. I am going to be in Paris. I am going to be in Paris. I am going to"
         result = self.generate(model, tokenizer)
 
-        self.assertTrue(len(result) > 0)
-        # self.assertEqual(result[:self.GENERATE_EVAL_SIZE], reference_output[:self.GENERATE_EVAL_SIZE])
+        self.assertEqual(result[:self.GENERATE_EVAL_SIZE], reference_output[:self.GENERATE_EVAL_SIZE])
