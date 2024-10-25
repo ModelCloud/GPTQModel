04be38c979162871eb657390be8eb3b5397a23bf
PZS-ModelCloud <pzs@modelcloud.ai>
2024-10-25 08:37:53 +0000

    add cohere test

--- tests/models/test_model_cohere.py
+++ tests/models/test_model_cohere.py
@@ -5,8 +5,7 @@ class TestCohere(ModelTest):
 
     def test_cohere(self):
         model, tokenizer = self.quantModel(self.NATIVE_MODEL_ID)
-        reference_output = ""
+        reference_output = "<BOS_TOKEN>I am in Paris and I am in love. I am in love with the city, the people, the food, the art, the history, the architecture, the fashion, the music, the art, the art, the art, the art, the art, the art, the art, the art, the art, the art, the art, the art, the art, the art, the art, the art, the art, the art, the art, the art, the art, the art,"
         result = self.generate(model, tokenizer)
 
-        self.assertTrue(len(result) > 0)
-        # self.assertEqual(result[:self.GENERATE_EVAL_SIZE], reference_output[:self.GENERATE_EVAL_SIZE])
+        self.assertEqual(result[:self.GENERATE_EVAL_SIZE], reference_output[:self.GENERATE_EVAL_SIZE])
