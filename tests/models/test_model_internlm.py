cc99d7eae79a49cae9660f24aa7b1d31456dcc03
PZS-ModelCloud <pzs@modelcloud.ai>
2024-10-26 02:53:40 +0000

    code clean up

--- tests/models/test_model_internlm.py
+++ tests/models/test_model_internlm.py
@@ -4,8 +4,7 @@ class TestInternlm(ModelTest):
     NATIVE_MODEL_ID = "internlm/internlm-7b"
 
     def test_internlm(self):
-        # model, tokenizer = self.quantModel(self.NATIVE_MODEL_ID, True)
-        model, tokenizer = self.loadQuantModel(f"/monster/data/pzs/quantization/{self.NATIVE_MODEL_ID}", True)
+        model, tokenizer = self.quantModel(self.NATIVE_MODEL_ID, True)
 
         reference_output = ""
         result = self.generate(model, tokenizer)
