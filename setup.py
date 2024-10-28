11d5fdb53eab0ba72aa49cb2ecb94a29fb95db6f
CSY-ModelCloud <csy@modelcloud.ai>
2024-10-28 12:52:36 +0800

    print log for name & version

--- setup.py
+++ setup.py
@@ -223,6 +223,8 @@ class CachedWheelsCommand(_bdist_wheel):
             print("Raw wheel path", wheel_path)
 
             os.rename(wheel_filename, wheel_path)
+
+            print(f"defined name & version: {common_setup_kwargs['name']} {common_setup_kwargs['version']}")
         except (urllib.error.HTTPError, urllib.error.URLError):
             print("Precompiled wheel not found. Building from source...")
             # If the wheel could not be downloaded, build from source
