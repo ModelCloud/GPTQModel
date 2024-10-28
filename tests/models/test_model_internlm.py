f877fdbca109d254e6bb1f2291ac7195c8d6b1b0
PZS-ModelCloud <pzs@modelcloud.ai>
2024-10-28 09:02:23 +0000

    add internlm test

--- tests/models/test_model_internlm.py
+++ tests/models/test_model_internlm.py
@@ -4,10 +4,10 @@ class TestInternlm(ModelTest):
     NATIVE_MODEL_ID = "internlm/internlm-7b"
 
     def test_internlm(self):
+        # transformers 4.40.0 run normal
         model, tokenizer = self.quantModel(self.NATIVE_MODEL_ID, trust_remote_code=True)
 
-        reference_output = ""
+        reference_output = "<s>I am in Paris and I am in love with the city. I am in love with the people. I am in love with the food. I am in love with the art. I am in love with the fashion. I am in love with the architecture. I am in love with the history. I am in love with the language. I am in love with the culture. I am in love with the romance. I am in love with the city. I am in love with Paris.\nI am in love with"
         result = self.generate(model, tokenizer)
 
-        self.assertTrue(len(result) > 0)
-        # self.assertEqual(result[:self.GENERATE_EVAL_SIZE], reference_output[:self.GENERATE_EVAL_SIZE])
+        self.assertEqual(result[:self.GENERATE_EVAL_SIZE], reference_output[:self.GENERATE_EVAL_SIZE])
