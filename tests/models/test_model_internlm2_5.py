8bb251349ff04679d1302e089ca325d3e6d9876e
PZS-ModelCloud <124652791+PZS-ModelCloud@users.noreply.github.com>
2024-10-28 17:45:21 +0800

    add internlm test (#491)
    * add internlm test
    
    * update comment

--- tests/models/test_model_internlm2_5.py
+++ tests/models/test_model_internlm2_5.py
@@ -4,8 +4,9 @@ class TestInternlm2_5(ModelTest):
     NATIVE_MODEL_ID = "internlm/internlm2_5-1_8b-chat"
 
     def test_internlm2_5(self):
+        # transformers<=4.44.2 run normal
         model, tokenizer = self.quantModel(self.NATIVE_MODEL_ID, trust_remote_code=True)
-        reference_output = "<|begin_of_text|>I am in Paris and I am so excited to be here! The city of love, art, fashion, and food. I have been dreaming of visiting Paris for years, and now that I am finally here, I am determined to make the most of my time. I have a list of all the must-see sights, but I also want to explore the city like a local. I want to discover the hidden gems, the secret spots, and the authentic experiences that only a local would know about."
+        reference_output = "<s>I am in Paris and I want to visit the Eiffel Tower. I have a 3 day trip to Paris. I want to visit the Eiffel Tower, the Louvre Museum, and the Notre Dame Cathedral. I have a budget of $100 per day. I can stay in hotels or hostels. I can also use public transportation.I have a good idea of how to get around Paris. I will stay in a hotel or hostel for the first two nights. I will stay in a hotel"
         result = self.generate(model, tokenizer)
 
         self.assertEqual(result[:self.GENERATE_EVAL_SIZE], reference_output[:self.GENERATE_EVAL_SIZE])
