1fad2bd0da5766d55814508cf4459e8148508b2b
PZS-Modelcloud <pzs@modelcloud.ai>
2024-11-05 05:57:29 +0000

    merge code

--- tests/models/test_gemma.py
+++ tests/models/test_gemma.py
@@ -4,6 +4,7 @@ class TestGemma(ModelTest):
     NATIVE_MODEL_ID = "google/gemma-2-9b"
     NATIVE_ARC_CHALLENGE_ACC = 0.6143
     NATIVE_ARC_CHALLENGE_ACC_NORM = 0.6553
+
     def test_gemma(self):
         self.quant_lm_eval()
 
--- tests/models/test_llama2.py
+++ tests/models/test_llama2.py
@@ -5,6 +5,7 @@ class TestLlama2(ModelTest):
     NATIVE_ARC_CHALLENGE_ACC = 0.3490
     NATIVE_ARC_CHALLENGE_ACC_NORM = 0.3635
     APPLY_CHAT_TEMPLATE = True
+    QUANT_ARC_MAX_POSITIVE_DELTA = 23
 
     def test_llama2(self):
         self.quant_lm_eval()
