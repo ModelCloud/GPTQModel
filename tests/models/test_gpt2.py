4d366313091ba10e470c4d2e75b62bc06a0c195e
PZS-Modelcloud <pzs@modelcloud.ai>
2024-11-05 10:46:40 +0000

    fix value

--- tests/models/test_gpt2.py
+++ tests/models/test_gpt2.py
@@ -4,7 +4,7 @@ from model_test import ModelTest  # noqa: E402
 
 class TestGpt2(ModelTest):
     NATIVE_MODEL_ID = "openai-community/gpt2"
-    NATIVE_ARC_CHALLENGE_ACC = 0.2270
+    NATIVE_ARC_CHALLENGE_ACC = 0.1903
     NATIVE_ARC_CHALLENGE_ACC_NORM = 0.2270
     TORCH_DTYPE = torch.float16
     TRUST_REMOTE_CODE = True
