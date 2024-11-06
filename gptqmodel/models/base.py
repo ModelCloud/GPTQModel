86c53d5c880723b603509cf80c72487df4ceb3f9
ZYC <zyc@modelcloud.ai>
2024-11-06 08:03:41 +0000

    add save() method

--- gptqmodel/models/base.py
+++ gptqmodel/models/base.py
@@ -620,6 +620,19 @@ class BaseGPTQModel(nn.Module):
         """shortcut for model.prepare_inputs_for_generation"""
         return self.model.prepare_inputs_for_generation(*args, **kwargs)
 
+    def save(
+            self,
+            save_dir: str,
+            safetensors_metadata: Optional[Dict[str, str]] = None,
+            use_safetensors: bool = True,
+            max_shard_size: Optional[str] = None,
+            **kwargs,
+    ):
+        if self.quantized:
+            self.save_quantized(save_dir, safetensors_metadata, use_safetensors, max_shard_size)
+        else:
+            self.save_pretrained(save_dir, **kwargs)
+            
     def save_quantized(
         self,
         save_dir: str,
