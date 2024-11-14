183b9f452af3151d1d756ec196e684b6f201e1cb
Qubitium <Qubitium@modelcloud.ai>
2024-11-14 23:33:32 +0000

    add file

---
+++ tests/models/testglm.py
@@ -0,0 +1,9 @@
+from model_test import ModelTest  # noqa: E402
+
+
+class TestGlm(ModelTest):
+    # real: THUDM/glm-4-9b-chat-hf
+    NATIVE_MODEL_ID = "/monster/data/model/glm-4-9b-chat-hf"
+
+    def test_glm(self):
+        self.quant_lm_eval()
