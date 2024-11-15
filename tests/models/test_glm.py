c404ad092ad033615c1d401f7e6f0ad495c0988f
ZYC-Modelcloud <zyc@modelcloud.ai>
2024-11-15 14:56:20 +0800

    add model glm native score

--- tests/models/test_glm.py
+++ tests/models/test_glm.py
@@ -4,6 +4,8 @@ from model_test import ModelTest  # noqa: E402
 class TestGlm(ModelTest):
     # real: THUDM/glm-4-9b-chat-hf
     NATIVE_MODEL_ID = "/monster/data/model/glm-4-9b-chat-hf"
+    NATIVE_ARC_CHALLENGE_ACC = 0.5154
+    NATIVE_ARC_CHALLENGE_ACC_NORM = 0.5316
     USE_VLLM = False
 
     def test_glm(self):
