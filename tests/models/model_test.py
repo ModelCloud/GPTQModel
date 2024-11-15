21766cb882f50a274e38af639119bf6b33daaec3
ZYC-ModelCloud <165365217+ZYC-ModelCloud@users.noreply.github.com>
2024-11-15 16:05:01 +0800

    unit tests add args USE_VLLM (#582)
    * add args default use vllm
    
    * use hf if not use vllm
    
    * modify model_args
    
    * remove args
    
    * mod clean up
    
    * add model glm native score
    
    ---------
    
    Co-authored-by: PZS-ModelCloud <pzs@modelcloud.ai>

--- tests/models/model_test.py
+++ tests/models/model_test.py
@@ -27,6 +27,7 @@ class ModelTest(unittest.TestCase):
     APPLY_CHAT_TEMPLATE = False
     TORCH_DTYPE = "auto"
     BATCH_SIZE = "auto"
+    USE_VLLM = True
 
     def generate(self, model, tokenizer, prompt=None):
         if prompt is None:
@@ -121,15 +122,20 @@ class ModelTest(unittest.TestCase):
 
     def lm_eval(self, model, apply_chat_template=False, trust_remote_code=False):
         with tempfile.TemporaryDirectory() as tmp_dir:
+            if self.USE_VLLM:
+                model_args = f"pretrained={model.model_id_or_path},dtype=auto,gpu_memory_utilization=0.8,tensor_parallel_size=1,trust_remote_code={trust_remote_code}"
+            else:
+                model_args = ""
             results = model.lm_eval(
-                model="vllm",
-                model_args=f"pretrained={model.model_id_or_path},dtype=auto,gpu_memory_utilization=0.8,tensor_parallel_size=1,trust_remote_code={trust_remote_code}",
+                model="vllm" if self.USE_VLLM else "hf",
+                model_args=model_args,
                 output_path=tmp_dir,
                 tasks=self.TASK_NAME,
                 apply_chat_template=apply_chat_template,
                 trust_remote_code=trust_remote_code,
                 batch_size=self.BATCH_SIZE,
             )
+
             print('--------Eval Result---------')
             print(make_table(results))
             if "groups" in results:
