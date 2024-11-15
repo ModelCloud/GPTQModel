1a8ebf866a773a91d3d20b854f84a39e3b475654
LRL-ModelCloud <lrl@modelcloud.ai>
2024-11-15 09:35:44 +0800

    fix layer_modules

--- gptqmodel/models/definitions/glm.py
+++ gptqmodel/models/definitions/glm.py
@@ -10,6 +10,6 @@ class GLM(BaseGPTQModel):
     layer_modules = [
         ["self_attn.k_proj", "self_attn.v_proj", "self_attn.q_proj"],
         ["self_attn.o_proj"],
-        ["mlp.up_proj", "mlp.gate_proj"],
+        ["mlp.gate_up_proj"],
         ["mlp.down_proj"],
     ]
