a23a5968686058e1fea425ce788c32e63c343faa
ZX-ModelCloud <165115237+ZX-ModelCloud@users.noreply.github.com>
2024-11-19 23:45:47 +0800

    Some models put hidden_states in kwargs instead of args. (#621)

--- gptqmodel/models/base.py
+++ gptqmodel/models/base.py
@@ -389,6 +389,12 @@ class BaseGPTQModel(nn.Module):
             layer_input = []
             for inp in args:
                 layer_input.append(move_to(inp, data_device))
+            if len(layer_input) == 0:
+                # Some models put hidden_states in kwargs instead of args.
+                # For example, gptj ...
+                if kwargs.get("hidden_states") is not None:
+                    layer_input.append(move_to(kwargs["hidden_states"], data_device))
+
             layer_inputs.append(layer_input)
 
             # Keyword arguments.
