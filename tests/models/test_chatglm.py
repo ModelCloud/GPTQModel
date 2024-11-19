150c08648150ef6753f3c6b3669319227247136a
LRL-ModelCloud <lrl@modelcloud.ai>
2024-11-19 20:09:17 +0800

    fixed test_chatglm.py

--- tests/models/test_chatglm.py
+++ tests/models/test_chatglm.py
@@ -3,6 +3,8 @@ from model_test import ModelTest  # noqa: E402
 
 class TestChatGlm(ModelTest):
     NATIVE_MODEL_ID = "/monster/data/model/chatglm3-6b" # "THUDM/chatglm3-6b"
+    NATIVE_ARC_CHALLENGE_ACC = 0.3319
+    NATIVE_ARC_CHALLENGE_ACC_NORM = 0.3729
     TRUST_REMOTE_CODE = True
 
     def test_chatglm(self):
