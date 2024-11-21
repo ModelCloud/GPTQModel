1ecda2e6bec11e18c59fee04bb017d66bf2bb1c1
LRL-ModelCloud <lrl@modelcloud.ai>
2024-11-21 14:11:32 +0800

    up score

--- tests/models/test_qwen2_5.py
+++ tests/models/test_qwen2_5.py
@@ -4,8 +4,8 @@ from model_test import ModelTest  # noqa: E402
 class TestQwen2_5(ModelTest):
     NATIVE_MODEL_ID = "/monster/data/model/Qwen2.5-0.5B-Instruct"
     QUANT_ARC_MAX_NEGATIVE_DELTA = 0.2
-    NATIVE_ARC_CHALLENGE_ACC = 0.2338
-    NATIVE_ARC_CHALLENGE_ACC_NORM = 0.2372
+    NATIVE_ARC_CHALLENGE_ACC = 0.2739
+    NATIVE_ARC_CHALLENGE_ACC_NORM = 0.3055
     TRUST_REMOTE_CODE = False
     APPLY_CHAT_TEMPLATE = True
     BATCH_SIZE = 6
