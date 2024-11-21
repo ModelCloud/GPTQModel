e00e6a1b1be6415ac9ced36f8f5dcc294e4e9859
CSY <csy@modelcloud.ai>
2024-11-21 13:42:34 +0800

    skip test_deepseekv2_lite

--- tests/models/test_deepseekv2_lite.py
+++ tests/models/test_deepseekv2_lite.py
@@ -1,6 +1,7 @@
 from model_test import ModelTest  # noqa: E402
 
 
+# TODO, CPU memory is not enough for this model. skip it for now
 class TestDeepseekV2Lite(ModelTest):
     NATIVE_MODEL_ID = "/monster/data/model/DeepSeek-Coder-V2-Lite-Instruct" # "deepseek-ai/DeepSeek-Coder-V2-Lite-Instruct"
     NATIVE_ARC_CHALLENGE_ACC = 0.4753
