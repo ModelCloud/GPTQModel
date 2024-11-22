3d198150895a7c718ca9542dc54db0be2a39ec8f
CSY-ModelCloud <csy@modelcloud.ai>
2024-11-22 10:35:02 +0800

    disable chat for mpt (#641)

--- tests/models/test_mpt.py
+++ tests/models/test_mpt.py
@@ -5,7 +5,7 @@ class TestMpt(ModelTest):
     NATIVE_MODEL_ID = "/monster/data/model/mpt-7b-instruct" # "mosaicml/mpt-7b-instruct"
     NATIVE_ARC_CHALLENGE_ACC = 0.4275
     NATIVE_ARC_CHALLENGE_ACC_NORM = 0.4454
-    APPLY_CHAT_TEMPLATE = True
+    APPLY_CHAT_TEMPLATE = False
     TRUST_REMOTE_CODE = False
     BATCH_SIZE = 6
 
