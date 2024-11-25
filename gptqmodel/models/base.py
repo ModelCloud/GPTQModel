913ba172911a6b274743540d1afecb57b7fedfc8
LRL-ModelCloud <lrl@modelcloud.ai>
2024-11-25 11:55:37 +0800

    fix kv_last_layer

--- gptqmodel/models/base.py
+++ gptqmodel/models/base.py
@@ -546,16 +546,15 @@ class BaseGPTQModel(nn.Module):
                         additional_layer_inputs["position_ids"] = layer_position_ids
                     for k, v in layer_input_kwargs[j].items():
                         additional_layer_inputs[k] = nested_move_to(v, cur_layer_device)
+
+                    if hasattr(layer, "reuse_kv"):
+                        if layer.reuse_kv:
+                            additional_layer_inputs["kv_last_layer"] = shared_kv_cache_dict.get(i-1)
+
                     with torch.no_grad():
-                        if hasattr(layer, "reuse_kv"):
-                            if layer.reuse_kv:
-                                layer(*layer_input, **additional_layer_inputs, kv_last_layer=shared_kv_cache_dict[-1])
-                            else:
-                                layer_output = layer(*layer_input, **additional_layer_inputs)
-                                if shared_kv_cache_dict.get(i) is None:
-                                    shared_kv_cache_dict[i] = layer_output[-1]
-                        else:
-                            layer(*layer_input, **additional_layer_inputs)
+                        layer_output = layer(*layer_input, **additional_layer_inputs)
+                        if shared_kv_cache_dict.get(i) is None:
+                            shared_kv_cache_dict[i] = layer_output[-1]
                 for h in handles:
                     h.remove()
 
