b452ad9ce80d72b3d73d1683ea4c8cf200d52c0b
LRL-ModelCloud <lrl@modelcloud.ai>
2024-11-25 21:23:02 +0800

    add quantized model support

--- examples/benchmark/ipex.py
+++ examples/benchmark/ipex.py
@@ -1,13 +1,20 @@
 import time
 import torch
 import argparse
-from transformers import AutoTokenizer
-from transformers import AutoModelForCausalLM
+from datasets import load_dataset
+from transformers import AutoTokenizer, AutoConfig, AutoModelForCausalLM
+from gptqmodel import GPTQModel, BACKEND
+
+def prepare_dataset_for_bench(tokenizer, batch_size=8):
+    dataset = load_dataset("json", data_files="prompts.jsonl", split="train")[:batch_size]
+    prompts = [[{"role": "user", "content": data}] for data in dataset['input']]
+    input_tensors = tokenizer.apply_chat_template(prompts, add_generation_prompt=True, return_tensors="pt", padding=True)
+    return input_tensors
 
 parser = argparse.ArgumentParser(description="Benchmark IPEX vs HF on a pre-trained model.")
 parser.add_argument("--model", type=str, required=True, help="Path or name of the pre-trained model.")
 parser.add_argument("--cores", type=int, default=8, help="Number of CPU cores to use.")
-parser.add_argument("--batch", type=int, default=2, help="Batch size for processing messages.")
+parser.add_argument("--batch", type=int, default=8, help="Batch size for processing messages.")
 parser.add_argument("--backend", type=str, choices=["ipex", "hf"], default="ipex", help="Backend to optimize the model. Choose between 'ipex' and 'hf'.")
 ars = parser.parse_args()
 
@@ -19,55 +26,47 @@ print("use backend: ", ars.backend)
 # Set the number of threads to use
 torch.set_num_threads(ars.cores)
 
-tokenizer = AutoTokenizer.from_pretrained(ars.model)
-model = AutoModelForCausalLM.from_pretrained(ars.model, device_map="cpu", torch_dtype=torch.bfloat16)
+# load model, check model backend
+config = AutoConfig.from_pretrained(ars.model)
+is_quantized_model = hasattr(config, "quantization_config")
+if is_quantized_model:
+    print("load quantized model, will use BACKEND.IPEX")
+    model = GPTQModel.load(ars.model, backend=BACKEND.IPEX)
+    model.to(torch.device("cpu"))
+else:
+    model = AutoModelForCausalLM.from_pretrained(ars.model, device_map="cpu", torch_dtype=torch.bfloat16)
+
+# set model to eval mode
 model.eval()
 
-if ars.backend == "ipex":
+if ars.backend == "ipex" and not is_quantized_model:
     import intel_extension_for_pytorch as ipex
     model = ipex.llm.optimize(model, dtype=torch.bfloat16)
+    model.forward = torch.compile(model.forward, dynamic=True, backend="ipex")
 
+# load tokenizer, and normalize the pad_token_id
+tokenizer = AutoTokenizer.from_pretrained(ars.model)
 tokenizer.pad_token_id = 128004 # <|finetune_right_pad_id|>
 model.pad_token_id = tokenizer.pad_token_id
 
-messages = [
-    {"role": "user", "content": "How can I effectively learn programming?"},
-    {"role": "user", "content": "How to choose and care for different types of fabrics?"},
-    {"role": "user", "content": "How do creation myths vary across religions?"},
-    {"role": "user", "content": "Name a traditional dance from Greece."},
-    {"role": "user", "content": "Describe the process of cellular respiration."},
-    {"role": "user", "content": "How has mobile gaming changed the industry?"},
-    {"role": "user", "content": "What are the benefits of using a standing desk?"},
-    {"role": "user", "content": "Provide a detailed explanation of how multiplayer netcode works in online games?"},
-    {"role": "user", "content": "What are the key differences between renewable and non-renewable energy sources?"},
-    {"role": "user", "content": "How can I improve my time management skills effectively?"},
-    {"role": "user", "content": "What are the steps involved in baking a perfect sourdough bread?"},
-    {"role": "user", "content": "Explain the significance of the Renaissance period in European history."},
-    {"role": "user", "content": "What are the primary causes and effects of deforestation worldwide?"},
-    {"role": "user", "content": "How do plants adapt to survive in extreme desert environments?"},
-    {"role": "user", "content": "What strategies can help reduce stress during exam preparation?"},
-    {"role": "user", "content": "Describe the cultural importance of tea ceremonies in Japan."},
-]
+# prepare dataset for benchmark
+input_ids = prepare_dataset_for_bench(tokenizer, ars.batch)
 
-if ars.batch > len(messages):
-    raise ValueError("batch_size should be less than or equal to the number of messages")
-
-messages = [[messages[i]] for i in range(ars.batch)]
-
+# benchmark
 with torch.no_grad(), torch.amp.autocast("cpu", dtype=torch.bfloat16):
     start = time.time()
     new_token_len = 0
-    input_tensor = tokenizer.apply_chat_template(messages, add_generation_prompt=True, return_tensors="pt", padding=True)
-
-    outputs = model.generate(input_ids=input_tensor.to(model.device), max_new_tokens=512, pad_token_id=tokenizer.pad_token_id)
+    outputs = model.generate(input_ids=input_ids.to(model.device), max_new_tokens=512, pad_token_id=tokenizer.pad_token_id)
     for i, output in enumerate(outputs):
-        new_token_len += len(output) - len(input_tensor[i])
+        new_token_len += len(output) - len(input_ids[i])
         # debug print
         # result = tokenizer.decode(output, skip_special_tokens=False)
         # print(result)
         # print("="*50)
 
     total_time = time.time() - start
-    print(f"generate use :{total_time}")
-    print(f"total new token: {new_token_len}")
-    print(f"token/sec: {(new_token_len/total_time):.4f}")
+
+# display benchmark result
+print(f"generate use :{total_time}")
+print(f"total new token: {new_token_len}")
+print(f"token/sec: {(new_token_len/total_time):.4f}")
