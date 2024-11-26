4914a17349cbc7e2498f11ac847971c3c920efda
LRL-ModelCloud <lrl@modelcloud.ai>
2024-11-26 16:53:39 +0800

    Set the "OMP_NUM_THREADS" environment variable to the specified number of cores to control the number of threads used by OpenMP

--- examples/benchmark/ipex.py
+++ examples/benchmark/ipex.py
@@ -1,15 +1,7 @@
+import os
 import time
-import torch
 import argparse
-from datasets import load_dataset
-from transformers import AutoTokenizer, AutoConfig, AutoModelForCausalLM
-from gptqmodel import GPTQModel, BACKEND
 
-def prepare_dataset_for_bench(tokenizer, batch_size=8):
-    dataset = load_dataset("json", data_files="prompts.jsonl", split="train")[:batch_size]
-    prompts = [[{"role": "user", "content": data}] for data in dataset['input']]
-    input_tensors = tokenizer.apply_chat_template(prompts, add_generation_prompt=True, return_tensors="pt", padding=True)
-    return input_tensors
 
 parser = argparse.ArgumentParser(description="Benchmark IPEX vs HF on a pre-trained model.")
 parser.add_argument("--model", type=str, required=True, help="Path or name of the pre-trained model.")
@@ -23,33 +15,56 @@ print("use cores: ", ars.cores)
 print("use batch: ", ars.batch)
 print("use backend: ", ars.backend)
 
-# Set the number of threads to use
+# Set the "OMP_NUM_THREADS" environment variable to the specified number of cores to control the number of threads used by OpenMP
+os.environ["OMP_NUM_THREADS"] = str(ars.cores)
+
+import torch
+# Set the number of threads used by PyTorch
 torch.set_num_threads(ars.cores)
 
+
+def prepare_dataset_for_bench(tokenizer, batch_size=8):
+    from datasets import load_dataset
+
+    dataset = load_dataset("json", data_files="prompts.jsonl", split="train")[:batch_size]
+    prompts = [[{"role": "user", "content": data}] for data in dataset['input']]
+    input_tensors = tokenizer.apply_chat_template(prompts, add_generation_prompt=True, return_tensors="pt", padding=True)
+    print(input_tensors.shape)
+    return input_tensors
+
+
+from transformers import AutoTokenizer, AutoConfig, AutoModelForCausalLM
 # load model, check model backend
 config = AutoConfig.from_pretrained(ars.model)
 is_quantized_model = hasattr(config, "quantization_config")
 if is_quantized_model:
+    from gptqmodel import GPTQModel, BACKEND
+
     print("load quantized model, will use BACKEND.IPEX")
     model = GPTQModel.load(ars.model, backend=BACKEND.IPEX)
 else:
     model = AutoModelForCausalLM.from_pretrained(ars.model, device_map="cpu", torch_dtype=torch.bfloat16)
 
+
 # set model to eval mode
 model.eval()
+
 
 if ars.backend == "ipex" and not is_quantized_model:
     import intel_extension_for_pytorch as ipex
     model = ipex.llm.optimize(model, dtype=torch.bfloat16)
-    model.forward = torch.compile(model.forward, dynamic=True, backend="ipex")
+    # model.forward = torch.compile(model.forward, dynamic=True, backend="ipex")
+
 
 # load tokenizer, and normalize the pad_token_id
 tokenizer = AutoTokenizer.from_pretrained(ars.model)
 tokenizer.pad_token_id = 128004 # <|finetune_right_pad_id|>
 model.pad_token_id = tokenizer.pad_token_id
 
+
 # prepare dataset for benchmark
 input_ids = prepare_dataset_for_bench(tokenizer, ars.batch)
+
 
 # benchmark
 with torch.no_grad(), torch.amp.autocast("cpu", dtype=torch.bfloat16):
@@ -64,6 +79,7 @@ with torch.no_grad(), torch.amp.autocast("cpu", dtype=torch.bfloat16):
         # print("="*50)
 
     total_time = time.time() - start
+
 
 # display benchmark result
 print(f"generate use :{total_time}")
