8b3ecd0b9f45dc6bc992899de0fe37cc84ae1fab
CSY <csy@modelcloud.ai>
2024-11-28 14:55:37 +0800

    use local model path

--- tests/test_q4_cuda.py
+++ tests/test_q4_cuda.py
@@ -35,7 +35,7 @@ class TestsQ4CUDA(unittest.TestCase):
             reference_output = "<s> I am in Paris and I am in love with you.\n\nScene 2:\n\n(The stage is now dark, but the audience can see the characters walking around the stage.)\n\n(The stage is now lit up, but the audience can only see the characters' silhouettes.)\n\n("
             new_tokens = 60
 
-        model_id = "LnL-AI/TinyLlama-1.1B-Chat-v1.0-GPTQ-4bit"
+        model_id = "/monster/data/model/TinyLlama-1.1B-Chat-v1.0-GPTQ-4bit"
         revision = "desc_act_true"
 
         model_q = GPTQModel.from_quantized(
@@ -83,7 +83,7 @@ class TestsQ4CUDA(unittest.TestCase):
             reference_output = "<s> I am in Paris and I am in love with you.\n\nScene 2:\n\n(The stage is now dark, but the audience can see the characters walking around the stage.)\n\n(The stage is now lit up, but the audience can see the characters walking around the stage.)\n\n(The"
             new_tokens = 60
 
-        model_id = "LnL-AI/TinyLlama-1.1B-Chat-v1.0-GPTQ-4bit"
+        model_id = "/monster/data/model/TinyLlama-1.1B-Chat-v1.0-GPTQ-4bit"
 
         model_q = GPTQModel.from_quantized(
             model_id,
