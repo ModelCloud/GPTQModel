712d869629399e6cbea431b83560f96d1a272dbc
CSY <csy@modelcloud.ai>
2024-11-28 14:59:03 +0800

    fix ruff

--- tests/test_q4_cuda.py
+++ tests/test_q4_cuda.py
@@ -7,8 +7,7 @@ os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 import unittest  # noqa: E402
 
 import torch  # noqa: E402
-from gptqmodel import BACKEND  # noqa: E402
-from gptqmodel import GPTQModel  # noqa: E402
+from gptqmodel import BACKEND, GPTQModel  # noqa: E402
 from parameterized import parameterized  # noqa: E402
 from transformers import AutoTokenizer  # noqa: E402
 
