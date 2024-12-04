ba469c05ddcd7e5db92f6c79a60470e843ea6051
CSY <csy@modelcloud.ai>
2024-12-04 15:34:17 +0800

    add base score

--- tests/test_asym_gptq_v1.py
+++ tests/test_asym_gptq_v1.py
@@ -12,6 +12,8 @@ class Test(ModelTest):
     NATIVE_MODEL_ID = "/monster/data/model/Llama-3.2-1B-Instruct"  # "meta-llama/Llama-3.2-1B-Instruct"
     QUANT_FORMAT = FORMAT.GPTQ
     SYM = False
+    NATIVE_ARC_CHALLENGE_ACC = 0.2747
+    NATIVE_ARC_CHALLENGE_ACC_NORM = 0.2935
 
     def test(self):
         self.quant_lm_eval()
