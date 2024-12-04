1935669d257c381e157c1771d75fbaf6a90a3603
CSY-ModelCloud <csy@modelcloud.ai>
2024-12-04 18:13:31 +0800

    revert scores (#746)

--- tests/models/test_exaone.py
+++ tests/models/test_exaone.py
@@ -3,8 +3,8 @@ from model_test import ModelTest
 
 class TestExaone(ModelTest):
     NATIVE_MODEL_ID = "/monster/data/model/EXAONE-3.0-7.8B-Instruct" # "LGAI-EXAONE/EXAONE-3.0-7.8B-Instruct"
-    NATIVE_ARC_CHALLENGE_ACC = 0.46
-    NATIVE_ARC_CHALLENGE_ACC_NORM = 0.46
+    NATIVE_ARC_CHALLENGE_ACC = 0.4232
+    NATIVE_ARC_CHALLENGE_ACC_NORM = 0.4164
     TRUST_REMOTE_CODE = True
     BATCH_SIZE = 6
 
