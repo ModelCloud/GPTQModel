90a4fcf2fdfc255da5cde20ee444b8942b1bd847
CSY-ModelCloud <csy@modelcloud.ai>
2024-12-04 18:11:34 +0800

    revert scores

--- tests/test_asym_gptq_v1.py
+++ tests/test_asym_gptq_v1.py
@@ -10,10 +10,11 @@ from models.model_test import ModelTest  # noqa: E402
 
 class Test(ModelTest):
     NATIVE_MODEL_ID = "/monster/data/model/Llama-3.2-1B-Instruct"  # "meta-llama/Llama-3.2-1B-Instruct"
+    NATIVE_ARC_CHALLENGE_ACC = 0.3567
+    NATIVE_ARC_CHALLENGE_ACC_NORM = 0.3805
+    QUANT_ARC_MAX_NEGATIVE_DELTA = 0.36
     QUANT_FORMAT = FORMAT.GPTQ
     SYM = False
-    NATIVE_ARC_CHALLENGE_ACC = 0.2747
-    NATIVE_ARC_CHALLENGE_ACC_NORM = 0.2935
 
     def test(self):
         self.quant_lm_eval()
