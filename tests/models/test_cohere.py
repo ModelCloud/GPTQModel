eda8706fcc01f94168a554599c2592cd93d40283
CSY <csy@modelcloud.ai>
2024-12-04 20:20:36 +0800

    update cohere delta

--- tests/models/test_cohere.py
+++ tests/models/test_cohere.py
@@ -5,7 +5,7 @@ class TestCohere(ModelTest):
     NATIVE_MODEL_ID = "/monster/data/model/aya-expanse-8b" # "CohereForAI/aya-expanse-8b"
     NATIVE_ARC_CHALLENGE_ACC = 0.5401
     NATIVE_ARC_CHALLENGE_ACC_NORM = 0.5640
-    QUANT_ARC_MAX_NEGATIVE_DELTA = 0.12
+    QUANT_ARC_MAX_NEGATIVE_DELTA = 0.15
     BATCH_SIZE = 4
 
     def test_cohere(self):
