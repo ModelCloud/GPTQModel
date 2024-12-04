f5b26f8d16de6a7b8312567c1b725122a023afaa
CSY <csy@modelcloud.ai>
2024-12-04 20:21:17 +0800

    update longllama delta

--- tests/models/test_longllama.py
+++ tests/models/test_longllama.py
@@ -6,7 +6,7 @@ class TestLongLlama(ModelTest):
     NATIVE_ARC_CHALLENGE_ACC = 0.3515
     NATIVE_ARC_CHALLENGE_ACC_NORM = 0.3652
     TRUST_REMOTE_CODE = True
-    QUANT_ARC_MAX_NEGATIVE_DELTA = 0.4
+    QUANT_ARC_MAX_NEGATIVE_DELTA = 0.5
     USE_VLLM = False
 
     def test_longllama(self):
