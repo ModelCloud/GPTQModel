01fb9eeedcc166cf3aa460cbbd09a1fcacb2e31d
CSY <csy@modelcloud.ai>
2024-12-04 16:34:58 +0800

    revert to normal test

--- tests/test_asym_gptq_v1.py
+++ tests/test_asym_gptq_v1.py
@@ -3,10 +3,8 @@ import os
 
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 # -- end do not touch
-import tempfile  # noqa: E402
 from parameterized import parameterized  # noqa: E402
 
-from gptqmodel import QuantizeConfig, GPTQModel  # noqa: E402
 from gptqmodel.quantization import FORMAT  # noqa: E402
 
 from models.model_test import ModelTest  # noqa: E402
@@ -17,36 +15,14 @@ class Test(ModelTest):
     QUANT_FORMAT = FORMAT.GPTQ
     QUANT_ARC_MAX_NEGATIVE_DELTA = 0.2
 
-    @classmethod
-    def setUpClass(self):
-        self.tokenizer = self.load_tokenizer(self.NATIVE_MODEL_ID)
-        self.datas = self.load_dataset(self.tokenizer)
-
     @parameterized.expand([True, False])
     def test(self, sym: bool):
+        self.SYM = sym
         if sym:
             self.NATIVE_ARC_CHALLENGE_ACC = 0.2269
             self.NATIVE_ARC_CHALLENGE_ACC_NORM = 0.2269
         else:
             self.NATIVE_ARC_CHALLENGE_ACC = 0.2747
             self.NATIVE_ARC_CHALLENGE_ACC_NORM = 0.2935
-        quantize_config = QuantizeConfig(
-            format=self.QUANT_FORMAT,
-            desc_act=self.DESC_ACT,
-            sym=sym,
-        )
-        model = GPTQModel.load(
-            self.NATIVE_MODEL_ID,
-            quantize_config=quantize_config,
-            device_map="auto",
-        )
 
-        model.quantize(self.datas)
-
-        with (tempfile.TemporaryDirectory()) as tmpdirname:
-            model.save(tmpdirname)
-            self.tokenizer.save_pretrained(tmpdirname)
-            model, tokenizer = self.loadQuantModel(tmpdirname)
-
-            task_results = self.lm_eval(model=model)
-            self.check_results(task_results)
+        self.quant_lm_eval()
