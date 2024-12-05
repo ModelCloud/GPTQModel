936f98307d10144eda085160c13a82aeee119bbb
CSY <csy@modelcloud.ai>
2024-12-05 16:08:41 +0800

    update inference kernel

--- tests/models/test_opt.py
+++ tests/models/test_opt.py
@@ -2,12 +2,12 @@ from model_test import ModelTest
 
 
 class TestOpt(ModelTest):
-    NATIVE_MODEL_ID = "/monster/data/model/opt-125m" # "facebook/opt-125m"
+    NATIVE_MODEL_ID = "/monster/data/model/opt-125m"  # "facebook/opt-125m"
     NATIVE_ARC_CHALLENGE_ACC = 0.1894
     NATIVE_ARC_CHALLENGE_ACC_NORM = 0.2278
 
-    KERNEL_QUANT = set(["OPTDecoder", "ReLU", "MarlinQuantLinear", "Linear", "OPTAttention", "ModuleList", "OPTForCausalLM", "OPTDecoderLayer", "OPTModel", "OPTLearnedPositionalEmbedding", "Embedding", "OPTGPTQ", "LayerNorm"])
-    KERNEL_INFERENCE = KERNEL_QUANT
+    KERNEL_QUANT = {"OPTDecoder", "ReLU", "MarlinQuantLinear", "Linear", "OPTAttention", "ModuleList", "OPTForCausalLM", "OPTDecoderLayer", "OPTModel", "OPTLearnedPositionalEmbedding", "Embedding", "OPTGPTQ", "LayerNorm"}
+    KERNEL_INFERENCE = {'Embedding', 'LayerNorm', 'Linear', 'MarlinQuantLinear', 'ModuleList', 'OPTAttention', 'OPTDecoder', 'OPTDecoderLayer', 'OPTForCausalLM', 'OPTGPTQ', 'OPTLearnedPositionalEmbedding', 'OPTModel', 'ReLU'}
 
     def test_opt(self):
         self.quant_lm_eval()
