063cbe29ea74a72dfa0662efcff70f1280c1f2d6
CSY <csy@modelcloud.ai>
2024-12-05 16:12:16 +0800

    update quant kernel

--- tests/models/test_opt.py
+++ tests/models/test_opt.py
@@ -6,7 +6,7 @@ class TestOpt(ModelTest):
     NATIVE_ARC_CHALLENGE_ACC = 0.1894
     NATIVE_ARC_CHALLENGE_ACC_NORM = 0.2278
 
-    KERNEL_QUANT = {"OPTDecoder", "ReLU", "MarlinQuantLinear", "Linear", "OPTAttention", "ModuleList", "OPTForCausalLM", "OPTDecoderLayer", "OPTModel", "OPTLearnedPositionalEmbedding", "Embedding", "OPTGPTQ", "LayerNorm"}
+    KERNEL_QUANT = {'Embedding', 'ExllamaQuantLinear', 'LayerNorm', 'Linear', 'ModuleList', 'OPTDecoder', 'OPTDecoderLayer', 'OPTForCausalLM', 'OPTGPTQ', 'OPTLearnedPositionalEmbedding', 'OPTModel', 'OPTSdpaAttention', 'ReLU'}
     KERNEL_INFERENCE = {'Embedding', 'LayerNorm', 'Linear', 'MarlinQuantLinear', 'ModuleList', 'OPTAttention', 'OPTDecoder', 'OPTDecoderLayer', 'OPTForCausalLM', 'OPTGPTQ', 'OPTLearnedPositionalEmbedding', 'OPTModel', 'ReLU'}
 
     def test_opt(self):
