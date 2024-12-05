a435388f4644db7446f241cb3a1280b06421b255
CSY <csy@modelcloud.ai>
2024-12-05 20:21:31 +0800

    do lm eval

--- tests/test_lm_head.py
+++ tests/test_lm_head.py
@@ -4,19 +4,20 @@ import os
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 # -- end do not touch
 
-import unittest  # noqa: E402
-
 from gptqmodel import GPTQModel  # noqa: E402
 from gptqmodel.nn_modules.qlinear import BaseQuantLinear  # noqa: E402
+from models.model_test import ModelTest  # noqa: E402
 
 
-class TestLmHead(unittest.TestCase):
-    MODEL_ID = "/monster/data/model/TinyLlama-1.1B-intermediate-step-1341k-3T-autoround-lm_head-symFalse" # "LnL-AI/TinyLlama-1.1B-intermediate-step-1341k-3T-autoround-lm_head-symFalse"
+class TestLmHead(ModelTest):
+    NATIVE_MODEL_ID = "/monster/data/model/TinyLlama-1.1B-intermediate-step-1341k-3T-autoround-lm_head-symFalse"  # "LnL-AI/TinyLlama-1.1B-intermediate-step-1341k-3T-autoround-lm_head-symFalse"
     DEVICE = "cuda:0"
 
     def test_load(self):
-
-        model = GPTQModel.load(self.MODEL_ID, device=self.DEVICE)
+        model = GPTQModel.load(self.NATIVE_MODEL_ID, device=self.DEVICE)
 
         # validate lm_head is loaded as quantized layer
         assert isinstance(model.model.lm_head, BaseQuantLinear)
+
+    def test_eval(self):
+        self.quant_lm_eval()
