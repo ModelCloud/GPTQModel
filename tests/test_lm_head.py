ec582db987e457fd4dd8c30aae08a6281f03cab0
CSY <csy@modelcloud.ai>
2024-12-05 20:46:38 +0800

    update native score

--- tests/test_lm_head.py
+++ tests/test_lm_head.py
@@ -12,6 +12,9 @@ from models.model_test import ModelTest  # noqa: E402
 class TestLmHead(ModelTest):
     NATIVE_MODEL_ID = "/monster/data/model/TinyLlama-1.1B-intermediate-step-1341k-3T-autoround-lm_head-symFalse"  # "LnL-AI/TinyLlama-1.1B-intermediate-step-1341k-3T-autoround-lm_head-symFalse"
     DEVICE = "cuda:0"
+    NATIVE_ARC_CHALLENGE_ACC = 0.2799
+    NATIVE_ARC_CHALLENGE_ACC_NORM = 0.3046
+    QUANT_ARC_MAX_DELTA_FLOOR_PERCENT = 0.2
 
     def test_load(self):
         model = GPTQModel.load(self.NATIVE_MODEL_ID, device=self.DEVICE)
