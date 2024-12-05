85052aee69743b7dcc1c1ec0e72b934892014de1
LRL-ModelCloud <lrl@lbx.dev>
2024-12-05 15:45:32 +0800

    cleanup

--- gptqmodel/utils/backend.py
+++ gptqmodel/utils/backend.py
@@ -3,18 +3,17 @@ from enum import Enum
 
 class BACKEND(Enum):
     AUTO = 0  # choose the optimal local kernel based on quant_config compatibility
-    AUTO_CPU = 1 # choose the optimal cpu-only local kernel; for transformer/optimum compat
-    AUTO_TRAINABLE = 2 # choose the optimal trainable local kernel for post-quant training
-    CUDA = 3
-    TORCH = 4
-    TRITON = 5
-    EXLLAMA_V1 = 6
-    EXLLAMA_V2 = 7
-    MARLIN = 8
-    BITBLAS = 9
-    IPEX = 10
-    VLLM = 11 # external inference engine
-    SGLANG = 12 # external inference engine
+    AUTO_TRAINABLE = 1 # choose the optimal trainable local kernel for post-quant training
+    CUDA = 2
+    TORCH = 3
+    TRITON = 4
+    EXLLAMA_V1 = 5
+    EXLLAMA_V2 = 6
+    MARLIN = 7
+    BITBLAS = 8
+    IPEX = 9
+    VLLM = 10 # external inference engine
+    SGLANG = 11 # external inference engine
 
 def get_backend(backend: str):
     try:
