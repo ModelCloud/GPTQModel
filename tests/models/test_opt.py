ef5bae92dbb9c0ff371d79c7ceeb632228a8c122
CSY <csy@modelcloud.ai>
2024-12-05 18:36:40 +0800

    update type

--- tests/models/test_opt.py
+++ tests/models/test_opt.py
@@ -8,8 +8,8 @@ class TestOpt(ModelTest):
     NATIVE_ARC_CHALLENGE_ACC = 0.1894
     NATIVE_ARC_CHALLENGE_ACC_NORM = 0.2278
 
-    KERNEL_QUANT = set(backend_dict[BACKEND.EXLLAMA_V1])
-    KERNEL_INFERENCE = set(backend_dict[BACKEND.MARLIN])
+    KERNEL_QUANT = {backend_dict[BACKEND.EXLLAMA_V1]}
+    KERNEL_INFERENCE = {backend_dict[BACKEND.MARLIN]}
 
     def test_opt(self):
         self.quant_lm_eval()
