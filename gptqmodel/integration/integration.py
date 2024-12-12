2882e0e90d7dae6e2c37aadf4b19ce41b07bb373
CSY-ModelCloud <csy@modelcloud.ai>
2024-12-12 21:30:28 +0800

    fix ruff

--- gptqmodel/integration/integration.py
+++ gptqmodel/integration/integration.py
@@ -2,34 +2,40 @@ HAS_OPTIMUM = True
 try:
     import optimum.gptq as optimum_gptq
     from optimum.gptq import quantizer as optimum_quantizer
+    from optimum.utils import import_utils as optimum_import_utils
+    from optimum.utils import testing_utils as optimum_testing_utils
+
     from .src.optimum.gptq import quantizer as patched_optimum_quantizer
-    from optimum.utils import testing_utils as optimum_testing_utils, import_utils as optimum_import_utils
-    from .src.optimum.utils import testing_utils as patched_optimum_testing_utils, import_utils as patched_optimum_import_utils
+    from .src.optimum.utils import import_utils as patched_optimum_import_utils
+    from .src.optimum.utils import testing_utils as patched_optimum_testing_utils
 except BaseException:
     HAS_OPTIMUM = False
 
 HAS_PEFT = True
 try:
     from peft import import_utils as peft_import_utils
-    from .src.peft import import_utils as patched_peft_import_utils
     from peft.tuners.adalora.model import AdaLoraModel as peft_AdaLoraModel
-    from .src.peft.tuners.adalora.model import AdaLoraModel as patched_peft_AdaLoraModel
-    from peft.tuners.lora import gptq as peft_gptq, model as peft_model
-    from .src.peft.tuners.lora import gptq as patched_peft_gptq, model as patched_peft_model
+    from peft.tuners.lora import gptq as peft_gptq
+    from peft.tuners.lora import model as peft_model
     from peft.utils import other as peft_other
+
+    from .src.peft import import_utils as patched_peft_import_utils
+    from .src.peft.tuners.adalora.model import AdaLoraModel as patched_peft_AdaLoraModel
+    from .src.peft.tuners.lora import gptq as patched_peft_gptq
+    from .src.peft.tuners.lora import model as patched_peft_model
     from .src.peft.utils import other as patched_peft_other
 except BaseException:
     HAS_PEFT = False
 
+import transformers.testing_utils as transformers_testing_utils  # noqa: E402
 from transformers.quantizers import quantizer_gptq as transformers_quantizer_gptq  # noqa: E402
-from .src.transformers.quantizers import quantizer_gptq as patched_transformers_quantizer_gptq  # noqa: E402
 from transformers.utils import import_utils as transformers_import_utils  # noqa: E402
-from .src.transformers.utils import import_utils as patched_transformers_import_utils  # noqa: E402
 from transformers.utils import quantization_config as transformers_quantization_config  # noqa: E402
-from .src.transformers.utils import quantization_config as patched_transformers_quantization_config  # noqa: E402
-import transformers.testing_utils as transformers_testing_utils
-from .src.transformers import testing_utils as patched_transformers_testing_utils
 
+from .src.transformers import testing_utils as patched_transformers_testing_utils  # noqa: E402
+from .src.transformers.quantizers import quantizer_gptq as patched_transformers_quantizer_gptq  # noqa: E402
+from .src.transformers.utils import import_utils as patched_transformers_import_utils  # noqa: E402
+from .src.transformers.utils import quantization_config as patched_transformers_quantization_config  # noqa: E402
 
 
 def patch_hf():
