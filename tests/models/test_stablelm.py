b7ab5a760136edc1525d3fbf93a061634b3f99be
CSY-ModelCloud <csy@modelcloud.ai>
2024-12-13 22:02:46 +0800

    decrease Stablelm score delta & revert hymba requirements install in tests (#851)
    * revert hymba dep install
    
    * decrease Stablelm score delta

--- tests/models/test_stablelm.py
+++ tests/models/test_stablelm.py
@@ -5,6 +5,7 @@ class TestStablelm(ModelTest):
     NATIVE_MODEL_ID = "/monster/data/model/stablelm-base-alpha-3b"
     NATIVE_ARC_CHALLENGE_ACC = 0.2363
     NATIVE_ARC_CHALLENGE_ACC_NORM = 0.2577
+    QUANT_ARC_MAX_DELTA_FLOOR_PERCENT = 0.2
     TRUST_REMOTE_CODE = True
     BATCH_SIZE = 6
 
