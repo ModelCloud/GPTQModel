406d91cdc28e06ca5d40de05bf8c6273bdd0ff62
CSY <csy@modelcloud.ai>
2024-12-16 11:33:44 +0800

    fix quantize_config may be None

--- gptqmodel/models/auto.py
+++ gptqmodel/models/auto.py
@@ -195,7 +195,7 @@ class GPTQModel:
                            "`from_pretrained` with `quantize_config`.")
             return cls.from_quantized(model_id_or_path, trust_remote_code=trust_remote_code)
 
-        if quantize_config.dynamic:
+        if quantize_config and quantize_config.dynamic:
             logger.warning("GPTQModel's per-module `dynamic` quantization feature is currently not upstreamed to hf/vllm/sglang. If you're using vllm, you need to install this PR: https://github.com/vllm-project/vllm/pull/7086")
 
         model_type = check_and_get_model_type(model_id_or_path, trust_remote_code)
