60446b15cedb0b1b61af3290eb972200ce73e494
Qubitium-ModelCloud <qubitium@modelcloud.ai>
2024-12-16 17:34:33 +0800

    Update gptq.py

--- gptqmodel/quantization/gptq.py
+++ gptqmodel/quantization/gptq.py
@@ -81,7 +81,7 @@ class GPTQ:
     ):
         # TODO: waiting for pytorch implementgation of aten ops for MPS
         if sys.platform == "darwin" and os.getenv("PYTORCH_ENABLE_MPS_FALLBACK") != "1":
-            raise RuntimeError("You're running quant on darwin, please set the PYTORCH_ENABLE_MPS_FALLBACK environment variable, otherwise torch will throw an error.")
+            raise  RuntimeError("For MacOS you must set env `PYTORCH_ENABLE_MPS_FALLBACK=1` before running quantization.")
 
         W = self.layer.weight.data.clone()
 
