23350b54cbbd97a9dc6c58873acb0a63f42ff1bb
CSY <csy@modelcloud.ai>
2024-12-16 17:25:31 +0800

    set PYTORCH_ENABLE_MPS_FALLBACK for macos

--- gptqmodel/models/auto.py
+++ gptqmodel/models/auto.py
@@ -1,4 +1,9 @@
 from __future__ import annotations
+import os
+import sys
+
+if sys.platform == "darwin":
+    os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "1"
 
 import os.path
 from os.path import isdir, join
