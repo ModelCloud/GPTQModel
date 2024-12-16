e3364ce0cb57d946e7245544a7a6f28128d73fe8
CL-ModelCloud <cl@modelcloud.ai>
2024-12-16 07:47:34 +0000

    revert code

--- gptqmodel/models/auto.py
+++ gptqmodel/models/auto.py
@@ -116,6 +116,7 @@ MODEL_MAP = {
 
 HAS_IPEX = False
 try:
+    from intel_extension_for_pytorch.llm.quantization import IPEXWeightOnlyQuantizedLinear
     HAS_IPEX = True
 except Exception:
     pass
