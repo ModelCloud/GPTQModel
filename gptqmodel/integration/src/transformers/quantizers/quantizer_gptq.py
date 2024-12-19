d4f6f0814f82638c178498e5e30835d3fca74a17
CSY-ModelCloud <csy@modelcloud.ai>
2024-12-19 10:36:59 +0800

    update optimum min version (#913)

--- gptqmodel/integration/src/transformers/quantizers/quantizer_gptq.py
+++ gptqmodel/integration/src/transformers/quantizers/quantizer_gptq.py
@@ -72,7 +72,7 @@ class GptqHfQuantizer(HfQuantizer):
             )
         elif is_gptqmodel_available() and (
             version.parse(importlib.metadata.version("gptqmodel")) < version.parse("1.4.3")
-            or version.parse(importlib.metadata.version("optimum")) < version.parse("1.23.99")
+            or version.parse(importlib.metadata.version("optimum")) < version.parse("1.23.3")
         ):
             raise ImportError("The gptqmodel version should be >= 1.4.3, optimum version should >= 1.24.0")
 
