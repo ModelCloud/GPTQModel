802b4f56e100808d32a38f1935b7c3c1ebec8e00
CL-ModelCloud <cl@modelcloud.ai>
2024-12-20 04:47:04 +0000

    add torch benchmark test

---
+++ tests/benchmark/test_torch_inference.py
@@ -0,0 +1,71 @@
+import os
+import time
+
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = "11"
+import unittest  # noqa: E402
+from transformers import AutoTokenizer  # noqa: E402
+from gptqmodel import BACKEND, GPTQModel  # noqa: E402
+
+
+class TestsTorchBenchmark(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        self.model_id = "/monster/data/lrl/llama31/best-8b-it/gptq_4bit_07-29_06-46-12_maxlen2048_ns1024_descFalse_damp0.005/"
+        self.device = 'cuda'
+        self.min_new_tokens = 100
+        self.prompts = [
+            "I am in Paris and I",
+            "The capital of the United Kingdom is",
+            "The largest ocean on Earth is",
+            "The world’s longest river is",
+            "The tallest mountain in the world is",
+            "The currency used in Japan is",
+            "How to consult a dictionary?",
+            "What is the boiling point of water in degrees Celsius?"
+            "Which is the most widely used Internet search engine in the world?",
+            "What is the official language of France?"
+        ]
+        self.num_runs = 10
+
+    def test_torch_benchmark(self):
+        model = GPTQModel.from_quantized(
+            self.model_id,
+            device=self.device,
+            backend=BACKEND.TORCH,
+        )
+
+        tokenizer = AutoTokenizer.from_pretrained(self.model_id)
+        inp = tokenizer(self.prompts, padding=True, truncation=True, return_tensors="pt", padding_side='left').to(self.device)
+
+        times = []
+        for _ in range(self.num_runs):
+            start_time = time.time()
+            res = model.generate(**inp, num_beams=1, min_new_tokens=self.min_new_tokens,
+                                 max_new_tokens=self.min_new_tokens)
+            end_time = time.time()
+
+            elapsed_time = end_time - start_time
+            times.append(elapsed_time)
+
+        sum_time = sum(times)
+        avg_tokens_per_second = (self.min_new_tokens * self.num_runs) / sum_time
+
+        print("**************** Result(token/s) Info****************")
+        print(f"Times: {times}")
+        print(f"Sum Times: {sum_time}")
+        print(f"Sum New Tokens: {self.min_new_tokens * self.num_runs}")
+        print(f"Benchmark Result: {avg_tokens_per_second} token/s")
+        print("**************** Result(token/s) Info End****************")
+
+        self.assertTrue(avg_tokens_per_second > 4, f"Average tokens per second {avg_tokens_per_second} is not greater than 4.")
+
+
+
+
+
+
+
+
+
+
