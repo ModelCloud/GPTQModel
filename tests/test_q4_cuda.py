e9cff065ec0d55d94fe7dca0bbb2882f7e74794a
CSY-ModelCloud <csy@modelcloud.ai>
2024-12-20 10:40:02 +0800

    simplify cuda tests

--- tests/test_q4_cuda.py
+++ tests/test_q4_cuda.py
@@ -25,16 +25,6 @@ class TestsQ4CUDA(unittest.TestCase):
         ]
     )
     def test_generation_desc_act_true(self, torch_dtype, device):
-        prompt = "I am in Paris and"
-
-        # Reference generated with the cuda-old kernel
-        if device == "cpu":
-            # CPU implementation is extremely slow.
-            new_tokens = 2
-            reference_output = "<s> I am in Paris and I am"
-        else:
-            reference_output = "<s> I am in Paris and I am in love with you.\n\nScene 2:\n\n(The stage is now dark, but the audience can see the characters walking around the stage.)\n\n(The stage is now lit up, but the audience can only see the characters' silhouettes.)\n\n("
-            new_tokens = 60
 
         model_id = "/monster/data/model/TinyLlama-1.1B-Chat-v1.0-GPTQ-4bit"
         revision = "desc_act_true"
@@ -49,21 +39,17 @@ class TestsQ4CUDA(unittest.TestCase):
 
         tokenizer = AutoTokenizer.from_pretrained(model_id)
 
-        inp = tokenizer(prompt, return_tensors="pt").to(device)
+        inp = tokenizer("The capital of France is is", return_tensors="pt").to(device)
 
         # This one uses Autocast.
-        res = model_q.generate(**inp, num_beams=1, min_new_tokens=new_tokens, max_new_tokens=new_tokens)
-        predicted_text = tokenizer.decode(res[0])
-        print("predicted_text", predicted_text)
-        print("reference_output", reference_output)
-        self.assertEqual(predicted_text[:GENERATE_EVAL_SIZE], reference_output[:GENERATE_EVAL_SIZE])
+        generate_str = tokenizer.decode(model_q.generate(**inp, max_new_tokens=2)[0])
+        print(f"generate_str: {generate_str}")
+        self.assertIn("paris", generate_str.lower())
 
         # This one does not.
-        res = model_q.model.generate(**inp, num_beams=1, min_new_tokens=new_tokens, max_new_tokens=new_tokens)
-        predicted_text = tokenizer.decode(res[0])
-        print("predicted_text", predicted_text)
-        print("reference_output", reference_output)
-        self.assertEqual(predicted_text[:GENERATE_EVAL_SIZE], reference_output[:GENERATE_EVAL_SIZE])
+        generate_str = tokenizer.decode(model_q.model.generate(**inp, max_new_tokens=2)[0])
+        print(f"generate_str: {generate_str}")
+        self.assertIn("paris", generate_str.lower())
 
     @parameterized.expand(
         [
@@ -72,17 +58,6 @@ class TestsQ4CUDA(unittest.TestCase):
         ]
     )
     def test_generation_desc_act_false(self, torch_dtype, device):
-        prompt = "I am in Paris and"
-
-        # Reference generated with the cuda-old kernel
-        if device == "cpu":
-            # CPU implementation is extremely slow.
-            new_tokens = 3
-            reference_output = "<s> I am in Paris and I am in"
-        else:
-            reference_output = "<s> I am in Paris and I am in love with you.\n\nScene 2:\n\n(The stage is now dark, but the audience can see the characters walking around the stage.)\n\n(The stage is now lit up, but the audience can see the characters walking around the stage.)\n\n(The"
-            new_tokens = 60
-
         model_id = "/monster/data/model/TinyLlama-1.1B-Chat-v1.0-GPTQ-4bit"
 
         model_q = GPTQModel.from_quantized(
@@ -93,16 +68,15 @@ class TestsQ4CUDA(unittest.TestCase):
         )
         tokenizer = AutoTokenizer.from_pretrained(model_id)
 
-        inp = tokenizer(prompt, return_tensors="pt").to(device)
+        inp = tokenizer("The capital of France is is", return_tensors="pt").to(device)
 
         # This one uses Autocast.
-        res = model_q.generate(**inp, num_beams=1, min_new_tokens=new_tokens, max_new_tokens=new_tokens)
-        predicted_text = tokenizer.decode(res[0])
-
-        self.assertEqual(predicted_text[:GENERATE_EVAL_SIZE], reference_output[:GENERATE_EVAL_SIZE])
+        generate_str = tokenizer.decode(model_q.generate(**inp, max_new_tokens=2)[0])
+        print(f"generate_str: {generate_str}")
+        self.assertIn("paris", generate_str.lower())
 
         # This one does not.
-        res = model_q.model.generate(**inp, num_beams=1, min_new_tokens=new_tokens, max_new_tokens=new_tokens)
-        predicted_text = tokenizer.decode(res[0])
+        generate_str = tokenizer.decode(model_q.model.generate(**inp, max_new_tokens=2)[0])
+        print(f"generate_str: {generate_str}")
+        self.assertIn("paris", generate_str.lower())
 
-        self.assertEqual(predicted_text[:GENERATE_EVAL_SIZE], reference_output[:GENERATE_EVAL_SIZE])
