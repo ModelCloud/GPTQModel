2475f6fd62c23145253018a801863d23cea32dfc
CSY-ModelCloud <csy@modelcloud.ai>
2024-12-20 10:44:42 +0800

    simplify cuda tests

--- tests/test_q4_cuda.py
+++ tests/test_q4_cuda.py
@@ -1,7 +1,6 @@
 # -- do not touch
 import os
 
-
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 # -- end do not touch
 
@@ -13,11 +12,12 @@ from transformers import AutoTokenizer  # noqa: E402
 
 from gptqmodel import BACKEND, GPTQModel  # noqa: E402
 
-
-GENERATE_EVAL_SIZE = 100
-
-
 class TestsQ4CUDA(unittest.TestCase):
+
+    def setUpClass(cls):
+        cls.model_id = "/monster/data/model/TinyLlama-1.1B-Chat-v1.0-GPTQ-4bit"
+        cls.tokenizer = AutoTokenizer.from_pretrained(cls.model_id)
+
     @parameterized.expand(
         [
             (torch.bfloat16, "cuda:0"),
@@ -25,31 +25,20 @@ class TestsQ4CUDA(unittest.TestCase):
         ]
     )
     def test_generation_desc_act_true(self, torch_dtype, device):
-
-        model_id = "/monster/data/model/TinyLlama-1.1B-Chat-v1.0-GPTQ-4bit"
-        revision = "desc_act_true"
-
         model_q = GPTQModel.from_quantized(
-            model_id,
-            revision=revision,
+            self.model_id,
+            revision="desc_act_true",
             device=device,
             backend=BACKEND.CUDA,
             torch_dtype=torch_dtype,
         )
-
-        tokenizer = AutoTokenizer.from_pretrained(model_id)
-
-        inp = tokenizer("The capital of France is is", return_tensors="pt").to(device)
+        input = self.tokenizer("The capital of France is is", return_tensors="pt").to(device)
 
         # This one uses Autocast.
-        generate_str = tokenizer.decode(model_q.generate(**inp, max_new_tokens=2)[0])
-        print(f"generate_str: {generate_str}")
-        self.assertIn("paris", generate_str.lower())
+        self.generate(model_q, input)
 
         # This one does not.
-        generate_str = tokenizer.decode(model_q.model.generate(**inp, max_new_tokens=2)[0])
-        print(f"generate_str: {generate_str}")
-        self.assertIn("paris", generate_str.lower())
+        self.generate(model_q.model, input)
 
     @parameterized.expand(
         [
@@ -58,25 +47,22 @@ class TestsQ4CUDA(unittest.TestCase):
         ]
     )
     def test_generation_desc_act_false(self, torch_dtype, device):
-        model_id = "/monster/data/model/TinyLlama-1.1B-Chat-v1.0-GPTQ-4bit"
-
         model_q = GPTQModel.from_quantized(
-            model_id,
+            self.model_id,
             device=device,
             backend=BACKEND.CUDA,
             torch_dtype=torch_dtype,
         )
-        tokenizer = AutoTokenizer.from_pretrained(model_id)
 
-        inp = tokenizer("The capital of France is is", return_tensors="pt").to(device)
+        input = self.tokenizer("The capital of France is is", return_tensors="pt").to(device)
 
         # This one uses Autocast.
-        generate_str = tokenizer.decode(model_q.generate(**inp, max_new_tokens=2)[0])
-        print(f"generate_str: {generate_str}")
-        self.assertIn("paris", generate_str.lower())
+        self.generate(model_q, input)
 
         # This one does not.
-        generate_str = tokenizer.decode(model_q.model.generate(**inp, max_new_tokens=2)[0])
+        self.generate(model_q.model, input)
+
+    def generate(self, model, input):
+        generate_str = self.tokenizer.decode(model.model.generate(**input, max_new_tokens=2)[0])
         print(f"generate_str: {generate_str}")
         self.assertIn("paris", generate_str.lower())
-
