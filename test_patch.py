8c5cfe89e08743e80a619b17492b268c533ab256
CSY <csy@modelcloud.ai>
2024-12-25 15:50:38 +0800

    fix 2 model load

--- test_patch.py
+++ test_patch.py
@@ -1,5 +1,6 @@
 import time
 
+import torch
 from transformers import AutoTokenizer, LlamaModel
 
 from gptqmodel import GPTQModel
@@ -9,17 +10,16 @@ LlamaModel.forward = patched_LlamaModel.forward
 
 model_id = "/monster/data/lrl/qwq_quant/vortex_v2"
 
-model = GPTQModel.load(model_id)
+model = GPTQModel.load(model_id, device = "cpu")
+
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 
 input_ids = tokenizer("Uncovering deep insights begins with", return_tensors="pt").to(model.device)
 
 
-patched_model = GPTQModel.load(model_id)
-
 now = time.time()
 
-result = patched_model.generate(
+result = model.generate(
     **input_ids, min_new_tokens=100, max_new_tokens=100
 )[0]
 
