a8198cd7f4a9e64a728624008b23a5264d44a827
CSY <csy@modelcloud.ai>
2024-12-25 15:47:45 +0800

    update model

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ from transformers import AutoTokenizer, LlamaModel
 from gptqmodel import GPTQModel
 
 
-model_id = "/monster/data/model/Llama-3.2-1B-Instruct-gptqmodel-4bit-vortext-v1"
+model_id = "/monster/data/lrl/qwq_quant/vortex_v2"
 
 model = GPTQModel.load(model_id)
 tokenizer = AutoTokenizer.from_pretrained(model_id)
@@ -20,5 +20,6 @@ result = model.generate(
 
 print(f"non patched time={time.time() - now}")
 
+# 20.77G 5.4s
 if __name__ == '__main__':
     print()
--- test_patch.py
+++ test_patch.py
@@ -5,15 +5,15 @@ from transformers import AutoTokenizer, LlamaModel
 from gptqmodel import GPTQModel
 from gptqmodel.integration.src.transformers.models.llama.modeling_llama import LlamaModel as patched_LlamaModel
 
+LlamaModel.forward = patched_LlamaModel.forward
 
-model_id = "/monster/data/model/Llama-3.2-1B-Instruct-gptqmodel-4bit-vortext-v1"
+model_id = "/monster/data/lrl/qwq_quant/vortex_v2"
 
 model = GPTQModel.load(model_id)
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 
 input_ids = tokenizer("Uncovering deep insights begins with", return_tensors="pt").to(model.device)
 
-LlamaModel.forward = patched_LlamaModel.forward
 
 patched_model = GPTQModel.load(model_id)
 
