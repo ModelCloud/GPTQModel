2e1dc1916c384c2e9843651df5dd37ca85267f3f
CSY <csy@modelcloud.ai>
2024-12-26 09:10:19 +0800

    update patch test

--- test_patch.py
+++ test_patch.py
@@ -2,15 +2,25 @@ import time
 
 import torch
 from transformers import AutoTokenizer, LlamaModel
+# from transformers.models.llama.modeling_llama import LlamaDecoderLayer
 
-from gptqmodel import GPTQModel
+from gptqmodel import GPTQModel, BACKEND
 from gptqmodel.integration.src.transformers.models.llama.modeling_llama import LlamaModel as patched_LlamaModel
 
 LlamaModel.forward = patched_LlamaModel.forward
 
-model_id = "/monster/data/lrl/qwq_quant/vortex_v2"
+# model_id = "/monster/data/lrl/qwq_quant/vortex_v2"
+model_id = "/monster/data/model/Llama-3.2-1B-Instruct-gptqmodel-4bit-vortext-v1"
 
-model = GPTQModel.load(model_id, device = "cpu")
+cuda = torch.device("cuda")
+
+model = GPTQModel.load(model_id, backend = BACKEND.TORCH, device = "cpu")
+# model.to("cpu")
+
+# for name, module in model.named_modules():
+#     if isinstance(module, LlamaDecoderLayer):
+#       print(f"eeeeeeeeee {name} {module}".replace("\n", "\\n"))
+#       module.to("cpu")
 
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 
