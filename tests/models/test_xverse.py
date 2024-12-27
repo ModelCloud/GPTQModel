efba528326f7e5cad67eccf7ad5f9198d67cfb73
CSY-ModelCloud <csy@modelcloud.ai>
2024-12-27 11:26:08 +0800

    downgrade delta to 0.2

--- tests/models/test_xverse.py
+++ tests/models/test_xverse.py
@@ -5,6 +5,7 @@ class TestXVerse(ModelTest):
     NATIVE_MODEL_ID = "/monster/data/model/XVERSE-7B-Chat" # "xverse/XVERSE-7B-Chat"
     NATIVE_ARC_CHALLENGE_ACC = 0.4198
     NATIVE_ARC_CHALLENGE_ACC_NORM = 0.4044
+    QUANT_ARC_MAX_DELTA_FLOOR_PERCENT = 0.2
     TRUST_REMOTE_CODE = True
     APPLY_CHAT_TEMPLATE = True
     BATCH_SIZE = 6
