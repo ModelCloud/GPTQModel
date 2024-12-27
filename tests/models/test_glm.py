35150a45ad30849559f07235a41351dbfa068b48
CSY <csy@modelcloud.ai>
2024-12-27 13:58:48 +0800

    decrease delta to -20%

--- tests/models/test_glm.py
+++ tests/models/test_glm.py
@@ -6,6 +6,7 @@ class TestGlm(ModelTest):
     NATIVE_MODEL_ID = "/monster/data/model/glm-4-9b-chat-hf"
     NATIVE_ARC_CHALLENGE_ACC = 0.5154
     NATIVE_ARC_CHALLENGE_ACC_NORM = 0.5316
+    QUANT_ARC_MAX_DELTA_FLOOR_PERCENT = 0.2
     USE_VLLM = False
 
     def test_glm(self):
