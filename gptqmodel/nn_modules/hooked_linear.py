8ef9632c362a966a6c6463628ccca0b007e88741
LRL-ModelCloud <lrl@lbx.dev>
2025-01-04 16:07:38 +0800

    add HookedConv2d

--- gptqmodel/nn_modules/hooked_linear.py
+++ gptqmodel/nn_modules/hooked_linear.py
@@ -1,4 +1,5 @@
 import torch
+import transformers
 
 
 class HookedConv1D(torch.nn.Conv1d):
@@ -15,6 +16,28 @@ class HookedConv1D(torch.nn.Conv1d):
         custom_conv1d.bias = conv1d.bias
         return custom_conv1d    
 
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
+        output = super().forward(input)
+        if self.forward_hook:
+            self.forward_hook(self, input, output)
+        return output
+
+
+class HookedConv2d(torch.nn.Conv2d):
+    def __init__(self, in_channels: int, out_channels: int, kernel_size: int, stride: int, padding: int) -> None:
+        torch.nn._ConvNd.__init__(self)
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+
+        self.forward_hook = None
+
+    @staticmethod
+    def from_conv2d(conv2d: torch.nn.Conv2d):
+        custom_conv2d = HookedConv2d(conv2d.in_channels, conv2d.out_channels)
+        custom_conv2d.weight = conv2d.weight
+        custom_conv2d.bias = conv2d.bias
+        return custom_conv2d
+    
     def forward(self, input: torch.Tensor) -> torch.Tensor:
         output = super().forward(input)
         if self.forward_hook:
@@ -49,7 +72,9 @@ def replace_linear_with_hooked_linear(module):
     for name, child in module.named_children():
         if isinstance(child, torch.nn.Linear):
             setattr(module, name, HookedLinear.from_linear(child))
-        elif isinstance(child, torch.nn.Conv1d):
+        elif isinstance(child, transformers.pytorch_utils.Conv1D):
             setattr(module, name, HookedConv1D.from_conv1d(child))
+        elif isinstance(child, torch.nn.Conv2d):
+            setattr(module, name, HookedLinear.from_linear(child))
         else:
             replace_linear_with_hooked_linear(child)
