37eb52c90ef931e8bcd13f1d64f1d42613a321df
LRL-ModelCloud <165116337+LRL-ModelCloud@users.noreply.github.com>
2025-01-04 14:18:06 +0800

    fix hooked linear init (#1011)
    * fix hooked linear init
    
    * cleanup
    
    * Update hooked_linear.py
    
    * Update hooked_linear.py
    
    * Update hooked_linear.py
    
    ---------
    
    Co-authored-by: Qubitium-ModelCloud <qubitium@modelcloud.ai>

--- gptqmodel/nn_modules/hooked_linear.py
+++ gptqmodel/nn_modules/hooked_linear.py
@@ -2,8 +2,12 @@ import torch
 
 
 class HookedLinear(torch.nn.Linear):
-    def __init__(self, in_features: int, out_features: int, bias: bool = True, device=None, dtype=None) -> None:
-        super().__init__(in_features, out_features, bias, device, dtype)
+    def __init__(self, in_features: int, out_features: int) -> None:
+        # avoid calling super().__init__() as it would allocate memory based on in/out features
+        torch.nn.Module.__init__(self)
+        self.in_features = in_features
+        self.out_features = out_features
+            
         self.forward_hook = None
 
     @staticmethod
@@ -16,11 +20,9 @@ class HookedLinear(torch.nn.Linear):
 
     @staticmethod
     def from_linear(linear: torch.nn.Linear):
-        custom_linear = HookedLinear(linear.in_features, linear.out_features, bias=linear.bias is not None,
-                                     device=linear.weight.device, dtype=linear.weight.dtype)
+        custom_linear = HookedLinear(linear.in_features, linear.out_features)
         custom_linear.weight = linear.weight
-        if linear.bias is not None:
-            custom_linear.bias = linear.bias
+        custom_linear.bias = linear.bias
         return custom_linear
 
     def forward(self, input: torch.Tensor) -> torch.Tensor:
