aba43c0be015f251c98edd26bc0c05721dd253cb
CSY-ModelCloud <csy@modelcloud.ai>
2025-01-08 16:44:26 +0800

    don't download whl for xpu env (#1059)

--- setup.py
+++ setup.py
@@ -253,7 +253,7 @@ if BUILD_CUDA_EXT:
 
 class CachedWheelsCommand(_bdist_wheel):
     def run(self):
-        if FORCE_BUILD:
+        if FORCE_BUILD or torch.xpu.is_available():
             return super().run()
 
         python_version = f"cp{sys.version_info.major}{sys.version_info.minor}"
