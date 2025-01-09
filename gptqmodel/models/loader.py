48cccd5764265a05144bc3bc013584da23455442
LRL-ModelCloud <lrl@lbx.dev>
2025-01-09 19:50:39 +0800

    Revert "Update loader.py"
    This reverts commit 1366d353e23e399f4541a3066b32934fad075fd2.

--- gptqmodel/models/loader.py
+++ gptqmodel/models/loader.py
@@ -299,22 +299,6 @@ def ModelLoader(cls):
                 qlinear_kernel=None,
                 model_local_path=model_local_path,
             )
-        
-        if backend == BACKEND.MLX:
-            from mlx_lm import load
-            from ..utils.mlx import mlx_generate
-
-            model, tokenizer = load(model_local_path)
-            cls.generate = lambda self, **kwargs: mlx_generate(model=model, tokenizer=tokenizer,
-                                                                           **kwargs)
-            return cls(
-                model,
-                quantized=True,
-                tokenizer=tokenizer,
-                quantize_config=quantize_config,
-                qlinear_kernel=None,
-                model_local_path=model_local_path,
-            )
 
         if quantize_config.format == FORMAT.MARLIN:
             # format marlin requires marlin kernel
@@ -567,6 +551,24 @@ def ModelLoader(cls):
         model.eval()
 
         tokenizer = get_tokenizer(model_id_or_path, config=config, trust_remote_code=trust_remote_code)
+
+        if backend == BACKEND.MLX:
+            import tempfile
+            from ..utils.mlx import convert_gptq_to_mlx_weights, mlx_generate
+            from mlx_lm.utils import save_weights, save_config
+            from mlx_lm import load
+
+            with tempfile.TemporaryDirectory() as temp_dir:
+                mlx_weights, mlx_config = convert_gptq_to_mlx_weights(model_id_or_path, model, quantize_config.to_dict())
+
+                save_weights(temp_dir, mlx_weights, donate_weights=True)
+                save_config(mlx_config, config_path=temp_dir + "/config.json")
+                tokenizer.save_pretrained(temp_dir)
+                
+                model, _ = load(temp_dir)
+
+                cls.generate = lambda _, **kwargs: mlx_generate(model=model, tokenizer=tokenizer, **kwargs)
+
 
         return cls(
             model,
