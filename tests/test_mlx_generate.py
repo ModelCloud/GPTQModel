0b42b1a85483f11994d0b45245685f4a03f09f49
CL-ModelCloud <cl@modelcloud.ai>
2025-01-09 20:33:07 +0800

    add mlx generate test (#1069)
    * mlx test
    
    * add generate test

---
+++ tests/test_mlx_generate.py
@@ -0,0 +1,33 @@
+import os
+import sys
+
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+if sys.platform == "darwin":
+    os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "1"
+
+import unittest
+
+from gptqmodel import GPTQModel, BACKEND
+
+class TestMlxGenerate(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        self.pretrained_model_id = "/monster/data/model/Qwen2.5-0.5B-Instruct/gptq_4bits_01-07_14-18-11_maxlen1024_ns1024_descFalse_damp0.1/"
+
+    def test_mlx_generate(self):
+        mlx_model = GPTQModel.load(
+            self.pretrained_model_id,
+            backend=BACKEND.MLX
+        )
+
+        prompt = "The capital of France is"
+
+        messages = [{"role": "user", "content": prompt}]
+        prompt = mlx_model.tokenizer.apply_chat_template(
+            messages, add_generation_prompt=True
+        )
+        text = mlx_model.generate(prompt=prompt)
+        assert "paris" in text.lower()
+
+
+
