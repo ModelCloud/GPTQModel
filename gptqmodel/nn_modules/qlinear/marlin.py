fe9ba7715325ff83cb7243aa24619b296a5c4673
CSY-ModelCloud <csy@modelcloud.ai>
2025-01-18 09:42:07 +0800

    check compute capability with validate_device

--- gptqmodel/nn_modules/qlinear/marlin.py
+++ gptqmodel/nn_modules/qlinear/marlin.py
@@ -15,6 +15,7 @@
 
 # Adapted from vllm at https://github.com/vllm-project/vllm/blob/main/vllm/model_executor/layers/quantization/gptq_marlin.py
 
+import os
 from typing import Any, Dict, List, Optional, Tuple
 
 import numpy as np
@@ -313,6 +314,18 @@ class MarlinQuantLinear(BaseQuantLinear):
         if marlin_import_exception is not None:
             return False, marlin_import_exception
         return cls._validate(**args)
+
+    @classmethod
+    def validate_device(cls, device: DEVICE):
+        super().validate_device(device)
+        CUDA_VISIBLE_DEVICES = os.environ.get("CUDA_VISIBLE_DEVICES")
+        if device == DEVICE.CUDA:
+            if CUDA_VISIBLE_DEVICES is None:
+                has_cuda_v8 = any(torch.cuda.get_device_capability(i)[0] >= 8 for i in range(torch.cuda.device_count()))
+            else:
+                has_cuda_v8 = all(torch.cuda.get_device_capability(int(i))[0] >= 8 for i in CUDA_VISIBLE_DEVICES.split(","))
+            if not has_cuda_v8:
+                raise NotImplementedError("marlin kernel only supports compute capability >= 8.0, you are going to run it on an incompatible device.")
 
     def post_init(self):
         device = self.qweight.device
