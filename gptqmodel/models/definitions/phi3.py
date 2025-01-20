0db07fdce3f2f1d0a75da26b7a5b7dbf4e78660e
CSY <csy@modelcloud.ai>
2025-01-20 18:03:46 +0800

    use EXPERT_INDEX_PLACEHOLDER

--- gptqmodel/models/definitions/phi3.py
+++ gptqmodel/models/definitions/phi3.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+from .._const import EXPERT_INDEX_PLACEHOLDER
 from ..base import BaseGPTQModel
 
 
@@ -34,61 +34,12 @@ class PhiMoEGPTQForCausalLM(BaseGPTQModel):
     layer_type = "PhiMoEDecoderLayer"
     layers_node = "model.layers"
     base_modules = ["model.embed_tokens", "model.norm"]
+
     layer_modules = [
         ["self_attn.k_proj", "self_attn.v_proj", "self_attn.q_proj"],
         ["self_attn.o_proj"],
-        [
-            "block_sparse_moe.experts.0.w1",
-            "block_sparse_moe.experts.1.w1",
-            "block_sparse_moe.experts.2.w1",
-            "block_sparse_moe.experts.3.w1",
-            "block_sparse_moe.experts.4.w1",
-            "block_sparse_moe.experts.5.w1",
-            "block_sparse_moe.experts.6.w1",
-            "block_sparse_moe.experts.7.w1",
-            "block_sparse_moe.experts.0.w3",
-            "block_sparse_moe.experts.1.w3",
-            "block_sparse_moe.experts.2.w3",
-            "block_sparse_moe.experts.3.w3",
-            "block_sparse_moe.experts.4.w3",
-            "block_sparse_moe.experts.5.w3",
-            "block_sparse_moe.experts.6.w3",
-            "block_sparse_moe.experts.7.w3",
-            "block_sparse_moe.experts.8.w1",
-            "block_sparse_moe.experts.9.w1",
-            "block_sparse_moe.experts.10.w1",
-            "block_sparse_moe.experts.11.w1",
-            "block_sparse_moe.experts.12.w1",
-            "block_sparse_moe.experts.13.w1",
-            "block_sparse_moe.experts.14.w1",
-            "block_sparse_moe.experts.15.w1",
-            "block_sparse_moe.experts.8.w3",
-            "block_sparse_moe.experts.9.w3",
-            "block_sparse_moe.experts.10.w3",
-            "block_sparse_moe.experts.11.w3",
-            "block_sparse_moe.experts.12.w3",
-            "block_sparse_moe.experts.13.w3",
-            "block_sparse_moe.experts.14.w3",
-            "block_sparse_moe.experts.15.w3",
-        ],
-        [
-            "block_sparse_moe.experts.0.w2",
-            "block_sparse_moe.experts.1.w2",
-            "block_sparse_moe.experts.2.w2",
-            "block_sparse_moe.experts.3.w2",
-            "block_sparse_moe.experts.4.w2",
-            "block_sparse_moe.experts.5.w2",
-            "block_sparse_moe.experts.6.w2",
-            "block_sparse_moe.experts.7.w2",
-            "block_sparse_moe.experts.8.w2",
-            "block_sparse_moe.experts.9.w2",
-            "block_sparse_moe.experts.10.w2",
-            "block_sparse_moe.experts.11.w2",
-            "block_sparse_moe.experts.12.w2",
-            "block_sparse_moe.experts.13.w2",
-            "block_sparse_moe.experts.14.w2",
-            "block_sparse_moe.experts.15.w2"
-        ],
+        [f"block_sparse_moe.experts.{EXPERT_INDEX_PLACEHOLDER}.w1"],
+        [f"block_sparse_moe.experts.{EXPERT_INDEX_PLACEHOLDER}.w2"],
     ]
 
 __all__ = ["Phi3GPTQ", "PhiMoEGPTQForCausalLM"]
