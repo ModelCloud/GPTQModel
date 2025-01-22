2f0f23ee259234fcac7acb66e7e7cf85815471e3
CSY <csy@modelcloud.ai>
2025-01-22 10:07:54 +0800

    clean more

--- tests/test_q4_triton.py
+++ tests/test_q4_triton.py
@@ -19,22 +19,14 @@ import os
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 # -- end do not touch
 
-import unittest  # noqa: E402
-
 import torch  # noqa: E402
 from gptqmodel import BACKEND, GPTQModel  # noqa: E402
 from gptqmodel.nn_modules.qlinear.tritonv2 import TritonV2QuantLinear  # noqa: E402
 from transformers import AutoTokenizer  # noqa: E402
+from models.model_test import ModelTest  # noqa: E402
 
-GENERATE_EVAL_SIZE = 100
-
-class TestsQ4Triton(unittest.TestCase):
+class TestsQ4Triton(ModelTest):
     def test_generation_desc_act_false(self):
-        prompt = "I am in Paris and"
-
-        reference_output = "<s> I am in Paris and I am in love with you.\n\nScene 2:\n\n(The stage is now dark, but the audience can see the characters walking around the stage.)\n\n(The stage is now lit up, but the audience can only see the characters' silhouettes.)\n\n("
-        new_tokens = 60
-
         model_id = "/monster/data/model/TinyLlama-1.1B-Chat-v1.0-GPTQ-4bit"
 
         model_q = GPTQModel.load(
@@ -51,19 +43,10 @@ class TestsQ4Triton(unittest.TestCase):
 
         tokenizer = AutoTokenizer.from_pretrained(model_id)
 
-        inp = tokenizer(prompt, return_tensors="pt").to("cuda:0")
-
         # This one uses Autocast.
-        res = model_q.generate(**inp, num_beams=1, min_new_tokens=new_tokens, max_new_tokens=new_tokens)
-        predicted_text = tokenizer.decode(res[0])
-
-        self.assertEqual(predicted_text[:GENERATE_EVAL_SIZE], reference_output[:GENERATE_EVAL_SIZE])
-
+        self.assertInference(model=model_q,tokenizer=tokenizer)
         # This one does not.
-        res = model_q.model.generate(**inp, num_beams=1, min_new_tokens=new_tokens, max_new_tokens=new_tokens)
-        predicted_text = tokenizer.decode(res[0])
-
-        self.assertEqual(predicted_text[:GENERATE_EVAL_SIZE], reference_output[:GENERATE_EVAL_SIZE])
+        self.assertInference(model=model_q.model,tokenizer=tokenizer)
 
     def test_generation_desc_act_true(self):
         prompt = "I am in Paris and"
@@ -89,10 +72,7 @@ class TestsQ4Triton(unittest.TestCase):
 
         tokenizer = AutoTokenizer.from_pretrained(model_id)
 
-        inp = tokenizer(prompt, return_tensors="pt").to(device)
-
-        res = model_q.generate(**inp, num_beams=1, min_new_tokens=60, max_new_tokens=60)
-
-        predicted_text = tokenizer.decode(res[0])
-
-        self.assertEqual(predicted_text[:GENERATE_EVAL_SIZE], reference_output[:GENERATE_EVAL_SIZE])
+        # This one uses Autocast.
+        self.assertInference(model=model_q,tokenizer=tokenizer)
+        # This one does not.
+        self.assertInference(model=model_q.model,tokenizer=tokenizer)
