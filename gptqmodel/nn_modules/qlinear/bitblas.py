5d854f5e62e8260e04a18be359d24a3dc8868065
CSY <csy@modelcloud.ai>
2025-01-22 09:13:00 +0800

    fix bitblas has not self.g_idx

--- gptqmodel/nn_modules/qlinear/bitblas.py
+++ gptqmodel/nn_modules/qlinear/bitblas.py
@@ -290,7 +290,7 @@ class BitBLASQuantLinear(BaseQuantLinear):
         if linear.bias is not None:
             self.bias = linear.bias.clone().half()
 
-        intweight = torch.round((W + scale_zeros[self.g_idx].T) / scales[self.g_idx].T).to(torch.int)
+        intweight = torch.round((W + scale_zeros[g_idx].T) / scales[g_idx].T).to(torch.int)
 
         intweight = intweight.t().contiguous()
         intweight = intweight.numpy().astype(np.uint32)
