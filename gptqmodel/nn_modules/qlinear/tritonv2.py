f942d1e0ecf614bd3e8a527adb2ee0851173dd02
CSY <csy@modelcloud.ai>
2025-01-23 09:25:14 +0800

    fix ruff

--- gptqmodel/nn_modules/qlinear/tritonv2.py
+++ gptqmodel/nn_modules/qlinear/tritonv2.py
@@ -21,6 +21,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import transformers
+import triton
+import triton.language as tl
 from packaging import version
 
 from ...models._const import DEVICE, PLATFORM
@@ -208,9 +210,6 @@ class TritonV2QuantLinear(BaseQuantLinear, TritonModuleMixin):
 __all__ = ["TritonV2QuantLinear"]
 
 
-import triton
-import triton.language as tl
-
 @triton.jit
 def add_kernel(x_ptr,  # *Pointer* to first input vector.
                y_ptr,  # *Pointer* to second input vector.
@@ -224,13 +223,14 @@ def add_kernel(x_ptr,  # *Pointer* to first input vector.
     mask = offsets < n_elements
     x = tl.load(x_ptr + offsets, mask=mask)
     y = tl.load(y_ptr + offsets, mask=mask)
-    output = x + y
+    output = x + y  # noqa: F841
 
 
 def add(x: torch.Tensor, y: torch.Tensor):
     output = torch.empty_like(x)
     n_elements = output.numel()
-    grid = lambda meta: (triton.cdiv(n_elements, meta['BLOCK_SIZE']), )
+    def grid(meta):
+        return (triton.cdiv(n_elements, meta['BLOCK_SIZE']), )
     add_kernel[grid](x, y, output, n_elements, BLOCK_SIZE=1024)
     return output
 
@@ -243,7 +243,7 @@ def triton_xpu_available():
     try:
         add(x, y)
         return True
-    except Exception as e:
+    except Exception:
         return False
 
 
