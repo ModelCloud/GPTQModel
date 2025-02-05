77a9addbb69917e6a16fe8d04cc68669a0f61b63
suluyana <suluyan.sly@alibaba-inc.com>
2025-02-05 15:03:08 +0800

    add test case

---
+++ tests/test_modelscope.py
@@ -0,0 +1,26 @@
+import os
+os.environ["GPTQMODEL_USE_MODELSCOPE"] = "1"
+import sys
+import subprocess  # noqa: E402
+import importlib.util  # noqa: E402
+
+if importlib.util.find_spec("modelscope") is None:
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "modelscope>=1.22"])
+
+from models.model_test import ModelTest  # noqa: E402
+from gptqmodel import GPTQModel
+
+
+class TestLoadModelscope(ModelTest):
+
+    @classmethod
+    def setUpClass(self):
+        self.MODEL_ID = "Qwen/Qwen2-VL-7B-Instruct-GPTQ-Int4"
+
+    def test_load_modelscope(self):
+        model = GPTQModel.load(self.MODEL_ID)
+
+        result = model.generate("Hello")[0]
+        assert(model.tokenizer.decode(result))
+
+        del model
