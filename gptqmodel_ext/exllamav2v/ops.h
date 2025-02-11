e73a051f112f5ee92755b0748e095bd29c37741f
ZX-ModelCloud <zx@modelcloud.ai>
2025-02-11 22:37:46 +0800

    call gptq_shuffle()

--- gptqmodel_ext/exllamav2v/ops.h
+++ gptqmodel_ext/exllamav2v/ops.h
@@ -13,3 +13,5 @@ torch::Tensor gptq_gemm_lora(torch::Tensor a, torch::Tensor b_q_weight,
                         torch::Tensor b_gptq_scales, torch::Tensor b_g_idx,
                         bool use_exllama, int64_t bit,
                         torch::Tensor eora_ax, torch::Tensor eora_b);
+
+void gptq_shuffle(torch::Tensor q_weight, torch::Tensor q_perm, int64_t bit);
