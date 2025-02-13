40cc96116b6afa35e591fbccc073a28a2f4681dc
Qubitium <Qubitium@modelcloud.ai>
2025-02-13 13:15:55 +0000

    hack NamedModule

--- gptqmodel/looper/named_module.py
+++ gptqmodel/looper/named_module.py
@@ -4,8 +4,22 @@ import torch
 
 class NamedModule(torch.nn.Module):
     def __init__(self, module: torch.nn.Module, name: str, full_name:str, layer_index: int) -> None:
-        super().__init__(module)
+        super().__init__()
 
+        self.module = module
         self.name = name
         self.full_name = full_name
         self.layer_index = layer_index
+
+    def __getattr__(self, item):
+        try:
+            if item == "name":
+                return self.name
+            elif item == "full_name":
+                return self.full_name
+            elif item == "layer_index":
+                return self.layer_index
+
+            return self.module.__getattr__(item)
+        except Exception:
+            return getattr(self.model, item)
