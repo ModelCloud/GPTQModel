437c93959188eb4f9271ea6e116bdabe1692f94d
Qubitium <Qubitium@modelcloud.ai>
2025-02-13 18:35:40 +0000

    fix missing `modules` item

--- gptqmodel/looper/named_module.py
+++ gptqmodel/looper/named_module.py
@@ -6,14 +6,14 @@ class NamedModule(torch.nn.Module):
     def __init__(self, module: torch.nn.Module, name: str, full_name:str, layer_index: int) -> None:
         super().__init__()
 
-        self.module = module
-        self.name = name
-        self.full_name = full_name
-        self.layer_index = layer_index
+        self.module = module # wrapped module
+        self.name = name # module name
+        self.full_name = full_name # module full name (path) within model
+        self.layer_index = layer_index # layerid in a repeating layer, if in outside layer, this info may be fake
         self.state = {} # state is dict to store all temp data used in processor
 
     def __getattr__(self, name: str):
-        if name in ["name", "full_name", "layer_index", "state"]:
+        if name in ["module", "name", "full_name", "layer_index", "state"]:
             return getattr(self, name)
 
         return getattr(self.module, name)
