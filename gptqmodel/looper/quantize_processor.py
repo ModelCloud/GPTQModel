b892d7047c059218a791a9f290cd7ec7759bb896
Qubitium <Qubitium@modelcloud.ai>
2025-02-13 13:04:53 +0000

    use NamedModule

--- gptqmodel/looper/quantize_processor.py
+++ gptqmodel/looper/quantize_processor.py
@@ -5,6 +5,7 @@ from gptqmodel.looper.loop_processor import LoopProcessor
 from torch.nn import Module
 from torch import Tensor
 
+from gptqmodel.looper.named_module import NamedModule
 from gptqmodel.models.writer import (QUANT_LOG_DAMP, QUANT_LOG_FWD_TIME, QUANT_LOG_LAYER,
                      QUANT_LOG_LOSS, QUANT_LOG_MODULE, QUANT_LOG_TIME)
 from gptqmodel.quantization import GPTQ
@@ -21,16 +22,16 @@ class GPTQProcessor(LoopProcessor):
         self.module_names = []
         self.quant_log = []
 
-    def preprocess(self, module: Module, buffered_fwd: bool):
+    def preprocess(self, module: NamedModule, buffered_fwd: bool):
         bits = self.qcfg.bits
         sym = self.qcfg.sym
         mse = self.qcfg.mse
 
         # dynamic overrides
         if self.qcfg.dynamic is not None:
-            bits = self.qcfg.dynamic_get(module._gptqmodel_full_name, "bits", bits)
-            sym = self.qcfg.dynamic_get(module._gptqmodel_full_nam, "sym", sym)
-            mse = self.qcfg.dynamic_get(module._gptqmodel_full_nam, "mse", mse)
+            bits = self.qcfg.dynamic_get(module.full_name, "bits", bits)
+            sym = self.qcfg.dynamic_get(module.full_name, "sym", sym)
+            mse = self.qcfg.dynamic_get(module.full_name, "mse", mse)
 
         tmp = GPTQ(module)
 
@@ -40,7 +41,7 @@ class GPTQProcessor(LoopProcessor):
         # deepseek has massive # of sub-modules per layer, causing vram pressure
         # buffered mode is slower due to gpu<->cpu movement
         if buffered_fwd:  # TODO tweak this number for masive MoE
-            logger.info(f"Experimental: enabling fwd buffered mode for: `{module._gptqmodel_name}`")
+            logger.info(f"Experimental: enabling fwd buffered mode for: `{module.name}`")
             tmp.fwd_inputs_buffered = True
 
         tmp.quantizer.configure(
@@ -58,7 +59,7 @@ class GPTQProcessor(LoopProcessor):
             g.add_batch(inp[0].data, out.data)  # noqa: F821
         return tmp
 
-    def process(self, module: Module, state: Dict[str, ], pb: ProgressBar , fwd_time: int):
+    def process(self, module: NamedModule, state: Dict[str, ], pb: ProgressBar , fwd_time: int):
         # pb.set_description(f"Quantizing {name} in layer {module_index} of {layer_count - 1}")
         gptq = self.tasks
 
@@ -69,14 +70,14 @@ class GPTQProcessor(LoopProcessor):
 
         # dynamic overrides
         if self.qcfg.dynamic is not None:
-            group_size = self.qcfg.dynamic_get(module._gptqmodel_full_name, "group_size", group_size)
-            desc_act = self.qcfg.dynamic_get(module._gptqmodel_full_name, "desc_act", desc_act)
-            damp_percent = self.qcfg.dynamic_get(module._gptqmodel_full_name, "damp_percent", damp_percent)
-            static_groups = self.qcfg.dynamic_get(module._gptqmodel_full_name, "static_groups", static_groups)
+            group_size = self.qcfg.dynamic_get(module.full_name, "group_size", group_size)
+            desc_act = self.qcfg.dynamic_get(module.full_name, "desc_act", desc_act)
+            damp_percent = self.qcfg.dynamic_get(module.full_name, "damp_percent", damp_percent)
+            static_groups = self.qcfg.dynamic_get(module.full_name, "static_groups", static_groups)
 
         # logger.info(f"Quantizing module START: {name}, {gptq[name].shape()}")
         ## Need to return the quantized_weight for offloading
-        scale, zero, g_idx, duration, avg_loss, damp_percent, q_full_weight = gptq[module._gptqmodel_name].quantize(
+        scale, zero, g_idx, duration, avg_loss, damp_percent, q_full_weight = gptq[module.name].quantize(
             percdamp=damp_percent,
             group_size=group_size,
             actorder=desc_act,
@@ -104,13 +105,13 @@ class GPTQProcessor(LoopProcessor):
         #     )
         self.durations.append(duration)
         self.avg_losses.append(avg_loss)
-        self.module_names.append(f"layer-{module._gptqmodel_layer_index}-{module._gptqmodel_name}")
+        self.module_names.append(f"layer-{module.layer_index}-{module.name}")
 
-        stat = {QUANT_LOG_LAYER: module._gptqmodel_layer_index, QUANT_LOG_MODULE: module._gptqmodel_name, QUANT_LOG_LOSS: f"{avg_loss:.5f}",
+        stat = {QUANT_LOG_LAYER: module.layer_index, QUANT_LOG_MODULE: module.name, QUANT_LOG_LOSS: f"{avg_loss:.5f}",
                 QUANT_LOG_DAMP: f"{damp_percent:.5f}", QUANT_LOG_TIME: f"{duration:.3f}",
                 QUANT_LOG_FWD_TIME: f"{fwd_time:.3f}"}
         if self.qcfg.dynamic is not None:
-            stat["dynamic"] = self.qcfg.dynamic_get(layer_name=module._gptqmodel_full_name)
+            stat["dynamic"] = self.qcfg.dynamic_get(layer_name=module.full_name)
 
         self.quant_log.append(stat)
         logger.info(stat)
@@ -121,7 +122,7 @@ class GPTQProcessor(LoopProcessor):
         #     move_to(zero, CPU),
         #     move_to(g_idx, CPU),
         # )
-        gptq[module._gptqmodel_name].free()
+        gptq[module.name].free()
         # logger.info(f"Quantizing module END: {name}, {gptq[name].shape()}")
         return {
             "scale": scale,
