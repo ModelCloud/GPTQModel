e04a2b0827b33929bd5649e4ede2e3330875fcc0
CSY <csy@modelcloud.ai>
2025-02-14 20:41:09 +0800

    fix import

--- gptqmodel/quantization/quantizer.py
+++ gptqmodel/quantization/quantizer.py
@@ -20,7 +20,7 @@ import torch
 import torch.nn as nn
 
 from gptqmodel.quantization import QuantizeConfig
-from .gptq import HF_OPTIMUM
+from ..quantization.gptq import HF_OPTIMUM
 from ..utils.logger import setup_logger
 
 logger = setup_logger()
