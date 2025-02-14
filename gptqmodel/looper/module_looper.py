879b46483919af93917bd02b8c1dad9424826b65
CSY <csy@modelcloud.ai>
2025-02-14 09:40:16 +0800

    fix no super() for class obj

--- gptqmodel/looper/module_looper.py
+++ gptqmodel/looper/module_looper.py
@@ -21,10 +21,7 @@ class ModuleLooper():
         self.model = None
 
     def __getattr__(self, item):
-        try:
-            return super().__getattr__(item)
-        except Exception:
-            return getattr(self.model, item)
+        getattr(self.model, item)
 
     def cache_inputs(self, layers, auto_gc, calibration_dataset, calibration_enable_gpu_cache):
         layer_inputs = []
