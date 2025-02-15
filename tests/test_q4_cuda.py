bf4f5718a74ce3667f53a7adf54da4ef327d9427
CSY <csy@modelcloud.ai>
2025-02-15 21:29:28 +0800

    remove 3bits test in q4 cuda

--- tests/test_q4_cuda.py
+++ tests/test_q4_cuda.py
@@ -77,14 +77,3 @@ class TestsQ4CUDA(ModelTest):
         self.assertInference(model=model_q,tokenizer=self.tokenizer)
         # This one does not.
         self.assertInference(model=model_q.model,tokenizer=self.tokenizer)
-
-
-    def test_3bit(self):
-        self.NATIVE_MODEL_ID="/monster/data/model/Llama-3.2-1B-Instruct"
-        self.NATIVE_ARC_CHALLENGE_ACC = 0.3567
-        self.NATIVE_ARC_CHALLENGE_ACC_NORM = 0.3805
-        self.QUANT_ARC_MAX_DELTA_FLOOR_PERCENT = 0.40
-
-        self.QUANTIZE_CONFIG_BITS = 3
-
-        self.quant_lm_eval()
