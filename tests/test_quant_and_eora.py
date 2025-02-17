ce1312247b5d7b5a8fc89f8243d1d97c8d1ec203
Qubitium <Qubitium@modelcloud.ai>
2025-02-17 12:57:16 +0000

    clean up `test_quant_erao` so we have config at top and print config before lm-eval results
    # Conflicts:
    #	tests/test_quant_and_eora.py

--- tests/test_quant_and_eora.py
+++ tests/test_quant_and_eora.py
@@ -21,7 +21,7 @@ os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 
 import tempfile  # noqa: E402
 from typing import Optional  # noqa: E402
-
+from tabulate import tabulate # noqa: E402
 from datasets import load_dataset  # noqa: E402
 from gptqmodel import BACKEND, GPTQModel, QuantizeConfig  # noqa: E402
 from gptqmodel.adapter.adapter import Lora  # noqa: E402
@@ -60,9 +60,9 @@ def bench(path: str, backend: BACKEND, adapter: Optional[Lora]):
 
     return bench_result
 
-
 class Test(ModelTest):
-    NATIVE_MODEL_ID = "/monster/data/model/Qwen2.5-0.5B-Instruct/"
+    #NATIVE_MODEL_ID = "/monster/data/model/Qwen2.5-0.5B-Instruct/"
+    NATIVE_MODEL_ID = "meta-llama/Llama-3.2-1B"
 
     NATIVE_ARC_CHALLENGE_ACC = 0.3567
     NATIVE_ARC_CHALLENGE_ACC_NORM = 0.3805
@@ -70,30 +70,56 @@ class Test(ModelTest):
 
     @classmethod
     def setUpClass(cls):
-        cls.calibration_dataset = load_dataset(
+        pass
+
+    def test_quant_and_eora(self):
+        bits = 4
+        group_size = 64
+        desc_act = True
+        rank = 256
+        batch_size = 1
+        calibration_dataset_rows = 1024
+        calibration_dataset_concat_size = 0 # disable
+        auto_gc = False
+        adapter_file_name = "eora.safetensors"
+
+        config_dict = {
+            "bits": bits,
+            "group_size": group_size,
+            "desc_act": desc_act,
+            "rank": rank,
+            "batch_size": batch_size,
+            "calibration_dataset_rows": calibration_dataset_rows,
+            "calibration_dataset_concat_size": calibration_dataset_concat_size,
+            "auto_gc": auto_gc,
+            "adapter_file_name": adapter_file_name,
+        }
+
+        calibration_dataset = load_dataset(
             "allenai/c4",
             data_files="en/c4-train.00001-of-01024.json.gz",
             split="train"
-        ).select(range(128))["text"]
+        ).select(range(calibration_dataset_rows))["text"]
 
-    def test_quant_and_eora(self):
         with tempfile.TemporaryDirectory() as tmpdir:
             eora = Lora(
                 # for quant, path is save path. for load, it is loading path
-                path=os.path.join(tmpdir, "lora_adapter.safetensors"),
-                rank=512,
+                path=os.path.join(tmpdir, adapter_file_name),
+                rank=rank,
             )
 
             quant_config = QuantizeConfig(
-                bits=4,
-                group_size=32,
-                desc_act=True,  # bitblas only supports DESC_ACT=False
+                bits=bits,
+                group_size=group_size,
+                desc_act=desc_act,  # bitblas only supports DESC_ACT=False
                 adapter=eora
             )
 
-            model = GPTQModel.load(self.NATIVE_MODEL_ID, quant_config)
+            model = GPTQModel.load(
+                model_id_or_path=self.NATIVE_MODEL_ID,
+                quantize_config=quant_config)
 
-            model.quantize(self.calibration_dataset, batch_size=1, auto_gc=False)
+            model.quantize(calibration_dataset, batch_size=batch_size, auto_gc=auto_gc, calibration_dataset_concat_size=calibration_dataset_concat_size) #
 
             # EoRA adapter is saved according to Lora.path property
             # if Lora.path is not set, we will save the lora as "lora.safetensors" in the same path as quant model
@@ -104,51 +130,22 @@ class Test(ModelTest):
             torch_empty_cache()
 
             # BACKEND.EXLLAMA_V2, BACKEND.EXLLAMA_V1, BACKEND.TRITON, BACKEND.CUDA,
-            for backend in [BACKEND.TORCH]:  # BACKEND.IPEX, BACKEND.BITBLAS, BACKEND.EXLLAMA_V2V BACKEND.MARLIN
-                base_bench = bench(path=tmpdir, backend=backend, adapter=None)  # inference using qweights only
-                eora_bench = bench(path=tmpdir, backend=backend, adapter=eora)  # inference using eora (lora)
+            for backend in [ BACKEND.TORCH ]: # BACKEND.IPEX, BACKEND.BITBLAS, BACKEND.EXLLAMA_V2V BACKEND.MARLIN
+                base_bench = bench(path=tmpdir, backend=backend, adapter=None) # inference using qweights only
+                eora_bench = bench(path=tmpdir, backend=backend, adapter=eora) # inference using eora (lora)
+
+                print('--------Quant/EoRA Config ---------')
+
+                # Convert the dictionary to a list of lists for tabulate
+                table_data = [[key, value] for key, value in config_dict.items()]
+                print(tabulate(table_data, headers=["Key", "Value"], tablefmt="grid"))
 
                 print('--------Eval Base Result---------')
                 print(make_table(base_bench))
                 if "groups" in base_bench:
                     print(make_table(base_bench, "groups"))
-                # print('--------Eval Base Result End---------')
 
                 print('--------Eval EoRA Result---------')
                 print(make_table(eora_bench))
                 if "groups" in eora_bench:
                     print(make_table(eora_bench, "groups"))
-                # print('--------Eval EoRA Result End---------')
-
-    def test_eora_post_quant(self):
-        with tempfile.TemporaryDirectory() as tmpdir:
-            eora = Lora(
-                # for quant, path is save path. for load, it is loading path
-                path=os.path.join(tmpdir, "lora_adapter.safetensors"),
-                rank=512,
-            )
-
-            quantized_model_path = "/monster/data/model/Qwen2.5-0.5B-Instruct-gptq-4bit"
-
-            GPTQModel.eora_generate(model_id_or_path=self.NATIVE_MODEL_ID,
-                                    quantized_model_id_or_path=quantized_model_path, adapter=eora,
-                                    calibration_dataset=self.calibration_dataset)
-
-            # BACKEND.EXLLAMA_V2, BACKEND.EXLLAMA_V1, BACKEND.TRITON, BACKEND.CUDA,
-            for backend in [BACKEND.TORCH]:  # BACKEND.IPEX, BACKEND.BITBLAS, BACKEND.EXLLAMA_V2V BACKEND.MARLIN
-                base_bench = bench(path=quantized_model_path, backend=backend,
-                                   adapter=None)  # inference using qweights only
-                eora_bench = bench(path=quantized_model_path, backend=backend,
-                                   adapter=eora)  # inference using eora (lora)
-
-                print('--------Eval Base Result---------')
-                print(make_table(base_bench))
-                if "groups" in base_bench:
-                    print(make_table(base_bench, "groups"))
-                # print('--------Eval Base Result End---------')
-
-                print('--------Eval EoRA Result---------')
-                print(make_table(eora_bench))
-                if "groups" in eora_bench:
-                    print(make_table(eora_bench, "groups"))
-                # print('--------Eval EoRA Result End---------')
