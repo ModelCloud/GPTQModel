9a55290a2de4a70c063babaf605bb15f1114d05e
LRL-ModelCloud <lrl@lbx.dev>
2025-03-06 16:26:34 +0800

    add TestInstella

---
+++ tests/models/test_instella.py
@@ -0,0 +1,30 @@
+# Copyright 2024-2025 ModelCloud.ai
+# Copyright 2024-2025 qubitium@modelcloud.ai
+# Contact: qubitium@modelcloud.ai, x.com/qubitium
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from model_test import ModelTest
+
+
+# The official THUDM/chatglm3-6b's tokenization_chatglm.py has compatibility issues with transformers.
+# It will throw a TypeError: ChatGLMTokenizer._pad() got an unexpected keyword argument 'padding_side'
+# Adding a temporary padding_side parameter to the _pad method in tokenization_chatglm.py can prevent errors.
+class TestInstella(ModelTest):
+    NATIVE_MODEL_ID = "/monster/data/model/Instella-3B-Instruct/" 
+    NATIVE_ARC_CHALLENGE_ACC = 0.4377
+    NATIVE_ARC_CHALLENGE_ACC_NORM = 0.4804
+    TRUST_REMOTE_CODE = True
+
+    def test_instella(self):
+        self.quant_lm_eval()
