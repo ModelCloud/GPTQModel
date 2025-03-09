7a127f275c16c0c806eb2bdd85e12269be611c3e
Qubitium-ModelCloud <qubitium@modelcloud.ai>
2025-03-09 10:00:18 +0800

    update tests (#1398)
    Signed-off-by: Qubitium <Qubitium@modelcloud.ai>

--- tests/test_inference_speed.py
+++ tests/test_inference_speed.py
@@ -42,6 +42,7 @@ class TestInferenceSpeed(InferenceSpeed):
 
     @parameterized.expand(
         [
+            (InferenceSpeed.NATIVE_MODEL_ID, BACKEND.EXLLAMA_EORA, 282.64, False, False),
             (InferenceSpeed.NATIVE_MODEL_ID, BACKEND.MARLIN, 286.74, False, False),
             (InferenceSpeed.NATIVE_MODEL_ID, BACKEND.TORCH, 176.00, False, False),
             # (InferenceSpeed.NATIVE_MODEL_ID, BACKEND.TORCH, 53, False, False),
