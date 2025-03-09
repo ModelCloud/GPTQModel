72fa65a9e1b1edbd99f7df9c681d1c23204b9074
Qubitium <Qubitium@modelcloud.ai>
2025-03-09 01:59:41 +0000

    update tests
    Signed-off-by: Qubitium <Qubitium@modelcloud.ai>

--- tests/test_kernel_output.py
+++ tests/test_kernel_output.py
@@ -78,37 +78,37 @@ class TestKernelOutput(unittest.TestCase):
 
         return result
 
-    def assert_on_mismatch(self, a: Tensor, b: Tensor, rtol=0.00005, atol=0.00005):
-        torch.testing.assert_close(a, b, rtol=rtol, atol=atol)
+    def assert_on_mismatch(self, a: Tensor, b: Tensor, atol):
+        torch.testing.assert_close(a, b, rtol=0.15, atol=atol)
 
     @parameterized.expand([
-        (BACKEND.TORCH, 0.0000, 0.0000),
-        (BACKEND.TRITON, 0.00001, 0.00001),
-        (BACKEND.EXLLAMA_V1, 0.09, 0.0001),
-        (BACKEND.EXLLAMA_V2, 0.136, 0.0001),
-        (BACKEND.MARLIN, 0.00005, 0.00005),
-        (BACKEND.MARLIN_FP16, 0.0001, 0.0035),
-        (BACKEND.EXLLAMA_EORA, 0.00001, 0.00147)
+        (BACKEND.TORCH, 0.0000),
+        (BACKEND.TRITON, 0.00001),
+        (BACKEND.EXLLAMA_V1, 0.00075),
+        (BACKEND.EXLLAMA_V2, 0.00075),
+        (BACKEND.MARLIN, 0.00005),
+        (BACKEND.MARLIN_FP16, 0.0035),
+        (BACKEND.EXLLAMA_EORA, 0.00099)
     ])
-    def test_kernel_output(self, backend: BACKEND, r_tolerance: float, a_tolerance: float):
+    def test_kernel_output(self, backend: BACKEND, a_tolerance: float):
         out = self.forward(backend=backend)
 
         log.info(f"backend: {backend} ")
         log.info(out[0][:100])
 
         # torch vs exllama v1
-        self.assert_on_mismatch(self.torch_kernel_out, out, r_tolerance, a_tolerance)  # use torch as reference
+        self.assert_on_mismatch(self.torch_kernel_out, out, a_tolerance)  # use torch as reference
 
     @parameterized.expand([
-        (BACKEND.TORCH, 0.0000, 0.0000),
-        (BACKEND.TRITON, 0.00001, 0.00001),
-        (BACKEND.EXLLAMA_V1, 0.015, 0.0008),
-        (BACKEND.EXLLAMA_V2, 0.16, 0.0003),
-        (BACKEND.MARLIN, 0.00001, 0.00003),
-        (BACKEND.MARLIN_FP16, 0.0001, 0.0035),
-        (BACKEND.EXLLAMA_EORA, 0.00001, 0.00147)
+        (BACKEND.TORCH, 0.0000),
+        (BACKEND.TRITON, 0.00001),
+        (BACKEND.EXLLAMA_V1, 0.00075),
+        (BACKEND.EXLLAMA_V2, 0.00075),
+        (BACKEND.MARLIN, 0.00003),
+        (BACKEND.MARLIN_FP16, 0.0035),
+        (BACKEND.EXLLAMA_EORA, 0.00196)
     ])
-    def test_kernel_output_with_lora(self, backend: BACKEND, r_tolerance: float, a_tolerance: float):
+    def test_kernel_output_with_lora(self, backend: BACKEND, a_tolerance: float):
         out = self.forward(backend=backend, adapter=self.adapter)
 
         print(f"torch output with lora {self.torch_kernel_out_with_lora[0][:10]}")
@@ -116,4 +116,4 @@ class TestKernelOutput(unittest.TestCase):
         print(out[0][:10])
 
         # compare kernel output with torch kernel reference
-        self.assert_on_mismatch(self.torch_kernel_out_with_lora, out, r_tolerance, a_tolerance)  # use torch as reference
+        self.assert_on_mismatch(self.torch_kernel_out_with_lora, out, a_tolerance)  # use torch as reference
