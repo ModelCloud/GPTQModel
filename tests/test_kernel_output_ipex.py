90748fea14045e684d73085237c68bc601dc8a0a
jiqing-feng <jiqing.feng@intel.com>
2025-03-14 13:42:57 +0000

    enable different size tests
    Signed-off-by: jiqing-feng <jiqing.feng@intel.com>

--- tests/test_kernel_output_ipex.py
+++ tests/test_kernel_output_ipex.py
@@ -20,21 +20,23 @@ class TestKernelOutput(unittest.TestCase):
     }
     target = 'model.layers.6.self_attn.v_proj'
     device_map = "cpu"
-    m = 8
+    m = [1, 16, 64, 256, 1024]
     k = 2048
     torch_dtype = torch.float16
     r_tolerance = 0.0
     a_tolerance = 0.01
-    random_input_samples = 100
+    input_samples_each_size = 20
 
     @classmethod
     def setUp(self):
         self.torch_model = GPTQModel.load(self.model_path, backend=BACKEND.TORCH, device_map=self.device_map, torch_dtype=self.torch_dtype)
         self.x = []
         self.torch_kernel_outs = []
-        for i in range(self.random_input_samples):
-            self.x.append(torch.rand((self.m, self.k), dtype=self.torch_dtype))
-            self.torch_kernel_outs.append(self.forward(self, self.torch_model, self.x[i], backend=BACKEND.TORCH))
+        for dim_0 in self.m:
+            for _ in range(self.input_samples_each_size):
+                inputs = torch.rand((dim_0, self.k), dtype=self.torch_dtype)
+                self.x.append(inputs)
+                self.torch_kernel_outs.append(self.forward(self, self.torch_model, inputs, backend=BACKEND.TORCH))
 
     def forward(self, model, x, backend: BACKEND):
         target_qlinear_cls = self.target_qliner_map[backend]
@@ -62,7 +64,7 @@ class TestKernelOutput(unittest.TestCase):
         model = GPTQModel.load(self.model_path, backend=backend, device_map=self.device_map, torch_dtype=self.torch_dtype)
         log.info(f"device_map: {self.device_map} ")
         log.info(f"backend: {backend} ")
-        for i in range(self.random_input_samples):
+        for i in range(len(self.x)):
             out = self.forward(model, self.x[i], backend=backend)
             self.assert_on_mismatch(self.torch_kernel_outs[i], out, r_tolerance, a_tolerance)  # use torch as reference
 
