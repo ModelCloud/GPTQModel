6740088ecc67a45d88db5e0f38f666857752e265
jiqing-feng <jiqing.feng@intel.com>
2025-03-14 11:02:17 +0000

    enable ipex
    Signed-off-by: jiqing-feng <jiqing.feng@intel.com>

--- tests/test_kernel_output_ipex.py
+++ tests/test_kernel_output_ipex.py
@@ -12,31 +12,28 @@ from torch import Tensor
 
 log = LogBar.shared()
 
-XPU = torch.device("xpu")
 
 class TestKernelOutput(unittest.TestCase):
     model_path = "sliuau/llama3.2-1b-4bit-group128" # hf "sliuau/llama3.2-1b-4bit-group128"
-
     target_qliner_map = {
         BACKEND.TORCH: TorchQuantLinear,
         BACKEND.IPEX: IPEXQuantLinear,
     }
-
     target = 'model.layers.6.self_attn.v_proj'
+    device_map = "cpu"
+    m = 1
+    k = 2048
+    torch_dtype = torch.float16
+    r_tolerance = 0.7
+    a_tolerance = 0.005
 
     @classmethod
-    def setUpClass(cls):
+    def setUp(self):
+        self.x = torch.rand((self.m, self.k), dtype=self.torch_dtype)
+        self.torch_kernel_out = self.forward(self, self.x, backend=BACKEND.TORCH)
 
-        cls.m = 1
-        cls.k = 2048
-        cls.x = torch.rand((cls.m, cls.k), device=XPU, dtype=torch.float16)
-
-        # TORCH as reference output
-        cls.torch_kernel_out = cls.forward(cls, backend=BACKEND.TORCH)
-
-
-    def forward(self, backend: BACKEND, adapter: Adapter = None):
-        model = GPTQModel.load(self.model_path, backend=backend, adapter=adapter)
+    def forward(self, x, backend: BACKEND, adapter: Adapter = None):
+        model = GPTQModel.load(self.model_path, backend=backend, adapter=adapter, device_map=self.device_map, torch_dtype=self.torch_dtype)
 
         target_qlinear_cls = self.target_qliner_map[backend]
 
@@ -44,14 +41,13 @@ class TestKernelOutput(unittest.TestCase):
         result = None
         for name, module in modules.items():
             if name == self.target:
-                result = module(self.x)
+                result = module(x.to(model.device))
                 break
 
         assert result is not None
 
         del module
         del model
-        torch.cuda.empty_cache()
 
         return result
 
@@ -61,12 +57,26 @@ class TestKernelOutput(unittest.TestCase):
     @parameterized.expand([
         (BACKEND.TORCH, 0.0000, 0.00025),
         # (BACKEND.TRITON,  0.0000, 0.00025),
-        (BACKEND.IPEX,  0.0000, 0.0005),
+        (BACKEND.IPEX,  r_tolerance, a_tolerance),
     ])
     def test_kernel_output(self, backend: BACKEND, r_tolerance: float, a_tolerance: float):
-        out = self.forward(backend=backend)
+        out = self.forward(self.x, backend=backend)
 
+        log.info(f"device_map: {self.device_map} ")
         log.info(f"backend: {backend} ")
         log.info(out[0][:100])
 
         self.assert_on_mismatch(self.torch_kernel_out, out, r_tolerance, a_tolerance)  # use torch as reference
+
+
+class TestKernelOutputBFloat16(TestKernelOutput):
+    torch_dtype = torch.bfloat16
+
+
+@unittest.skipUnless(hasattr(torch, "xpu") and torch.xpu.is_available, reason="Test requires XPU")
+class TestKernelOutputXPU(TestKernelOutput):
+    device_map = "xpu:0"
+
+
+class TestKernelOutputBFloat16(TestKernelOutputXPU):
+    torch_dtype = torch.bfloat16
