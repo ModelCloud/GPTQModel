26f074aacd5b2aadfe33a68e9c4fd6117b936d24
ZX-ModelCloud <zx@modelcloud.ai>
2025-04-06 15:52:04 +0800

    fix qkvo forward when every 4 layer
    Signed-off-by: ZX-ModelCloud <zx@modelcloud.ai>

--- gptqmodel/models/definitions/llama4.py
+++ gptqmodel/models/definitions/llama4.py
@@ -27,8 +27,7 @@ class Llama4GPTQ(BaseGPTQModel):
     layer_type = "Llama4TextDecoderLayer"
 
     layer_modules = [
-        ["self_attn.k_proj", "self_attn.v_proj", "self_attn.q_proj"],
-        ["self_attn.o_proj"],
+        ["self_attn.k_proj", "self_attn.v_proj", "self_attn.q_proj", "self_attn.o_proj"],
 
         ["feed_forward.shared_expert.gate_proj", "feed_forward.shared_expert.up_proj"],
         ["feed_forward.shared_expert.down_proj"],
