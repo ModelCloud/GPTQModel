2d34c258f9a411f52f50a7b5879f1a437f5e4d3d
Qubitium-ModelCloud <qubitium@modelcloud.ai>
2025-04-30 15:56:18 +0800

    add xiomi mimo model support (#1571)
    Signed-off-by: Qubitium <Qubitium@modelcloud.ai>

---
+++ gptqmodel/models/definitions/mimo.py
@@ -0,0 +1,46 @@
+# Copyright 2024-2025 ModelCloud.ai
+# Copyright 2024-2025 qubitium@modelcloud.ai
+# Contact: qubitium@modelcloud.ai, x.com/qubitium
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from ..base import BaseGPTQModel
+
+
+# MiMo is based on Qwen2
+# https://huggingface.co/XiaomiMiMo/MiMo-7B-RL/blob/main/modeling_mimo.py
+class MimoGPTQ(BaseGPTQModel):
+    require_trust_remote_code = True
+    layer_type = "MiMoMTPLayers"
+
+    base_modules = ["model.embed_tokens", "model.norm"]
+    pre_lm_head_norm_module = "model.norm"
+
+    layers_node = ["model.layers"]
+
+    layer_modules = [
+        ["self_attn.k_proj", "self_attn.v_proj", "self_attn.q_proj"],
+        ["self_attn.o_proj"],
+        ["mlp.up_proj", "mlp.gate_proj"],
+        ["mlp.down_proj"],
+    ]
+
+    layers_modules_tree = [
+        "model",
+        "layers",
+        "#",
+        {
+            "self_attn": ("k_proj", "v_proj", "q_proj", "o_proj"),
+            "mlp": ("up_proj", "gate_proj", "down_proj"),
+        }
+    ]
