4545a655ca5c4a6690f92f57f3a16dba6954d552
CSY-ModelCloud <csy@modelcloud.ai>
2025-05-06 17:24:27 +0800

    [CI] reversed LIST

--- .github/scripts/release_map.py
+++ .github/scripts/release_map.py
@@ -1,5 +1,3 @@
-import json
-
 cu_list = [118, 121, 124, 126, 128]
 cp_list = [39, 310, 311, 312, 313]
 torch_list = ["2.0.1", "2.1.2", "2.2.2", "2.3.1", "2.4.1", "2.5.1", "2.6.0", "2.7.0"]
@@ -13,16 +11,18 @@ def is_valid_combo(cu, torch, cp):
             (torch == "2.5.1" and cu < 126) or \
             (torch == "2.6.0" and cu >= 126) or \
             (torch == "2.7.0" and cu >= 126):
+        print(f'- cuda: {cu}')
+        print(f'  torch: {torch}')
+        print(f'  python: {cp}')
         return True
     return False
 
 task_map = {}
 
-for cu in cu_list:
+for cu in reversed(cu_list):
     task_map[str(cu)] = {}
-    for cp in cp_list:
+    for cp in reversed(cp_list):
         task_map[str(cu)][str(cp)] = {}
-        for torch in torch_list:
+        for torch in reversed(torch_list):
             task_map[str(cu)][str(cp)][torch] = int(is_valid_combo(cu, torch, cp))
 
-print(json.dumps(task_map))
