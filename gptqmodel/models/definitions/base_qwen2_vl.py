d959c6c6ada3ad1ac36375cb0b01a44f770f4e79
LRL-ModelCloud <lrl@lbx.dev>
2025-05-29 16:21:34 +0800

    add language_model

--- gptqmodel/models/definitions/base_qwen2_vl.py
+++ gptqmodel/models/definitions/base_qwen2_vl.py
@@ -32,7 +32,7 @@ class BaseQwen2VLGPTQ(BaseGPTQModel):
     base_modules = ["model.embed_tokens", "model.norm"]
     pre_lm_head_norm_module = "model.norm"
 
-    layers_node = ["model.layers"]
+    layers_node = ["model.layers", "model.language_model.layers"]
 
     layer_modules = [
         ["self_attn.k_proj", "self_attn.v_proj", "self_attn.q_proj"],
@@ -43,6 +43,7 @@ class BaseQwen2VLGPTQ(BaseGPTQModel):
 
     layers_modules_tree = [
         "model",
+        "language_model",
         "layers",
         "#",
         {
