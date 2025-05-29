25cf29ad871a70cba8a19b67a07d24c5ed12c277
LRL-ModelCloud <165116337+LRL-ModelCloud@users.noreply.github.com>
2025-05-29 17:19:50 +0800

    [FIX] Qwen2.5 vl quant (#1623)
    * add language_model
    
    * fix layer hook

--- gptqmodel/models/definitions/base_qwen2_vl.py
+++ gptqmodel/models/definitions/base_qwen2_vl.py
@@ -32,7 +32,7 @@ class BaseQwen2VLGPTQ(BaseGPTQModel):
     base_modules = ["model.embed_tokens", "model.norm"]
     pre_lm_head_norm_module = "model.norm"
 
-    layers_node = ["model.layers"]
+    layers_node = ["model.layers", "model.language_model.layers"]
 
     layer_modules = [
         ["self_attn.k_proj", "self_attn.v_proj", "self_attn.q_proj"],
@@ -41,13 +41,19 @@ class BaseQwen2VLGPTQ(BaseGPTQModel):
         ["mlp.down_proj"],
     ]
 
-    layers_modules_tree = [
-        "model",
-        "layers",
+    branch = [
         "#",
         {
             "self_attn": ("k_proj", "v_proj", "q_proj", "o_proj"),
             "mlp": ("up_proj", "gate_proj", "down_proj"),
+        }
+    ]
+
+    layers_modules_tree = [
+        "model",
+        {
+            "layers": branch,
+            "language_model": {"layers": branch},
         }
     ]
 
