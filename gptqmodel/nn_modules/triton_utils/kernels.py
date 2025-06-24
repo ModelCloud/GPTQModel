251224794250748b666756623574d75c11e9ad1e
kaixuanliu <kaixuan.liu@intel.com>
2025-06-24 13:48:35 +0800

    fix bug for device error (#1631)
    Signed-off-by: Liu, Kaixuan <kaixuan.liu@intel.com>

--- gptqmodel/nn_modules/triton_utils/kernels.py
+++ gptqmodel/nn_modules/triton_utils/kernels.py
@@ -422,7 +422,7 @@ def transpose_quant_matmul_248(input, qweight, scales, qzeros, g_idx, bits, maxq
 
 class QuantLinearFunction(torch.autograd.Function):
     @staticmethod
-    @custom_bwd(device_type="xpu" if HAS_XPU else "cuda")
+    @custom_fwd(device_type="xpu" if HAS_XPU else "cuda")
     def forward(ctx, input, qweight, scales, qzeros, g_idx, bits, maxq):
         output = quant_matmul_248(input, qweight, scales, qzeros, g_idx, bits, maxq)
         ctx.save_for_backward(qweight, scales, qzeros, g_idx)
