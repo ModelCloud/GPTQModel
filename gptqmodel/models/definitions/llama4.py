1bfe8b25d5ce762ba0a22eef0abd6749ee32a92e
LRL2-ModelCloud <lrl2@modelcloud.ai>
2025-09-02 17:35:41 +0800

    add before_model_load

--- gptqmodel/models/definitions/llama4.py
+++ gptqmodel/models/definitions/llama4.py
@@ -41,6 +41,53 @@ class Llama4GPTQ(BaseGPTQModel):
         ["feed_forward.shared_expert.gate_proj", "feed_forward.shared_expert.up_proj", "feed_forward.shared_expert.down_proj"],
     ]
 
+    def before_model_load(self, load_quantized_model=False):
+        if load_quantized_model:
+            import torch
+            import torch.nn as nn
+            import transformers.models.llama4.modeling_llama4 as llama4_modeling
+            from transformers.integrations.hub_kernels import use_kernel_forward_from_hub
+
+            @use_kernel_forward_from_hub("Llama4TextMoe")
+            class SequentialLlama4TextMoe(torch.nn.Module):
+                def __init__(self, config):
+                    super().__init__()
+                    self.top_k = config.num_experts_per_tok
+                    self.hidden_dim = config.hidden_size
+                    print(config)
+                    self.num_experts = 16
+                    self.experts = nn.ModuleList(
+                        [llama4_modeling.Llama4TextMLP(config) for _ in range(self.num_experts)]
+                    )
+                    self.router = llama4_modeling.Llama4Router(config)
+                    self.shared_expert = llama4_modeling.Llama4TextMLP(config)  
+
+                def forward(self, hidden_states: torch.Tensor):
+                    hidden_states = hidden_states.reshape(-1, self.hidden_dim)
+                    router_logits = self.router(hidden_states)
+                    if isinstance(router_logits, tuple):
+                        router_scores, router_logits = router_logits
+                        router_scores = router_scores.t()
+                    else:
+                        # transformers < 4.54.0 only returns router_logits
+                        router_top_value, router_indices = torch.topk(router_logits, self.top_k, dim=1)
+
+                        router_scores = (
+                            torch.full_like(router_logits, float("-inf"))
+                            .scatter_(1, router_indices, router_top_value)
+                            .transpose(0, 1)
+                        )
+                        router_scores = torch.sigmoid(router_scores.float()).to(hidden_states.dtype)
+
+                    out = self.shared_expert(hidden_states)
+                    for i in range(self.num_experts):
+                        out += self.experts[i](hidden_states) * router_scores[i].reshape(-1, 1)
+
+                    return out, router_logits
+
+            llama4_modeling.Llama4TextMoe = SequentialLlama4TextMoe
+
+
     def after_model_load(self, model, load_quantized_model=False):
         if load_quantized_model:
             return model
