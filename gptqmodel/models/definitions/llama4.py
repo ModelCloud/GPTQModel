ecae5100aa5199e87cc14625f8a9a55830859ce2
LRL2-ModelCloud <lrl2@modelcloud.ai>
2025-09-02 11:54:59 +0800

    llama4 add after_model_load

--- gptqmodel/models/definitions/llama4.py
+++ gptqmodel/models/definitions/llama4.py
@@ -17,8 +17,7 @@
 from transformers import AutoModelForImageTextToText
 
 from ..base import BaseGPTQModel
-
-
+        
 class Llama4GPTQ(BaseGPTQModel):
     # some bug in the attention_mask of transformers.modeling_llama4,
     # so batch quantization for Llama4 is temporarily not supported.
@@ -36,3 +35,73 @@ class Llama4GPTQ(BaseGPTQModel):
 
         ["feed_forward.shared_expert.gate_proj", "feed_forward.shared_expert.up_proj", "feed_forward.shared_expert.down_proj"],
     ]
+
+    def after_model_load(self, model, load_quantized_model=False):
+        if load_quantized_model:
+            return model
+
+        from transformers.models.llama4.modeling_llama4 import Llama4TextMLP
+
+        class SequentialLlama4TextExperts(torch.nn.ModuleList):
+            def __init__(self, config, original):
+                self.num_experts = original.gate_up_proj.shape[0]
+                with no_init_weights():
+                    super().__init__([Llama4TextMLP(config) for _ in range(self.num_experts)])
+                intermediate_size = original.down_proj.shape[1]
+
+                for i in range(self.num_experts):
+                    gate_up = original.gate_up_proj[i]
+                    down = original.down_proj[i]
+                    gate_proj = gate_up[:, :intermediate_size]
+                    up_proj = gate_up[:, intermediate_size:]
+
+                    self[i].gate_proj.weight.data = gate_proj.t().contiguous()
+                    self[i].up_proj.weight.data = up_proj.t().contiguous()
+                    self[i].down_proj.weight.data = down.t().contiguous()
+
+        class SequentialLlama4TextMoe(torch.nn.Module):
+            def __init__(self, config, original):
+                super().__init__()
+                self.top_k = config.num_experts_per_tok
+                self.hidden_dim = config.hidden_size
+                self.num_experts = config.num_local_experts
+                self.experts = SequentialLlama4TextExperts(config, original.experts)
+                self.router = original.router
+                self.shared_expert = original.shared_expert
+
+            def forward(self, hidden_states: torch.Tensor):
+                hidden_states = hidden_states.reshape(-1, self.hidden_dim)
+                router_logits = self.router(hidden_states)
+                if isinstance(router_logits, tuple):
+                    router_scores, router_logits = router_logits
+                    router_scores = router_scores.t()
+                else:
+                    # transformers < 4.54.0 only returns router_logits
+                    router_top_value, router_indices = torch.topk(router_logits, self.top_k, dim=1)
+
+                    router_scores = (
+                        torch.full_like(router_logits, float("-inf"))
+                        .scatter_(1, router_indices, router_top_value)
+                        .transpose(0, 1)
+                    )
+                    router_scores = torch.sigmoid(router_scores.float()).to(hidden_states.dtype)
+
+                out = self.shared_expert(hidden_states)
+                for i in range(self.num_experts):
+                    out += self.experts[i](hidden_states) * router_scores[i].reshape(-1, 1)
+
+                return out, router_logits
+
+        model = model.to("cpu")
+        def process_module(name, module, model, config):
+            if isinstance(module, Llama4TextMoe):
+                new_module = SequentialLlama4TextMoe(config=config, ori_mlp=module)
+                parent, child = name.rsplit(".", maxsplit=1)
+                parent = model.get_submodule(parent)
+                setattr(parent, child, new_module)
+        
+        with ThreadPoolExecutor(max_workers=os.cpu_count()) as executor:
+            process_fn = partial(process_module, model=model, config=model.config.get_text_config())
+            list(executor.map(lambda x: process_fn(x[0], x[1]), model.named_modules()))
+
+        return model
