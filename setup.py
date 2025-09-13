3f2611e8ce6e27b0bd9688068471f327165a6ff4
Qubitium <Qubitium@modelcloud.ai>
2025-09-13 23:39:20 +0000

    suppress unused var warnings
    Signed-off-by: Qubitium <Qubitium@modelcloud.ai>

--- setup.py
+++ setup.py
@@ -305,7 +305,7 @@ if BUILD_CUDA_EXT == "1":
                 "--expt-relaxed-constexpr",
                 "--expt-extended-lambda",
                 "--use_fast_math",
-                "-diag-suppress=179,39",
+                "-diag-suppress=179,39,177",
             ]
         else:
             # hipify CUDA-like flags
