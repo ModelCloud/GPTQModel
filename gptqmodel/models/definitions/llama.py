4334ab6c7928a967eab6681baa95874b7117256c
Qubitium <Qubitium@modelcloud.ai>
2025-09-15 02:56:35 +0000

    template for dynamic awq rules
    Signed-off-by: Qubitium <Qubitium@modelcloud.ai>

--- gptqmodel/models/definitions/llama.py
+++ gptqmodel/models/definitions/llama.py
@@ -49,58 +49,106 @@ class LlamaGPTQ(BaseGPTQModel):
     # List them in the order executed in model forward() code
     # Many models have same execution order of: attention (q_k_v) projection, attention (output) projection, mlp (n) projections
     layer_modules = [
-        ["self_attn.k_proj", "self_attn.v_proj", "self_attn.q_proj"],
+        ["input_layernorm!"],
+        ["self_attn.q_proj", "self_attn.k_proj", "self_attn.v_proj"],
+        ["input_layernorm!"],
         ["self_attn.o_proj"],
         ["mlp.gate_proj", "mlp.up_proj",],
         ["mlp.down_proj"],
     ]
 
+
     def get_layers_for_scaling(self, module, input_feat, module_kwargs):
-        layers = []
+        nodes = []
+        last_non_norm_last = None  # last layer obj of the previous non-norm block
+        last_norm = None  # most recent norm obj (from a '!...' block)
+        last_module_root = None # self_attn.* has root == self_attn, mlp.* has root == mlp
 
-        # attention input
-        layers.append(
-            dict(
-                prev_op=module.input_layernorm,
-                layers=[
-                    module.self_attn.q_proj,
-                    module.self_attn.k_proj,
-                    module.self_attn.v_proj,
-                ],
-                inp=input_feat["self_attn.q_proj"],
-                module2inspect=module.self_attn,
-                kwargs=module_kwargs,
-            )
-        )
+        for block in self.layer_modules:
+            is_norm_block = all(name.endswith("!") for name in block)
 
-        # attention out
-        # Please refer to https://github.com/mit-han-lab/llm-awq/pull/67#issue-1850622696
-        if module.self_attn.v_proj.weight.shape == module.self_attn.o_proj.weight.shape:
-            layers.append(
-                dict(
-                    prev_op=module.self_attn.v_proj,
-                    layers=[module.self_attn.o_proj],
-                    inp=input_feat["self_attn.o_proj"],
+            if is_norm_block:
+                # Remember the latest norm (use the last entry if multiple are present)
+                last_norm = get_attr(module, block[-1])
+                continue
+
+            # Normal execution block
+            layers = [get_attr(module, name) for name in block]
+            prev_op = last_non_norm_last if last_non_norm_last is not None else last_norm
+
+
+            n = dict(
+                    prev_op=prev_op,
+                    layers=layers,
+                    inp=input_feat[block[0]],
+                    kwargs=module_kwargs
                 )
-            )
 
-        # linear 1
-        layers.append(
-            dict(
-                prev_op=module.post_attention_layernorm,
-                layers=[module.mlp.gate_proj, module.mlp.up_proj],
-                inp=input_feat["mlp.gate_proj"],
-                module2inspect=module.mlp,
-            )
-        )
+            root_split = block[0].split(".", 2)
+            if len(root_split) == 2:
+                root = root_split[0]
+                if root != last_module_root:
+                    last_module_root = root
 
-        # linear 2
-        layers.append(
-            dict(
-                prev_op=module.mlp.up_proj,
-                layers=[module.mlp.down_proj],
-                inp=input_feat["mlp.down_proj"],
-            )
-        )
+                node["module2inspect"] = get_attr(module, root)
 
-        return layers
+
+            nodes.append(n)
+
+            # Update tracker to the LAST item of this non-norm block
+            last_non_norm_last = layers[-1]
+
+        print(f"DEBUG AWQ NODES: {nodes}")
+        return nodes
+
+
+    # def get_layers_for_scaling(self, module, input_feat, module_kwargs):
+    #     layers = []
+    #
+    #     # attention input
+    #     layers.append(
+    #         dict(
+    #             prev_op=module.input_layernorm,
+    #             layers=[
+    #                 module.self_attn.q_proj,
+    #                 module.self_attn.k_proj,
+    #                 module.self_attn.v_proj,
+    #             ],
+    #             inp=input_feat["self_attn.q_proj"],
+    #             module2inspect=module.self_attn,
+    #             kwargs=module_kwargs,
+    #         )
+    #     )
+    #
+    #     # attention out
+    #     # Please refer to https://github.com/mit-han-lab/llm-awq/pull/67#issue-1850622696
+    #     # Not GPA attention we need to skip o projection quant
+    #     if module.self_attn.v_proj.weight.shape == module.self_attn.o_proj.weight.shape:
+    #         layers.append(
+    #             dict(
+    #                 prev_op=module.self_attn.v_proj,
+    #                 layers=[module.self_attn.o_proj],
+    #                 inp=input_feat["self_attn.o_proj"],
+    #             )
+    #         )
+    #
+    #     # linear 1
+    #     layers.append(
+    #         dict(
+    #             prev_op=module.post_attention_layernorm,
+    #             layers=[module.mlp.gate_proj, module.mlp.up_proj],
+    #             inp=input_feat["mlp.gate_proj"],
+    #             module2inspect=module.mlp,
+    #         )
+    #     )
+    #
+    #     # linear 2
+    #     layers.append(
+    #         dict(
+    #             prev_op=module.mlp.up_proj,
+    #             layers=[module.mlp.down_proj],
+    #             inp=input_feat["mlp.down_proj"],
+    #         )
+    #     )
+    #
+    #     return layers
