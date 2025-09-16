45fed88fd060924f8adb5fe8c8828ecea5c3d976
LRL-ModelCloud <lrl@lbx.dev>
2025-09-16 09:59:21 +0800

    mod qwen3_moe

--- gptqmodel/models/definitions/qwen3_moe.py
+++ gptqmodel/models/definitions/qwen3_moe.py
@@ -29,29 +29,16 @@ class Qwen3MoeGPTQ(BaseGPTQModel):
     layers_node = ["model.layers"]
     layer_type = "Qwen3DecoderLayer"
 
-    # TODO: full deprecation by gptqmodel v4.3
-    # legacy definition (deprecated): migrate to layers_modules_tree
-    layer_modules = [
-        ["self_attn.q_proj", "self_attn.k_proj", "self_attn.v_proj"],
-        ["self_attn.o_proj"],
-
-        ["mlp.gate"],
-
-        # uses dynamic_expert_index
-        [f"mlp.experts.{EXPERT_INDEX_PLACEHOLDER}.up_proj", f"mlp.experts.{EXPERT_INDEX_PLACEHOLDER}.gate_proj"],
-        [f"mlp.experts.{EXPERT_INDEX_PLACEHOLDER}.down_proj"],
-    ]
-
-    layers_modules_tree = [
+    _layers_modules_tree = [
         "model",
         "layers",
         "#",
         {
-            "self_attn": ("k_proj", "v_proj", "q_proj", "o_proj"),
+            "self_attn": ("k_proj:0", "v_proj:0", "q_proj:0", "o_proj:1"),
             "mlp": {
                 "gate": ("gate",),
                 "experts": {
-                    "#": ("up_proj", "gate_proj", "down_proj"),
+                    "#": ("up_proj:0", "gate_proj:0", "down_proj:1"),
                 },
             },
         }
