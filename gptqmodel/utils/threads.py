d8b8fc8a379d90ba9774665e654a9bf8bd308129
Qubitium <Qubitium@modelcloud.ai>
2025-09-19 02:23:39 +0000

    add threads
    Signed-off-by: Qubitium <Qubitium@modelcloud.ai>

--- gptqmodel/utils/threads.py
+++ gptqmodel/utils/threads.py
@@ -1,3 +1,8 @@
+# SPDX-FileCopyrightText: 2024-2025 ModelCloud.ai
+# SPDX-FileCopyrightText: 2024-2025 qubitium@modelcloud.ai
+# SPDX-License-Identifier: Apache-2.0
+# Contact: qubitium@modelcloud.ai, x.com/qubitium
+
 import threading
 import traceback
 from concurrent.futures import ThreadPoolExecutor
