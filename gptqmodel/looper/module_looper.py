f404d710fc9392767f84359980976d99d76c170c
LRL-ModelCloud <lrl@lbx.dev>
2025-09-22 17:50:02 +0800

    add todo

--- gptqmodel/looper/module_looper.py
+++ gptqmodel/looper/module_looper.py
@@ -543,16 +543,17 @@ class ModuleLooper():
                     for reverse_p in reversed(self.processors):
                         for name in processed_subset:
                             module = processed_subset[name]
-                            reverse_p.submodule_finalize(module, self.gptq_model)
+                            reverse_p.submodule_finalize(module)
 
+                            # TODO: offload packed module to disk
                             # checking for disk offloading
-                            if self.gptq_model.quantize_config.offload_to_disk:
-                                ASYNC_WORKER.submit(partial(
-                                    offload_to_disk,
-                                    model=self.gptq_model.model,
-                                    module=module,
-                                    disk_path=self.gptq_model.quantize_config.offload_to_disk_path,
-                                ))
+                            # if self.gptq_model.quantize_config.offload_to_disk:
+                            #     ASYNC_WORKER.submit(partial(
+                            #         offload_to_disk,
+                            #         model=self.gptq_model.model,
+                            #         module=module,
+                            #         disk_path=self.gptq_model.quantize_config.offload_to_disk_path,
+                            #     ))
 
                     #del module
 
