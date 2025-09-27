9aef8905a8417deb5d386c860ae1f975cb44f530
LRL2-ModelCloud <lrl2@modelcloud.ai>
2025-09-27 10:31:29 +0800

    add Qwen3OmniMoe

---
+++ gptqmodel/models/definitions/qwen3_omni_moe.py
@@ -0,0 +1,31 @@
+# SPDX-FileCopyrightText: 2024-2025 ModelCloud.ai
+# SPDX-FileCopyrightText: 2024-2025 qubitium@modelcloud.ai
+# SPDX-License-Identifier: Apache-2.0
+# Contact: qubitium@modelcloud.ai, x.com/qubitium
+
+from transformers import AutoModelForTextToWaveform
+from ..base import BaseQModel
+
+
+class Qwen3OmniMoeGPTQ(BaseQModel):
+    loader = AutoModelForTextToWaveform
+
+    pre_lm_head_norm_module = "thinker.model.norm"
+
+    module_tree = [
+        "thinker",
+        "model",
+        "layers",
+        "#",
+        {
+            "input_layernorm": ("input_layernorm:!",),
+            "self_attn": ("q_proj:0", "k_proj:0", "v_proj:0", "o_proj:1"),
+            "post_attention_layernorm": ("post_attention_layernorm:!",),
+            "mlp": {
+                "gate": ("gate",),
+                "experts": {
+                    "#": ("gate_proj:0", "up_proj:0", "down_proj:1"),
+                },
+            },
+        }
+    ]
