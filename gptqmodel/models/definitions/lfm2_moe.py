4985cd105533811c4c21674bc41d94a338d5932d
LRL2-ModelCloud <lrl2@modelcloud.ai>
2025-10-13 13:42:11 +0800

    add lfm2 moe model definition

---
+++ gptqmodel/models/definitions/lfm2_moe.py
@@ -0,0 +1,23 @@
+# SPDX-FileCopyrightText: 2024-2025 ModelCloud.ai
+# SPDX-FileCopyrightText: 2024-2025 qubitium@modelcloud.ai
+# SPDX-License-Identifier: Apache-2.0
+# Contact: qubitium@modelcloud.ai, x.com/qubitium
+
+from ..base import BaseQModel
+
+
+class LFM2MoeQModel(BaseQModel):
+    pre_lm_head_norm_module = "model.norm"
+
+    module_tree= [
+        "model",
+        "layers",
+        "#",
+        {
+            "input_layernorm": ("input_layernorm:!",),
+            "self_attn": ("q_proj:0", "k_proj:0", "v_proj:0", "o_proj:1"),
+            "mamba": ("in_proj:0", "out_proj:1"),
+            "feed_forward": ("gate_proj:0", "up_proj:0", "down_proj:1"),
+        }
+    ]
+
