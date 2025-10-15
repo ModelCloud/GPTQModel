be2628b8f4af7f108e9aa7e1dc38f3a3a9e8be0f
Qubitium <qubitium@modelcloud.ai>
2025-10-15 10:13:03 +0000

    spdx
    Signed-off-by: Qubitium <qubitium@modelcloud.ai>

--- gptqmodel/eora/eora.py
+++ gptqmodel/eora/eora.py
@@ -1,18 +1,9 @@
-# Copyright 2024-2025 NVIDIA CORPORATION
-# EoRA arXiv: https://arxiv.org/abs/2410.21271
+# SPDX-FileCopyrightText: 2024-2025 NVIDIA CORPORATION
+# SPDX-FileCopyrightText: 2025 ModelCloud.ai (qubitium@modelcloud.ai)
+# SPDX-License-Identifier: Apache-2.0
+# EoRA arXiv https://arxiv.org/abs/2410.21271
 # EoRA Official Repo: https://github.com/NVlabs/EoRA
-
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# This file has been modified by ModelCloud.AI team and qubitium@modelcloud.ai for adoption into GPT-QModel
 
 from typing import Tuple
 
