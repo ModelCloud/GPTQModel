aa085fb2c7b261dabb498d02f7a7db2de103ac87
Qubitium-ModelCloud <qubitium@modelcloud.ai>
2025-10-17 01:15:19 +0800

    Scores (#2049)
    * update scores
    
    * cleanup
    
    * cleanup

--- tests/models/test_qwen3_next.py
+++ tests/models/test_qwen3_next.py
@@ -8,13 +8,21 @@ from model_test import ModelTest
 
 class TestQwen3Next(ModelTest):
     NATIVE_MODEL_ID = "/monster/data/model/Qwen3-Next-80B-A3B-Instruct"
-    QUANT_ARC_MAX_DELTA_FLOOR_PERCENT = 0.05
+    QUANT_ARC_MAX_DELTA_FLOOR_PERCENT = 0.04
     NATIVE_ARC_CHALLENGE_ACC = 0.3900
     NATIVE_ARC_CHALLENGE_ACC_NORM = 0.3900
     TRUST_REMOTE_CODE = True
     APPLY_CHAT_TEMPLATE = True
-    EVAL_BATCH_SIZE = 6
-    #DATASET_SIZE = 1024
+    EVAL_BATCH_SIZE = 4
+    V2 = False
+    DEBUG = True
+    ACT_GROUP_AWARE = True
+    DESC_ACT = False
+    DATASET_SIZE = 1024
+    DATASET_SORT = "desc"
+    QUANT_BATCH_SIZE = 4
+    CALIB_NOISE_MODE = "unseen"
+    CALIB_NOISE_PERCENT = 0.025
 
     def test_mimo(self):
         self.quant_lm_eval()
