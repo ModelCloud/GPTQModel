797092895b87db9356c9ba9569c359b61f8adf1d
Qubitium <qubitium@modelcloud.ai>
2025-10-27 15:04:44 +0000

    ruff 0.14.2

--- format/format.sh
+++ format/format.sh
@@ -3,7 +3,7 @@
 cd "$(dirname "$0")" || exit
 
 # force ruff/isort to be same version as setup.py
-pip install -U ruff==0.13.0
+pip install -U ruff==0.14.2
 #isort==6.0.1
 
 ruff check ../gptqmodel/models ../gptqmodel/nn_modules ../gptqmodel/quantization ../gptqmodel/utils ../gptqmodel/__init__.py ../examples ../tests ../setup.py --fix --unsafe-fixes
