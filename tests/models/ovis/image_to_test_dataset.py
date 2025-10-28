b05e67ff23d1879c4b92ca4003f4613ba323d3dd
ZX-ModelCloud <zx@modelcloud.ai>
2025-10-28 04:03:47 +0000

    cleanup
    Signed-off-by: ZX-ModelCloud <zx@modelcloud.ai>

--- tests/models/ovis/image_to_test_dataset.py
+++ tests/models/ovis/image_to_test_dataset.py
@@ -8,8 +8,22 @@ from gptqmodel.models.definitions.ovis2 import Ovis2QModel
 from gptqmodel.models.definitions.base_qwen2_5_omni import BaseQwen2_5_OmniGPTQ
 from gptqmodel.models.definitions.base_qwen2_vl import BaseQwen2VLGPTQ
 
-
 def format_ovis_dataset(image, assistant):
+    return {
+        "image": image,
+        "conversations": [
+            {
+                "from": "human",
+                "value": "<image>\nWrite a detailed description of this image, do not forget about the texts on it if they exist. Also, do not forget to mention the type / style of the image. No bullet points. When writing descriptions, prioritize clarity and direct observation over embellishment or interpretation.\nDon't forget these rules:\n1. **Be Direct and Concise**: Provide straightforward descriptions without adding interpretative or speculative elements.\n2. **Use Segmented Details**: Break down details about different elements of an image into distinct sentences, focusing on one aspect at a time.\n3. **Maintain a Descriptive Focus**: Prioritize purely visible elements of the image, avoiding conclusions or inferences.\n4. **Follow a Logical Structure**: Begin with the central figure or subject and expand outward, detailing its appearance before addressing the surrounding setting.\n5. **Avoid Juxtaposition**: Do not use comparison or contrast language; keep the description purely factual.\n6. **Incorporate Specificity**: Mention age, gender, race, and specific brands or notable features when present, and clearly identify the medium if it's discernible."
+            },
+            {
+                "from": "gpt",
+                "value": assistant
+            }
+        ]
+    }
+
+def format_ovis2_dataset(image, assistant):
     return [
         {
             "role": "user",
@@ -66,8 +80,11 @@ def prepare_dataset(format_func, n_sample: int = 20) -> list[list[dict]]:
 
 
 def get_calib_dataset(model):
-    if isinstance(model, OvisQModel) or isinstance(model, Ovis2QModel):
+    if isinstance(model, OvisQModel):
         return prepare_dataset(format_ovis_dataset, n_sample=20)
+
+    if isinstance(model, Ovis2QModel):
+        return prepare_dataset(format_ovis2_dataset, n_sample=20)
 
     if isinstance(model, BaseQwen2VLGPTQ):
         return prepare_dataset(format_qwen2_vl_dataset, n_sample=20)
