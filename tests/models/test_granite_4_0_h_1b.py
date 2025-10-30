5f00148e92c13d5979ea5fd6062c2456897169e0
ZX-ModelCloud <zx@modelcloud.ai>
2025-10-30 18:57:49 +0800

    add test_granite_4_0_h_1b.py
    Signed-off-by: ZX-ModelCloud <zx@modelcloud.ai>

--- tests/models/test_granite_4_0_h_1b.py
+++ tests/models/test_granite_4_0_h_1b.py
@@ -19,21 +19,21 @@ from gptqmodel.utils.eval import EVAL
 # | arc_challenge :: acc,none      |   0.3174 |
 # | arc_challenge :: acc_norm,none |   0.3601 |
 # | mmlu_stem :: acc,none          |   0.3186 |
-class Test_Granite_4_0_H_350M(ModelTest):
-    NATIVE_MODEL_ID = "/monster/data/model/granite-4.0-h-350m" # "ibm-granite/granite-4.0-h-350m"
+class Test_Granite_4_0_H_1B(ModelTest):
+    NATIVE_MODEL_ID = "/monster/data/model/granite-4.0-h-1b" # "ibm-granite/granite-4.0-h-1b"
     GROUP_SIZE = 32
-    EVAL_BATCH_SIZE = 16
+    EVAL_BATCH_SIZE = 1
     LOAD_BACKEND = BACKEND.TORCH
     EVAL_TASKS = {
         EVAL.LM_EVAL.ARC_CHALLENGE: {
             "chat_template": True,
             "acc": {
-                "value": 0.3046,
+                "value": 0.3968,
                 "floor_pct": 0.04,
                 "ceil_pct": 0.10,
             },
             "acc_norm": {
-                "value": 0.3157,
+                "value": 0.4138,
                 "floor_pct": 0.04,
                 "ceil_pct": 0.10,
             },
@@ -41,7 +41,7 @@ class Test_Granite_4_0_H_350M(ModelTest):
         EVAL.LM_EVAL.MMLU_STEM: {
             "chat_template": False,
             "acc": {
-                "value": 0.2915,
+                "value": 0.4015,
                 "floor_pct": 0.1,
                 "ceil_pct": 0.20,
             },
