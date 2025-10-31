fd09b424544bad1cf8d87ce94258653a32726cd0
Qubitium <qubitium@modelcloud.ai>
2025-10-31 12:28:33 +0000

    update data

--- tests/models/test_qwen2_5.py
+++ tests/models/test_qwen2_5.py
@@ -10,20 +10,29 @@ from gptqmodel.utils.eval import EVAL
 
 # | Metric                         |   MARLIN |
 # |--------------------------------|----------|
-# | arc_challenge :: acc,none      |   0.2892 |
-# | arc_challenge :: acc_norm,none |   0.3302 |
-# | mmlu_stem :: acc,none          |   0.4351 |
+# | arc_challenge :: acc,none      |   0.2961 |
+# | arc_challenge :: acc_norm,none |   0.3285 |
+# | mmlu_stem :: acc,none          |   0.3942 |
+# | gsm8k_plat :: exact,flexible   |   0.2963 |
 class TestQwen2_5(ModelTest):
     GROUP_SIZE = 32
     NATIVE_MODEL_ID = "/monster/data/model/Qwen2.5-0.5B-Instruct"
     EVAL_BATCH_SIZE = 64
+    DATASET_CONCAT_SIZE = 2048
     EVAL_TASKS = {
+        EVAL.LM_EVAL.GSM8K_PLATINUM_COT: {
+            "chat_template": True,
+            "exact_match,flexible-extract": {
+                "value": 0.2963,
+                "floor_pct": 0.04,
+            },
+        },
         EVAL.LM_EVAL.ARC_CHALLENGE: {
-            "acc": {"value": 0.2910, "floor_pct": 0.04},
-            "acc_norm": {"value": 0.3268, "floor_pct": 0.04},
+            "acc": {"value": 0.2961, "floor_pct": 0.04},
+            "acc_norm": {"value": 0.3285, "floor_pct": 0.04},
         },
         EVAL.LM_EVAL.MMLU_STEM: {
-            "acc": {"value": 0.3819, "floor_pct": 0.04},
+            "acc": {"value": 0.3942, "floor_pct": 0.04},
         },
     }
 
