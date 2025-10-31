6f063b53e8786f6a01da69b9b5ce512752c292e4
Qubitium <qubitium@modelcloud.ai>
2025-10-31 12:39:06 +0000

    update

--- tests/models/test_llama3_2_awq.py
+++ tests/models/test_llama3_2_awq.py
@@ -9,29 +9,39 @@ from gptqmodel.quantization import FORMAT, METHOD
 from gptqmodel.utils.eval import EVAL
 
 
-# a100:0
-# desc_act = False, act_group_aware = False 0.2500/0.2841
-# desc_act = False, act_group_aware = True 0.3063/0.3456
-# desc_act = True, 0.3089/0.3328
+# | Metric                         |   MARLIN |
+# |--------------------------------|----------|
+# | arc_challenge :: acc,none      |   0.3106 |
+# | arc_challenge :: acc_norm,none |   0.3532 |
+# | mmlu_stem :: acc,none          |   0.3527 |
+# | gsm8k_plat :: exact,flexible   |   0.2192 |
 class TestLlama3_2_awq(ModelTest):
     NATIVE_MODEL_ID = "/monster/data/model/Llama-3.2-1B-Instruct" # "meta-llama/Llama-3.2-1B-Instruct"
     EVAL_BATCH_SIZE = 64
+    DATASET_CONCAT_SIZE = 2048 # new
     EVAL_TASKS = {
+        EVAL.LM_EVAL.GSM8K_PLATINUM_COT: {
+            "chat_template": True,
+            "exact_match,flexible-extract": {
+                "value": 0.2192,
+                "floor_pct": 0.04,
+            },
+        },
         EVAL.LM_EVAL.ARC_CHALLENGE: {
             "chat_template": True,
             "acc": {
-                "value": 0.3200,
+                "value": 0.3106,
                 "floor_pct": 0.04,
             },
             "acc_norm": {
-                "value": 0.3362,
+                "value": 0.3532,
                 "floor_pct": 0.04,
             },
         },
         EVAL.LM_EVAL.MMLU_STEM: {
             "chat_template": False,
             "acc": {
-                "value": 0.3657,
+                "value": 0.3527,
                 "floor_pct": 0.04,
             },
         },
