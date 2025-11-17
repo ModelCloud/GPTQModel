a607231d86c3aef8afe826aff79cb4d2a7e6d318
Guo Wei <guowei30@xiaomi.com>
2025-11-17 17:11:42 +0800

    Update test cases for qwen2.5-vl and qwen3-vl

--- tests/models/test_qwen2_5_vl.py
+++ tests/models/test_qwen2_5_vl.py
@@ -5,11 +5,10 @@
 
 from model_test import ModelTest
 
-from gptqmodel.models.definitions.qwen2_vl import Qwen2VLQModel
+from gptqmodel.models.definitions.qwen2_5_vl import Qwen2_5_VLQModel
 from gptqmodel.utils.eval import EVAL
 
-
-class TestQwen2_VL(ModelTest):
+class TestQwen2_5_VL(ModelTest):
     NATIVE_MODEL_ID = "/monster/data/model/Qwen2.5-VL-3B-Instruct"
     EVAL_TASKS = {
         EVAL.LM_EVAL.ARC_CHALLENGE: {
@@ -43,7 +42,7 @@ class TestQwen2_VL(ModelTest):
             messages, tokenize=False, add_generation_prompt=True
         )
 
-        image_inputs = Qwen2VLQModel.process_vision_info(messages)
+        image_inputs = Qwen2_5_VLQModel.process_vision_info(messages)
         inputs = processor(
             text=[text],
             images=image_inputs,
