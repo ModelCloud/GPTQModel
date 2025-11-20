546e79ecefea41d8416cf627b9ebd953ad44249f
ZX-ModelCloud <zx@modelcloud.ai>
2025-11-20 11:36:11 +0800

    format

--- tests/test_lm_head.py
+++ tests/test_lm_head.py
@@ -17,7 +17,7 @@ os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 from models.model_test import ModelTest  # noqa: E402
 
 from gptqmodel import GPTQModel  # noqa: E402
-from gptqmodel.quantization.config import QuantizeEmbed, QuantizeConfig
+from gptqmodel.quantization.config import QuantizeConfig, QuantizeEmbed
 from gptqmodel.utils.eval import EVAL  # noqa: E402
 
 
