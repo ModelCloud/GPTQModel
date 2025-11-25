65bb7d45dd1506b5bac4df66c9f519a818c5a658
ZX-ModelCloud <zx@modelcloud.ai>
2025-11-25 14:15:19 +0800

    remove gptqmodel/quantization/awq/modules/linear/
    Signed-off-by: ZX-ModelCloud <zx@modelcloud.ai>

--- gptqmodel/quantization/awq/modules/linear/__init__.py
+++
@@ -1,8 +0,0 @@
-# SPDX-FileCopyrightText: 2024-2025 ModelCloud.ai
-# SPDX-FileCopyrightText: 2024-2025 qubitium@modelcloud.ai
-# SPDX-License-Identifier: Apache-2.0
-# Contact: qubitium@modelcloud.ai, x.com/qubitium
-
-from .gemv import WQLinear_GEMV
-from .gemv_fast import WQLinear_GEMVFast
-from .marlin import WQLinear_Marlin
