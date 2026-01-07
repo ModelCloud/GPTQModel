5b97462ec5382dbb84f523d0bff5283b70e24240
ZX-ModelCloud <zx@modelcloud.ai>
2026-01-07 10:51:15 +0800

    Fixed a bug that caused quantization to fail for non-MoE models.
    Signed-off-by: ZX-ModelCloud <zx@modelcloud.ai>

--- gptqmodel/looper/module_looper.py
+++ gptqmodel/looper/module_looper.py
@@ -160,8 +160,11 @@ class ModuleLooper():
         # Track current subset for MoE lifecycle hooks
         self._current_subset: Optional[Dict[str, Any]] = None
 
-        num_experts = self.gptq_model.get_num_experts(self.gptq_model.model.config)
-        self.moe_routing_override = self.gptq_model.quantize_config.moe_routing_override(num_experts)
+        if self.gptq_model.dynamic_expert_index:
+            num_experts = self.gptq_model.get_num_experts(self.gptq_model.model.config)
+            self.moe_routing_override = self.gptq_model.quantize_config.moe_routing_override(num_experts)
+        else:
+            self.moe_routing_override = None
 
         for processor in self.processors:
             self._processor_mask_tls(processor)
