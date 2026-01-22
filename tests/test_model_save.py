e98ff1fa127f73558c151a9a58ed230e752221b7
ZX-ModelCloud <zx@modelcloud.ai>
2026-01-22 18:04:27 +0800

    remove test_model_save.py
    Signed-off-by: ZX-ModelCloud <zx@modelcloud.ai>

--- tests/test_model_save.py
+++
@@ -1,4 +0,0 @@
-# SPDX-FileCopyrightText: 2024-2025 ModelCloud.ai
-# SPDX-FileCopyrightText: 2024-2025 qubitium@modelcloud.ai
-# SPDX-License-Identifier: Apache-2.0
-# Contact: qubitium@modelcloud.ai, x.com/qubitium
