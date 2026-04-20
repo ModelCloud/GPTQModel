757663b30646c8fe8156d834485809336b15ce87
CSY-ModelCloud <csy@modelcloud.ai>
2026-04-20 15:46:15 +0800

    disable offload to disk for TeleChat

--- tests/models/test_telechat2.py
+++ tests/models/test_telechat2.py
@@ -25,6 +25,7 @@ class TestTeleChat_2(ModelTest):
         },
     }
     EVAL_TASKS_FAST = ModelTest.derive_fast_eval_tasks(EVAL_TASKS_SLOW)
+    OFFLOAD_TO_DISK = False
 
 
     def test_telechat2(self):
