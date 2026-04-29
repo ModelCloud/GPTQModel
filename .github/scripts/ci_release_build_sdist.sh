59df04e2037492c205ace75155dd2c3ba95ce835
CSY-ModelCloud <csy@modelcloud.ai>
2026-04-29 09:40:48 +0800

    [CI] install uv first

--- .github/scripts/ci_release_build_sdist.sh
+++ .github/scripts/ci_release_build_sdist.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-python -m pip install -U build twine setuptools
+python -m pip install uv
+uv python -m pip install -U build twine setuptools
 python -m build --sdist
