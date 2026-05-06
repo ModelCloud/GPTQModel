e705283b2196e5100bd956079dd4d48be4f2f1d3
ZX-ModelCloud <zx@modelcloud.ai>
2026-05-06 12:48:06 +0800

    added "qwen3_5_moe_text" definition and "qwen3_5_text" definition
    Signed-off-by: ZX-ModelCloud <zx@modelcloud.ai>

---
+++ tests/models/test_qwen3_5_text_only.py
@@ -0,0 +1,32 @@
+# SPDX-FileCopyrightText: 2026 ModelCloud.ai
+# SPDX-FileCopyrightText: 2026 qubitium@modelcloud.ai
+# SPDX-License-Identifier: Apache-2.0
+# Contact: qubitium@modelcloud.ai, x.com/qubitium
+from model_test import ModelTest
+
+
+# |--------------------------------|----------|
+# | arc_challenge :: acc,none      |   0.6092 |
+# | arc_challenge :: acc_norm,none |   0.6143 |
+# | mmlu_stem :: acc,none          |   0.8461 |
+class TestQwen3_5(ModelTest):
+    NATIVE_MODEL_ID = "/monster/data/model/Qwen3.5-9B-text-only" # principled-intelligence/Qwen3.5-9B-text-only
+    EVAL_TASKS_SLOW = {
+        "arc_challenge": {
+            "acc": {"value": 0.6092, "floor_pct": 0.04},
+            "acc_norm": {"value": 0.6143, "floor_pct": 0.04},
+        },
+        "gsm8k_platinum_cot": {
+            "chat_template": True,
+            "acc,num": {
+                "value": 0.3871,
+                "floor_pct": 0.04,
+                "ceil_pct": 0.04,
+            },
+        },
+    }
+    EVAL_TASKS_FAST = ModelTest.derive_fast_eval_tasks(EVAL_TASKS_SLOW)
+
+
+    def test_qwen3_5(self):
+        self.quantize_and_evaluate()
