8dce3e37eaa8e987f4d0017702778f863da71dfb
Qubitium <qubitium@modelcloud.ai>
2026-08-31 02:13:08 +0000

    fix(awq): tag all registered MoE roots

--- gptqmodel/models/definitions/gpt_oss.py
+++ gptqmodel/models/definitions/gpt_oss.py
@@ -18,7 +18,7 @@ class GPTOSSGPTQ(BaseQModel):
             "input_layernorm": ("input_layernorm:!",),
             "self_attn": ("q_proj:0", "k_proj:0", "v_proj:0", "o_proj:1"),
             "post_attention_layernorm": ("post_attention_layernorm:!",),
-            "mlp": {
+            "mlp:moe": {
                 "experts": {
                     "#": ("gate_proj:0", "up_proj:0", "down_proj:1"),
                 },
