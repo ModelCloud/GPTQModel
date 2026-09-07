34974875d9a174012ed0a72c6f780ccd4945bcb3
ZX-ModelCloud <zx@modelcloud.ai>
2026-09-07 16:59:19 +0800

    [MODEL] support `ouro` and `spark2_5` (#3061)
    * support ouro
    
    Signed-off-by: ZX-ModelCloud <zx@modelcloud.ai>
    
    * support spark2_5
    
    Signed-off-by: ZX-ModelCloud <zx@modelcloud.ai>
    
    ---------
    
    Signed-off-by: ZX-ModelCloud <zx@modelcloud.ai>

--- gptqmodel/models/auto.py
+++ gptqmodel/models/auto.py
@@ -166,6 +166,7 @@ from .definitions.nemotron_omni import NemotronOmniQModel  # noqa: E402
 from .definitions.olmo3 import Olmo3QModel  # noqa: E402
 from .definitions.olmoe import OlmoeQModel  # noqa: E402
 from .definitions.opt import OptQModel  # noqa: E402
+from .definitions.ouro import OuroQModel  # noqa: E402
 from .definitions.ovis import OvisQModel  # noqa: E402
 from .definitions.ovis2 import Ovis2QModel  # noqa: E402
 from .definitions.ovis2_5 import Ovis2_5QModel  # noqa: E402
@@ -188,6 +189,9 @@ from .definitions.qwen3_omni_moe import Qwen3OmniMoeGPTQ
 from .definitions.qwen3_vl import Qwen3_VLQModel
 from .definitions.qwen4_exp import Qwen4ExpQModel  # noqa: E402
 from .definitions.rw import RwgQModel  # noqa: E402
+from .definitions.solar_open import SolarOpenQModel  # noqa: E402
+from .definitions.solar_open2 import SolarOpen2QModel  # noqa: E402
+from .definitions.spark2_5 import Spark2_5QModel  # noqa: E402
 from .definitions.starcoder2 import Starcoder2QModel  # noqa: E402
 from .definitions.telechat2 import TeleChat2QModel
 from .definitions.voxtral import VoxtralGPTQ  # noqa: E402
@@ -338,6 +342,7 @@ MODEL_MAP = {
     "olmo2": LlamaQModel, # 100% llama clone
     "olmo3": Olmo3QModel,
     "olmoe": OlmoeQModel,
+    "ouro": OuroQModel,
     "ovis": OvisQModel,
     "ovis2": Ovis2QModel,
     "ovis2_5": Ovis2_5QModel,
@@ -356,6 +361,9 @@ MODEL_MAP = {
     "ernie4_5_moe_vl": Ernie4_5_VLMoeQModel, # Backward Compatibility alias
     "ernie4_5_vl_moe": Ernie4_5_VLMoeQModel,
     "seed_oss": LlamaQModel, # 100% llama clone
+    "solar_open": SolarOpenQModel,
+    "solar_open2": SolarOpen2QModel,
+    "spark2_5": Spark2_5QModel,
     "gpt_oss": GPTOSSGPTQ,
     "longcat_flash": LongCatFlashQModel,
     "locateanything": LocateAnythingQModel,
